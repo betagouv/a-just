ecb984fadae6773da6f7d2ff542333286cf120e6
François-Xavier Montigny <fxaviermontigny@gmail.com>
2021-11-25 13:50:01 +0100

    add activities page

--- front/src/app/routes/activities/activities.module.ts
+++ front/src/app/routes/activities/activities.module.ts
@@ -5,9 +5,10 @@ import { ActivitiesPageModule } from './activities.routing';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { MaterialModule } from 'src/app/libs/material.module';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ActivitiesPage],
-  imports: [ActivitiesPageModule, RouterModule, ComponentsModule, MaterialModule, CommonModule],
+  imports: [ActivitiesPageModule, RouterModule, ComponentsModule, MaterialModule, CommonModule, ReactiveFormsModule, FormsModule],
 })
 export class ActivitiesModule {}
--- front/src/app/routes/activities/activities.page.ts
+++ front/src/app/routes/activities/activities.page.ts
@@ -5,10 +5,6 @@ import { MainClass } from 'src/app/libs/main-class';
 import { ActivitiesService } from 'src/app/services/activities/activities.service';
 import { HumanResourceService } from 'src/app/services/human-resource/human-resource.service';
 
-interface VisualInterface extends ContentieuReferentielInterface {
-  showGroup: boolean;
-}
-
 @Component({
   templateUrl: './activities.page.html',
   styleUrls: ['./activities.page.scss'],
@@ -16,25 +12,58 @@ interface VisualInterface extends ContentieuReferentielInterface {
 export class ActivitiesPage extends MainClass implements OnDestroy {
   activities: ActivityInterface[] = [];
   activityMonth: Date = new Date();
-  referentiel: VisualInterface[] = [];
+  referentiel: ContentieuReferentielInterface[] = [];
 
-  constructor(private activitiesService: ActivitiesService, private humanResourceService: HumanResourceService) {
+  constructor(
+    private activitiesService: ActivitiesService,
+    private humanResourceService: HumanResourceService
+  ) {
     super();
 
-    this.watch(this.activitiesService.activities.subscribe(a => {
-      this.activities = a;
-    }));
-
-    this.watch(this.activitiesService.activityMonth.subscribe(a => this.activityMonth = a));
+    this.watch(
+      this.activitiesService.activities.subscribe((a) => {
+        this.activities = a;
+      })
+    );
 
     this.watch(
-      this.humanResourceService.contentieuxReferentiel.subscribe(ref => {
-        this.referentiel = ref.map(r => ({...r, showGroup: false}));
-      })
+      this.activitiesService.activityMonth.subscribe(
+        (a) => (this.activityMonth = a)
+      )
+    );
+
+    this.watch(
+      this.humanResourceService.contentieuxReferentiel.subscribe(
+        (ref) => (this.referentiel = ref)
+      )
     );
   }
 
   ngOnDestroy() {
     this.watcherDestroy();
+  }
+
+  onUpdateActivity(referentiel: ContentieuReferentielInterface) {
+    const options = {
+      in: 0,
+      out: 0,
+      stock: 0,
+    };
+
+    (referentiel.childrens || []).map((ref: ContentieuReferentielInterface) => {
+      options.in += ref.in || 0;
+      options.out += ref.out || 0;
+      options.stock += ref.stock || 0;
+    });
+
+    referentiel.in = options.in;
+    referentiel.out = options.out;
+    referentiel.stock = options.stock;
+
+    this.activitiesService.updateActivity(referentiel);
+  }
+
+  onUpdateMainActivity(referentiel: ContentieuReferentielInterface) {
+    this.activitiesService.updateActivity(referentiel);
   }
 }
