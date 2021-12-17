8879934c428ff266684fdf3cf2ef1cc971c274a7
François-Xavier Montigny <fxaviermontigny@gmail.com>
2021-12-17 12:42:34 +0100

    fix backup selected

--- api/src/routes-api/RouteHumanResources.js
+++ api/src/routes-api/RouteHumanResources.js
@@ -15,7 +15,7 @@ export default class RouteHumanResources extends Route {
   async getCurrentHr (ctx) {
     let { backupId } = this.body(ctx)
     const backups = await this.model.models.HRBackups.list(ctx.state.user.id)
-    backupId = backupId || backups.length ? backups[backups.length - 1].id : null
+    backupId = backupId || (backups.length ? backups[backups.length - 1].id : null)
 
     this.sendOk(ctx, {
       hr: await this.model.getCurrentHr(backupId),
