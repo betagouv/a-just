69994ecb99ad5b53faf0964797c5771b643bd4d1
François-Xavier Montigny <fxmontigny@users.noreply.github.com>
2022-01-03 11:03:57 +0100

    Feature/import activities (#35)
    * init BO with auto import datas
    
    * import activities

---
+++ front-admin/src/app/utils/file.ts
@@ -0,0 +1,37 @@
+export const exportFileToBlob = (file: File) => {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      // @ts-ignore
+      let blob = new Blob([new Uint8Array(e.target.result)], {
+        type: file.type,
+      });
+      resolve(blob);
+    };
+    reader.readAsArrayBuffer(file);
+  });
+};
+
+export const exportFileToBase64 = (file: File): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      resolve(reader.result as string);
+    };
+    reader.onerror = function (error) {
+      reject(error);
+    };
+  });
+};
+
+export const exportBase64ToBlob = (base64: string, type: string = 'text/csv') => {
+  var byteString = atob(base64.split(',')[1]);
+  var ab = new ArrayBuffer(byteString.length);
+  var ia = new Uint8Array(ab);
+
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  return new Blob([ab], { type });
+};
