3e20f0052cdb78f783dd6574959119945668b9bc
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-02-08 09:01:59 +0100

    fix backup option style

--- front/src/app/components/select/select.component.ts
+++ front/src/app/components/select/select.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnChanges, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 import { MainClass } from 'src/app/libs/main-class';
 
 export interface dataInterface {
@@ -16,6 +22,7 @@ export class SelectComponent extends MainClass implements OnChanges {
   @Input() icon: string = 'expand_more';
   @Input() value: number | string | null | number[] | string[] = null;
   @Input() datas: dataInterface[] = [];
+  @Input() multiple: boolean = true;
   @Output() valueChange: EventEmitter<number[] | string[]> = new EventEmitter();
   realValue: string = '';
 
@@ -29,25 +36,31 @@ export class SelectComponent extends MainClass implements OnChanges {
 
   findRealValue() {
     let valueFormated: number[] | string[] = [];
-    if(Array.isArray(valueFormated)) {
+    if (Array.isArray(valueFormated)) {
       // @ts-ignore
       valueFormated = this.value;
     } else {
       valueFormated = [valueFormated];
     }
 
-    this.realValue = valueFormated.map(val => {
-      const find = this.datas.find((d) => d.id === val);
-      if (find) {
-        return find.value;
-      }
-        
-      return this.value;
-    }).join(', ');    
+    this.realValue = valueFormated
+      .map((val) => {
+        const find = this.datas.find((d) => d.id === val);
+        if (find) {
+          return find.value;
+        }
+
+        return this.value;
+      })
+      .join(', ');
   }
 
-  onSelectedChanged(list: number[]) {
-    this.value = list;
+  onSelectedChanged(list: number[] | number) {
+    if (typeof list === 'number') {
+      this.value = [list];
+    } else {
+      this.value = list;
+    }
     this.valueChange.emit(this.value);
     this.findRealValue();
   }
