ac1a849cd682f557c2943101f6c44ef30aefe388
François-Xavier Montigny <fxmontigny@users.noreply.github.com>
2022-02-16 17:15:05 +0100

    Feature/group user roles (#64)
    * add new categories
    
    * add multiple roles

--- api/src/models/TableHrfonctions.js
+++ api/src/models/TableHrfonctions.js
@@ -1,7 +1,7 @@
 export default (sequelizeInstance, Model) => {
   Model.getAll = async () => {
     return await Model.findAll({
-      attributes: ['id', 'code', 'label', 'rank'],
+      attributes: ['id', 'code', 'label', 'rank', ['category_id', 'categoryId']],
       order: ['rank'],
     })
   }
