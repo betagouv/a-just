c66fbf6c104dde87ef871373ce2b944cb196d807
Vincent Viers <vincent.viers@beta.gouv.fr>
2022-02-23 15:35:58 +0100

    :sparkles: clean_nomenclature script

--- data-integration/clean_nomenclature.py
+++ data-integration/clean_nomenclature.py
@@ -1,4 +1,8 @@
+import logging
+
 import pandas as pd
+
+from utils import extract_nac
 
 # lire la nomenclature exportée de Mindmap en .csv (mode "Tableau Croisé Dynamique")
 klatur = pd.read_csv("config/nomenclature.csv", sep=";")
@@ -13,5 +17,24 @@ klatur.rename(columns={"Niveau 7": "NAC"}, inplace=True)
 klatur = klatur[
     ["Niveau 1", "Niveau 2", "Niveau 3", "Niveau 4", "NAC"]
 ].drop_duplicates()
+
+# extraire les NACs
+klatur.NAC = klatur.NAC.apply(extract_nac)
+
+# On supprime les feuilles de l'arbre qui ne sont pas des NACs
+klatur.dropna(subset=["NAC"], inplace=True)
+
+# on supprime le NAC "00A" qui est "Autre"
+klatur = klatur[klatur.NAC != "00A"]
+
+# vérifier qu'il n'y a pas de doublons
+if len(klatur["NAC"].unique()) != len(klatur):
+    logging.warning("Il semble y avoir des doublons dans la nomenclature.")
+    print(
+        klatur.groupby("NAC")
+        .agg({"Niveau 4": "count"})
+        .sort_values("Niveau 4", ascending=False)
+        .rename(columns={"Niveau 4": "compte"})
+    )
 
 klatur.to_csv("config/nomenclature_clean.csv")
