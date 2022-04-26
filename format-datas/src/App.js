5b8c4bb9a0e1a3a9f44f0379a1ef31c4607de682
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-04-26 11:02:27 +0200

    wip - data générator

--- format-datas/src/App.js
+++ format-datas/src/App.js
@@ -1,14 +1,14 @@
-import { join } from 'path'
-import config from 'config'
 import path from 'path'
-import xml2csv from '@wmfs/xml2csv'
-
+import lineByLine from 'n-readlines'
+import { appendFileSync, mkdirSync, readdirSync, rmSync, writeFileSync } from 'fs'
 
 export default class App {
   constructor () {}
 
-  start () {
+  async start () {
     console.log('--- START ---')
+    await this.convertFilesToCsv()
+
     this.done()
   }
 
@@ -16,45 +16,87 @@ export default class App {
     console.log('--- DONE ---')
     process.exit()
   }
-}
 
+  async convertFilesToCsv () {
+    const tmpFolder = path.join(__dirname, '../tmp')
+    const inputFolder = path.join(__dirname, '../inputs')
+    rmSync(tmpFolder, { recursive: true, force: true })
+    mkdirSync(tmpFolder, { recursive: true })
 
+    const files = readdirSync(inputFolder)
+      .filter((f) => f.endsWith('.xml'))
 
+    for(let i = 0; i < files.length; i++) {
+      const file = files[i]
+      console.log(file)
+      const csvPath = `${tmpFolder}/${file.replace('.xml', '.csv')}`
 
-/* xml2csv(
-  {
-    xmlPath: 'S06_men_20220224-002021_RGC-CA_f05.xml',
-    csvPath: 'S06_men_20220224-002021_RGC-CA_f05.csv',
-    rootXMLElement: 'ROW',
-    headerMap: [
-      ['I_ELST', 'I_ELST', 'string'],
-      ['C_TUS', 'C_TUS', 'string'],
-      ['COPRO', 'COPRO', 'string'],
-      ['NATAFF', 'NATAFF', 'string'],
-      ['MODSAI', 'MODSAI', 'string'],
-      ['MODFIN', 'MODFIN', 'string'],
-      ['AUTSAI', 'AUTSAI', 'string'],
-      ['AUTDJU', 'AUTDJU', 'string'],
-      ['RENPAR', 'RENPAR', 'string'],
-      ['DECISION', 'DECISION', 'string'],
-      ['PERIODE', 'PERIODE', 'string'],
-      ['NBAFF', 'NBAFF', 'string'],
-      ['NBAFFDUR', 'NBAFFDUR', 'string'],
-      ['SOMDUR', 'SOMDUR', 'string'],
-      ['NBAFFDURDEBAT', 'NBAFFDURDEBAT', 'string'],
-      ['SOMDURDEBAT', 'SOMDURDEBAT', 'string'],
-      ['MINDUR', 'MINDUR', 'string'],
-      ['MAXDUR', 'MAXDUR', 'string'],
-      ['NBAFFSTOCK_12M', 'NBAFFSTOCK_12M', 'string'],
-      ['NBAFFDAUDPLAI', 'NBAFFDAUDPLAI', 'string'],
-      ['SOMDURDAUDPLAI', 'SOMDURDAUDPLAI', 'string'],
-      ['NBAFFDURETAT', 'NBAFFDURETAT', 'string'],
-      ['SOMDURETAT', 'SOMDURETAT', 'string'],
-      ['DATMAJ', 'DATMAJ', 'string'],
-    ],
-  },
-  function (err, result) {
-    console.log(err)
-    console.log(result)
+      let liner = new lineByLine(`${inputFolder}/${file}`)
+      let line
+      let nbLine = 0
+      let secondTag = ''
+      let headerMap = []
+      let isEnd = false
+ 
+      // get header
+      while ((line = liner.next()) !== null && !isEnd) {
+        const lineFormated = line.toString('ascii').trim()
+
+        if(nbLine === 2) {
+          secondTag = this.getTagName(lineFormated)
+        } else if(`</${secondTag}>` === lineFormated) {
+          isEnd = true
+        } else if(nbLine > 2) {
+          const newTag = this.getTagName(lineFormated)
+          headerMap.push(newTag)
+        }
+
+        nbLine ++
+      }
+
+      // create file
+      writeFileSync(csvPath, `${headerMap.join(',')}\n`)
+      
+      // complete file
+      liner = new lineByLine(`${inputFolder}/${file}`)
+      let dataLines
+      let totalLine = 0
+      nbLine = 0
+      while ((line = liner.next()) !== null) {
+        const lineFormated = line.toString('ascii').trim()
+        const tag = this.getTagName(lineFormated)
+
+        if(tag === secondTag) {
+          secondTag = this.getTagName(lineFormated)
+          dataLines = headerMap.map(() => ('')) // create empty map
+        } else if(`</${secondTag}>` === lineFormated) {
+          // console.log('add new line', dataLines)
+          appendFileSync(csvPath, `${dataLines.join(',')}\n`)
+          totalLine++
+        } else if(nbLine > 2) {
+          const index = headerMap.indexOf(tag)
+          if(index !== -1) {
+            dataLines[index] = this.getTagValue(lineFormated)
+          }
+        }
+
+        nbLine ++
+      }
+
+      console.log(`add ${totalLine} lines`)
+    }
   }
-)*/
+
+  getTagName (stringNotFormated) {
+    let tag = stringNotFormated.trim().split('>')[0].replace(/(<|>)/g, '')
+    return tag.split(' ')[0]
+  }
+
+  getTagValue (stringNotFormated) {
+    let tab = stringNotFormated.trim().split('>')
+    if(tab.length > 1) {
+      return tab[1].trim().split('<')[0]
+    }
+    return ' '
+  }
+}
