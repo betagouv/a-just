59f9f4a64e4695cb00279c212ef8543e88e42330
Jimmy <jimmy.chevallier@free.fr>
2022-05-10 20:45:49 +0200

    FINISH Date to string calculator

--- front/src/app/components/referentiel-calculator/referentiel-calculator.component.ts
+++ front/src/app/components/referentiel-calculator/referentiel-calculator.component.ts
@@ -1,31 +1,42 @@
-import { Component, HostBinding, Input } from '@angular/core';
-import { CalculatorInterface } from 'src/app/interfaces/calculator';
-import { MainClass } from 'src/app/libs/main-class';
-import { CalculatorService } from 'src/app/services/calculator/calculator.service';
+import { Component, HostBinding, Input } from '@angular/core'
+import { CalculatorInterface } from 'src/app/interfaces/calculator'
+import { MainClass } from 'src/app/libs/main-class'
+import { CalculatorService } from 'src/app/services/calculator/calculator.service'
 
 @Component({
-  selector: 'aj-referentiel-calculator',
-  templateUrl: './referentiel-calculator.component.html',
-  styleUrls: ['./referentiel-calculator.component.scss'],
+    selector: 'aj-referentiel-calculator',
+    templateUrl: './referentiel-calculator.component.html',
+    styleUrls: ['./referentiel-calculator.component.scss'],
 })
 export class ReferentielCalculatorComponent extends MainClass {
-  @Input() calculator: CalculatorInterface | null = null;
-  @Input() sortBy: string = '';
-  @HostBinding('class.show-children') showChildren: boolean =
-    (this.calculator && this.calculator.childIsVisible) || false;
+    @Input() calculator: CalculatorInterface | null = null
+    @Input() sortBy: string = ''
+    @HostBinding('class.show-children') showChildren: boolean =
+        (this.calculator && this.calculator.childIsVisible) || false
 
-  constructor(private calcultorService: CalculatorService) {
-    super();
-  }
-
-  onToggleChildren() {
-    if (this.calculator) {
-      this.showChildren = !this.showChildren;
-      this.calculator.childIsVisible = this.showChildren;
-
-      if (this.showChildren) {
-        this.calcultorService.loadChildren(this.calculator.contentieux.id);
-      }
+    constructor(private calcultorService: CalculatorService) {
+        super()
     }
-  }
+
+    onToggleChildren() {
+        if (this.calculator) {
+            this.showChildren = !this.showChildren
+            this.calculator.childIsVisible = this.showChildren
+
+            if (this.showChildren) {
+                this.calcultorService.loadChildren(
+                    this.calculator.contentieux.id
+                )
+            }
+        }
+    }
+
+    decimalToStringDate(decimal: number | null) {
+        if (decimal != null) {
+            const n = new Date(0, 0)
+            n.setMinutes(+decimal * 60)
+            return n.toTimeString().slice(0, 5)
+        }
+        return
+    }
 }
