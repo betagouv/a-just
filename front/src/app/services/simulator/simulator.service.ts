a2f8353c10faaf7ce8ab74203555cc0dad277642
Jimmy <jimmy.chevallier@free.fr>
2022-05-18 16:37:34 +0200

    update calcul temps moyen par dossier

--- front/src/app/services/simulator/simulator.service.ts
+++ front/src/app/services/simulator/simulator.service.ts
@@ -183,6 +183,18 @@ export class SimulatorService extends MainClass {
             let etpAffected = this.getHRPositions(referentielId as number)
             let etpMag = etpAffected.length >= 0 ? etpAffected[0].totalEtp : 0
 
+            let etpAffectedToCompute = this.getHRPositions(
+                referentielId as number,
+                month(this.startCurrentSituation, counter),
+                true,
+                month(this.endCurrentSituation, counter, 'lastday')
+            )
+            let etpMagToCompute =
+                etpAffectedToCompute.length >= 0
+                    ? etpAffectedToCompute[0].totalEtp
+                    : 0
+
+            console.log('display etpMagToCompute', etpMagToCompute)
             // Temps moyens par dossier observé = (nb heures travaillées par mois) / (sorties moyennes par mois / etpt sur la periode)
             let realTimePerCase = fixDecimal(
                 ((environment.nbDaysByMagistrat / 12) *
@@ -318,7 +330,12 @@ export class SimulatorService extends MainClass {
             }
     }
 
-    getHRPositions(referentiel: number, date?: Date) {
+    getHRPositions(
+        referentiel: number,
+        date?: Date,
+        onPeriod?: boolean,
+        dateStop?: Date
+    ) {
         const hr = this.humanResourceService.hr.getValue()
         const categories = this.humanResourceService.categories.getValue()
         const hrCategories: any = {}
@@ -332,12 +349,22 @@ export class SimulatorService extends MainClass {
         })
 
         for (let i = 0; i < hr.length; i++) {
-            const etptAll = this.getHRVentilation(
-                hr[i],
-                referentiel,
-                categories,
-                date
-            )
+            const etptAll =
+                onPeriod === true
+                    ? this.getHRVentilationOnPeriod(
+                          hr[i],
+                          referentiel,
+                          categories,
+                          date instanceof Date ? date : undefined,
+                          dateStop instanceof Date ? dateStop : undefined
+                      )
+                    : this.getHRVentilation(
+                          hr[i],
+                          referentiel,
+                          categories,
+                          date
+                      )
+
             Object.values(etptAll).map((c) => {
                 if (c.etpt) {
                     hrCategories[c.label].list.push(hr[i])
@@ -396,6 +423,69 @@ export class SimulatorService extends MainClass {
         // format render
         for (const property in list) {
             list[property].etpt = list[property].etpt / 1
+        }
+
+        return list
+    }
+
+    getHRVentilationOnPeriod(
+        hr: HumanResourceInterface,
+        referentielId: number,
+        categories: HRCategoryInterface[],
+        dateStart: Date | undefined,
+        dateStop: Date | undefined
+    ): number {
+        const list: any = {}
+        categories.map((c) => {
+            list[c.id] = {
+                etpt: 0,
+                ...c,
+            }
+        })
+
+        const now = dateStart instanceof Date ? dateStart : new Date()
+        const stop = dateStop instanceof Date ? dateStop : new Date()
+
+        let nbDay = 0
+        do {
+            // only working day
+            if (workingDay(now)) {
+                nbDay++
+                const situation = this.humanResourceService.findSituation(
+                    hr,
+                    now
+                )
+
+                if (situation && situation.category && situation.category.id) {
+                    const activitiesFiltred = (
+                        situation.activities || []
+                    ).filter(
+                        (a) =>
+                            a.contentieux && a.contentieux.id === referentielId
+                    )
+                    const indispoFiltred =
+                        this.humanResourceService.findAllIndisponibilities(
+                            hr,
+                            now
+                        )
+                    let reelEtp =
+                        situation.etp - sumBy(indispoFiltred, 'percent')
+                    if (reelEtp < 0) {
+                        reelEtp = 0
+                    }
+
+                    const etp =
+                        (reelEtp * sumBy(activitiesFiltred, 'percent')) / 100
+
+                    list[situation.category.id].etpt += etp
+                }
+            }
+            now.setDate(now.getDate() + 1)
+        } while (now.getTime() <= stop.getTime())
+
+        // format render
+        for (const property in list) {
+            list[property].etpt = list[property].etpt / nbDay
         }
 
         return list
