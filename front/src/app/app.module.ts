c1997d5056b70f8a48a9d44be29d61f1a3bd92ec
Jimmy <jimmy.chevallier@free.fr>
2022-05-20 17:12:23 +0200

    update time

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,22 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './routes/app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './routes/app-routing.module'
+import { AppComponent } from './app.component'
+import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- front/src/app/routes/average-etp/average-etp.page.ts
+++ front/src/app/routes/average-etp/average-etp.page.ts
@@ -1,51 +1,60 @@
-import { Component, OnDestroy } from '@angular/core';
-import { ContentieuReferentielInterface } from 'src/app/interfaces/contentieu-referentiel';
-import { MainClass } from 'src/app/libs/main-class';
-import { ContentieuxOptionsService } from 'src/app/services/contentieux-options/contentieux-options.service';
-import { HumanResourceService } from 'src/app/services/human-resource/human-resource.service';
-import { ReferentielService } from 'src/app/services/referentiel/referentiel.service';
-import { fixDecimal } from 'src/app/utils/numbers';
+import { Component, OnDestroy } from '@angular/core'
+import { ContentieuReferentielInterface } from 'src/app/interfaces/contentieu-referentiel'
+import { MainClass } from 'src/app/libs/main-class'
+import { ContentieuxOptionsService } from 'src/app/services/contentieux-options/contentieux-options.service'
+import { HumanResourceService } from 'src/app/services/human-resource/human-resource.service'
+import { ReferentielService } from 'src/app/services/referentiel/referentiel.service'
+import { fixDecimal } from 'src/app/utils/numbers'
 
 @Component({
-  templateUrl: './average-etp.page.html',
-  styleUrls: ['./average-etp.page.scss'],
+    templateUrl: './average-etp.page.html',
+    styleUrls: ['./average-etp.page.scss'],
 })
 export class AverageEtpPage extends MainClass implements OnDestroy {
-  referentiel: ContentieuReferentielInterface[] = [];
-  perUnity: string = 'hour';
+    referentiel: ContentieuReferentielInterface[] = []
+    perUnity: string = 'hour'
 
-  constructor(
-    private contentieuxOptionsService: ContentieuxOptionsService,
-    private humanResourceService: HumanResourceService, 
-    private referentielService: ReferentielService,
-  ) {
-    super();
+    constructor(
+        private contentieuxOptionsService: ContentieuxOptionsService,
+        private humanResourceService: HumanResourceService,
+        private referentielService: ReferentielService
+    ) {
+        super()
 
-    this.watch(
-      this.humanResourceService.contentieuxReferentiel.subscribe(
-        (ref) => {
-          this.referentiel = ref.filter(
-            (r) =>
-              this.referentielService.idsIndispo.indexOf(r.id) === -1 &&
-              this.referentielService.idsSoutien.indexOf(r.id) === -1
-          );
-        }
-      )
-    );
-  }
+        this.watch(
+            this.humanResourceService.contentieuxReferentiel.subscribe(
+                (ref) => {
+                    this.referentiel = ref.filter(
+                        (r) =>
+                            this.referentielService.idsIndispo.indexOf(r.id) ===
+                                -1 &&
+                            this.referentielService.idsSoutien.indexOf(r.id) ===
+                                -1
+                    )
+                }
+            )
+        )
+    }
 
-  ngOnDestroy() {
-    this.watcherDestroy();
-  }
+    ngOnDestroy() {
+        this.watcherDestroy()
+    }
 
-  onUpdateOptions(referentiel: ContentieuReferentielInterface, value: number) {
-    this.contentieuxOptionsService.updateOptions({
-      ...referentiel,
-      averageProcessingTime: !value ? null : (this.perUnity === 'hour' ? value : fixDecimal(8 / value))
-    });
-  }
+    onUpdateOptions(
+        referentiel: ContentieuReferentielInterface,
+        value: number
+    ) {
+        this.contentieuxOptionsService.updateOptions({
+            ...referentiel,
+            averageProcessingTime: !value
+                ? null
+                : this.perUnity === 'hour'
+                ? value
+                : fixDecimal(8 / value),
+        })
+    }
 
-  changeUnity(unit: string) {
-    this.perUnity = unit;
-  }
+    changeUnity(unit: string) {
+        this.perUnity = unit
+    }
 }
