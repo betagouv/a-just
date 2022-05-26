d5da664decb6a91d4f1ff7952c554f91a3ae54ff
Jimmy <jimmy.chevallier@free.fr>
2022-05-26 17:25:38 +0200

    open popup with error

--- front/src/app/components/popup/popup.component.ts
+++ front/src/app/components/popup/popup.component.ts
@@ -1,85 +1,98 @@
 import {
-  Component,
-  OnInit,
-  OnDestroy,
-  Input,
-  Output,
-  EventEmitter,
-  HostBinding,
-} from '@angular/core';
+    Component,
+    OnInit,
+    OnDestroy,
+    Input,
+    Output,
+    EventEmitter,
+    HostBinding,
+} from '@angular/core'
 
 export interface ActionsInterface {
-  type?: string;
-  red?: boolean;
-  fill?: boolean;
-  content: string;
-  id: string;
+    type?: string
+    red?: boolean
+    fill?: boolean
+    content: string
+    id: string
 }
 
 @Component({
-  selector: 'aj-popup',
-  templateUrl: './popup.component.html',
-  styleUrls: ['./popup.component.scss'],
+    selector: 'aj-popup',
+    templateUrl: './popup.component.html',
+    styleUrls: ['./popup.component.scss'],
 })
 export class PopupComponent implements OnInit, OnDestroy {
-  @HostBinding('class.is-mobile') isMobile = false;
-  @Input()
-  set visible(d) {
-    this._visible = d;
-  }
-  get visible() {
-    return this._visible;
-  }
-  @Input() title: string = '';
-  @Input() actions: ActionsInterface[] = [];
-  @Input() actionsLeft: ActionsInterface[] = [];
-  @Input() closeIcon = false;
-  @Input() minHeight: string = '';
-  @Output() selectedAction = new EventEmitter();
-  @Output() onClose = new EventEmitter();
-  _visible = true;
-  selectedOptions = 0;
-
-  constructor() {
-  }
-
-  ngOnInit() {
-    const element = document.getElementById(
-      'hubspot-messages-iframe-container'
-    );
-    if (element) {
-      element.style.visibility = 'hidden';
+    @HostBinding('class.is-mobile') isMobile = false
+    @Input()
+    set visible(d) {
+        this._visible = d
     }
-  }
-
-  ngOnDestroy() {
-    const element = document.getElementById(
-      'hubspot-messages-iframe-container'
-    );
-    if (element) {
-      element.style.visibility = 'visible';
+    get visible() {
+        return this._visible
     }
-  }
+    @Input() title: string = ''
+    @Input() actions: ActionsInterface[] = []
+    @Input() actionsLeft: ActionsInterface[] = []
+    @Input() closeIcon = false
+    @Input() minHeight: string = ''
+    @Input() removeShadow: string = ''
+    @Output() selectedAction = new EventEmitter()
+    @Output() onClose = new EventEmitter()
 
-  onSelectAction(action: any) {
-    this.selectedAction.emit({ ...action, optionValue: this.selectedOptions });
-  }
+    _visible = true
+    selectedOptions = 0
 
-  onChangeOption(val: any) {
-    this.selectedOptions = val;
-  }
+    constructor() {}
 
-  getInternetExplorerVersion() {
-    var rv = -1;
-    if (navigator.appName == 'Microsoft Internet Explorer') {
-      var ua = navigator.userAgent;
-      var re = new RegExp('MSIE ([0-9]{1,}[\\.0-9]{0,})');
-      if (re.exec(ua) != null) rv = parseFloat(RegExp.$1);
-    } else if (navigator.appName == 'Netscape') {
-      var ua = navigator.userAgent;
-      var re = new RegExp('Trident/.*rv:([0-9]{1,}[\\.0-9]{0,})');
-      if (re.exec(ua) != null) rv = parseFloat(RegExp.$1);
+    ngOnInit() {
+        console.log('1')
+        const element = document.getElementById(
+            'hubspot-messages-iframe-container'
+        )
+        console.log('2')
+
+        if (element) {
+            element.style.visibility = 'hidden'
+        }
+        console.log('3')
     }
-    return rv;
-  }
+
+    ngOnDestroy() {
+        const element = document.getElementById(
+            'hubspot-messages-iframe-container'
+        )
+        if (element) {
+            element.style.visibility = 'visible'
+        }
+        console.log('destroy')
+    }
+
+    onSelectAction(action: any) {
+        this.selectedAction.emit({
+            ...action,
+            optionValue: this.selectedOptions,
+        })
+    }
+
+    onChangeOption(val: any) {
+        this.selectedOptions = val
+    }
+
+    getInternetExplorerVersion() {
+        console.log('4')
+
+        var rv = -1
+        if (navigator.appName == 'Microsoft Internet Explorer') {
+            var ua = navigator.userAgent
+            var re = new RegExp('MSIE ([0-9]{1,}[\\.0-9]{0,})')
+            if (re.exec(ua) != null) rv = parseFloat(RegExp.$1)
+        } else if (navigator.appName == 'Netscape') {
+            var ua = navigator.userAgent
+            var re = new RegExp('Trident/.*rv:([0-9]{1,}[\\.0-9]{0,})')
+            if (re.exec(ua) != null) rv = parseFloat(RegExp.$1)
+        }
+        console.log('5')
+
+        return rv
+    }
 }
---
+++ front/src/app/routes/simulator/simulator.tree.ts
@@ -0,0 +1,224 @@
+const tree = [
+    {
+        label: 'ETPT',
+        toDefine: [
+            'Un nombre',
+            'Un différentiel par rapport à la valeur de la situation initiale (+ou- N ETPT)',
+        ],
+        toAjust: [
+            {
+                label: 'Entrées',
+                toDefine: [
+                    'Une valeur sur la période',
+                    'Une variation en % sur la période',
+                ],
+                toSimulate: {
+                    toDisplay: ['Entrées', 'ETPT', 'Temps moyen / dossier'],
+                    toCalculate: [
+                        {
+                            label: 'Sorties',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                        {
+                            label: 'Stock',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                        {
+                            label: 'Taux de couverture',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                        {
+                            label: 'DTES',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                    ],
+                },
+            },
+
+            {
+                label: 'Sorties',
+                toDefine: [
+                    'Une valeur sur la période',
+                    'Une variation en % sur la période',
+                ],
+                toSimulate: {
+                    toDisplay: [
+                        'Entrées',
+                        'Sortie moyennes mensuelles',
+                        'ETPT',
+                    ],
+                    toCalculate: [
+                        {
+                            label: 'Entrées',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                        {
+                            label: 'Sorties moyennes mensuelles',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                        {
+                            label: 'ETPT',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                    ],
+                },
+            },
+
+            {
+                label: 'Temps moyen / dossier',
+                toDefine: ['Un temps moyen par dossier'],
+                toSimulate: {
+                    toDisplay: ['Entrées', 'ETPT', 'Temps moyen / dossier'],
+                    toCalculate: [
+                        {
+                            label: 'Sorties',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                        {
+                            label: 'Stock',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                        {
+                            label: 'Taux de couverture',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                        {
+                            label: 'DTES',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                    ],
+                },
+            },
+
+            {
+                label: 'DTES',
+                toDefine: [
+                    'Une valeur',
+                    'Une variation en % par rapport à la valeur en début de période',
+                ],
+                toSimulate: {
+                    toDisplay: ['Entrées', 'ETPT', 'DTES'],
+                    toCalculate: [
+                        {
+                            label: 'Sorties',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                        {
+                            label: 'Stock',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                        {
+                            label: 'Taux de couverture',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                        {
+                            label: 'Temps moyen / dossier',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                    ],
+                },
+            },
+
+            {
+                label: 'Stock',
+                toDefine: [
+                    'Une valeur',
+                    'Une variation en % par rapport à la valeur en début de période',
+                ],
+                toSimulate: {
+                    toDisplay: [
+                        'Entrées',
+                        'Stock',
+                        'ETPT',
+                        'Temps moyen / dossier',
+                    ],
+                    toCalculate: [
+                        {
+                            label: 'Sorties',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                        {
+                            label: 'Taux de couverture',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                        {
+                            label: 'DTES',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                    ],
+                },
+            },
+
+            {
+                label: 'Taux de couverture',
+                toDefine: ['Une valeur en %'],
+                toSimulate: {
+                    toDisplay: ['Entrées', 'ETPT', 'Taux de couverture'],
+                    toCalculate: [
+                        {
+                            label: 'Sorties',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                        {
+                            label: 'Stock',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                        {
+                            label: 'DTES',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                        {
+                            label: 'Temps moyen / dossier',
+                            fct: (param: number) => {
+                                return param
+                            },
+                        },
+                    ],
+                },
+            },
+        ],
+    },
+]
+export { tree }
