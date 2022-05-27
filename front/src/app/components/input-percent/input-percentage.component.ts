5e8c64661b06a697e60cf981e2fa4ac3963054e3
Jimmy <jimmy.chevallier@free.fr>
2022-05-27 08:20:49 +0200

    popup return value

---
+++ front/src/app/components/input-percent/input-percentage.component.ts
@@ -0,0 +1,58 @@
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnChanges,
+    OnInit,
+    Output,
+    SimpleChanges,
+} from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms'
+
+@Component({
+    selector: 'aj-input-percentage',
+    templateUrl: './input-percentage.component.html',
+    styleUrls: ['./input-percentage.component.scss'],
+})
+export class InputPercentageComponent implements OnInit, OnChanges {
+    @Input() referenceValue: number = 0
+    @Input() reset: boolean = false
+    @Output() valueChange = new EventEmitter()
+
+    valueForm = new FormGroup({
+        percentage: new FormControl(''),
+    })
+    constructor() {
+        this.valueForm.controls.percentage.valueChanges.subscribe((value) => {
+            console.log(value)
+
+            this.valueChange.emit(
+                this.returnSomething(
+                    this.valueForm.controls['percentage'].value
+                )
+            )
+        })
+    }
+
+    ngOnInit(): void {}
+
+    ngOnChanges(changes: SimpleChanges) {
+        console.log('onchange', this.reset)
+        if (this.reset === true) {
+            this.valueForm.controls['percentage'].setValue('')
+            this.reset = false
+        }
+    }
+    print(x: any) {
+        console.log(x)
+    }
+
+    returnSomething(x: any): string {
+        return x
+            ? String(
+                  this.referenceValue -
+                      ((-parseInt(x) * 1) / 100) * this.referenceValue
+              )
+            : ''
+    }
+}
