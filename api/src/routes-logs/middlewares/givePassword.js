662f5035ff1e8aac30ec2d3cb66e3faa011155cf
François-Xavier Montigny <fxmontigny@users.noreply.github.com>
2022-05-31 12:58:49 +0200

    Feature/hr control date (#116)
    * change date start and date end
    
    * control max indispo
    
    * accept pwd into params url
    
    * change date end control

--- api/src/routes-logs/middlewares/givePassword.js
+++ api/src/routes-logs/middlewares/givePassword.js
@@ -2,8 +2,9 @@ import config from 'config'
 
 module.exports = async function async (ctx, next) {
   const token = ctx.get('Authorization')
-  if (ctx.request.url.startsWith('/logs/') && token !== `Bearer ${config.logsPassword}`) {
-    console.log('token', token)
+  const params = new URLSearchParams(ctx.req._parsedUrl.query)
+  const password = params.get('pwd')
+  if (ctx.request.url.startsWith('/logs/') && !(token === `Bearer ${config.logsPassword}` || password === config.logsPassword)) {
     ctx.throw(401, ctx.state.__('Accès non autorisé'))
   } else {
     return next()
