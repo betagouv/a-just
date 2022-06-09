c412403261aa096a38970af61c00221461c71348
Jimmy <jimmy.chevallier@free.fr>
2022-06-09 21:50:37 +0200

    maj calcul pour testing

--- front/src/app/interfaces/simulator.ts
+++ front/src/app/interfaces/simulator.ts
@@ -1,18 +1,19 @@
-import { etpAffectedInterface } from './calculator';
+import { etpAffectedInterface } from './calculator'
 
 export interface SimulatorInterface {
-  totalIn: number | null;
-  totalOut: number | null;
-  lastStock: number | null;
-  etpMag: number | null;
-  etpFon: number | null;
-  etpCont: number | null;
-  realCoverage: number | null;
-  realDTESInMonths: number | null;
-  realTimePerCase: number | null;
-  calculateCoverage: number | null;
-  calculateDTESInMonths: number | null;
-  calculateTimePerCase: number | null;
-  nbMonth: number;
-  etpAffected: etpAffectedInterface[] | null;
+    totalIn: number | null
+    totalOut: number | null
+    lastStock: number | null
+    etpMag: number | null
+    etpFon: number | null
+    etpCont: number | null
+    realCoverage: number | null
+    realDTESInMonths: number | null
+    realTimePerCase: number | null
+    calculateCoverage: number | null
+    calculateDTESInMonths: number | null
+    calculateTimePerCase: number | null
+    nbMonth: number
+    etpAffected: etpAffectedInterface[] | null
+    etpToCompute?: number | null
 }
