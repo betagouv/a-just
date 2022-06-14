719c7f78dc3bcdd1f505923c20d7fb9d1d86df2e
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-06-14 11:19:33 +0200

    fix create new contentieux

--- api/src/models/TableHrbackups.js
+++ api/src/models/TableHrbackups.js
@@ -203,5 +203,26 @@ export default (sequelizeInstance, Model) => {
     return list
   }
 
+  Model.haveAccess = async (id, userId) => {
+    const hr = await Model.findOne({
+      where: {
+        id,
+      },
+      attributes: ['id'],
+      model: Model.models.HRBackups,
+      include: [{
+        attributes: ['id'],
+        model: Model.models.UserVentilations,
+        required: true,
+        where: {
+          user_id: userId,
+        },
+      }],
+      raw: true,
+    })
+
+    return hr ? true : false
+  }
+
   return Model
 }
