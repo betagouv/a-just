6e1569199fc7fe8cfae4a9c49931ab827f4f14c2
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-06-14 10:43:41 +0200

    on remove average time backup

--- front/src/app/services/contentieux-options/contentieux-options.service.ts
+++ front/src/app/services/contentieux-options/contentieux-options.service.ts
@@ -115,6 +115,7 @@ export class ContentieuxOptionsService extends MainClass {
       return this.serverService
         .delete(`contentieux-options/remove-backup/${this.backupId.getValue()}`)
         .then(() => {
+          this.autoReloadData = true
           this.backupId.next(null)
         })
     }
