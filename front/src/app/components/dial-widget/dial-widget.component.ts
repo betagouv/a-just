e2e3cf02ff2d09a91599f118c40e7526f16539fc
Jimmy <jimmy.chevallier@free.fr>
2022-06-16 18:47:08 +0200

    update widgets

--- front/src/app/components/dial-widget/dial-widget.component.ts
+++ front/src/app/components/dial-widget/dial-widget.component.ts
@@ -1,9 +1,19 @@
+import { animate, style, transition, trigger } from '@angular/animations'
 import { Component, Input, OnInit } from '@angular/core'
 
 @Component({
   selector: 'aj-dial-widget',
   templateUrl: './dial-widget.component.html',
   styleUrls: ['./dial-widget.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(500, style({ opacity: 1 })),
+      ]),
+      transition(':leave', [animate(500, style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class DialWidgetComponent implements OnInit {
   @Input() dateStart: string = ''
@@ -15,4 +25,8 @@ export class DialWidgetComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  toNumber(str: string): number {
+    return parseInt(str)
+  }
 }
