e7b3ccd59a371a6e7cfd4898363dfebba9ca8fac
Jimmy <jimmy.chevallier@free.fr>
2022-06-21 21:03:26 +0200

    test export not working

---
+++ front/src/app/routes/simulator/simulator.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SimulatorComponent } from './simulator.component';
+
+describe('SimulatorComponent', () => {
+  let component: SimulatorComponent;
+  let fixture: ComponentFixture<SimulatorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SimulatorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SimulatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
