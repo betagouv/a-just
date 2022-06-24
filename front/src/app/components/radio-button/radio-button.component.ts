5542a50efebaa27e307681ef225c1ac81e5f1c17
Jimmy <jimmy.chevallier@free.fr>
2022-06-24 16:48:47 +0200

    wip

--- front/src/app/components/radio-button/radio-button.component.ts
+++ front/src/app/components/radio-button/radio-button.component.ts
@@ -1,22 +1,47 @@
-import { Component, OnInit } from '@angular/core'
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core'
 
 @Component({
   selector: 'aj-radio-button',
   templateUrl: './radio-button.component.html',
   styleUrls: ['./radio-button.component.scss'],
 })
-export class RadioButtonComponent implements OnInit {
+export class RadioButtonComponent implements OnChanges, OnInit {
+  @Input() switchColor: string = ''
+  @Input() bgColor: string = 'white'
+
   o = {
     label: 'Cheap',
     checked: true,
   }
 
-  constructor() {}
+  elementRef: HTMLElement | undefined
 
-  ngOnInit(): void {}
-
-  clicked() {
-    this.o.checked = !this.o.checked
-    console.log(this.o)
+  constructor(element: ElementRef<HTMLElement>) {
+    this.elementRef = element.nativeElement
   }
+
+  ngOnInit(): void {
+    const checkElement = (
+      this.elementRef!.getElementsByClassName(
+        'check'
+      ) as HTMLCollectionOf<HTMLElement>
+    )[0]
+
+    checkElement.style.setProperty('--boxBackColor', this.bgColor)
+
+    const dotElement = (
+      this.elementRef!.getElementsByClassName(
+        'check__indicator'
+      ) as HTMLCollectionOf<HTMLElement>
+    )[0]
+    dotElement.style.setProperty('--boxAfterBackColor', this.switchColor)
+  }
+  ngOnChanges(changes: SimpleChanges): void {}
 }
