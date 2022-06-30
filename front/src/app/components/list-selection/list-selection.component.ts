69a1d92589f30e1e43cffd2e3243eb84c78880bb
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-06-30 10:30:37 +0200

    WIP - filter panel

--- front/src/app/components/list-selection/list-selection.component.ts
+++ front/src/app/components/list-selection/list-selection.component.ts
@@ -1,4 +1,13 @@
-import { Component, EventEmitter, Input, OnInit, Output, OnChanges, SimpleChanges } from '@angular/core'
+import { HostListener } from '@angular/core'
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core'
 import { ItemInterface } from 'src/app/interfaces/item'
 
 @Component({
@@ -11,23 +20,32 @@ export class ListSelectionComponent implements OnInit, OnChanges {
   @Input() icon: string = ''
   @Input() list: ItemInterface[] = []
   @Input() value: string | null = null
-  @Output() itemChanged: EventEmitter<string | null> = new EventEmitter<
+  @Output() valueChanged: EventEmitter<string | null> = new EventEmitter<
     string | null
   >()
   itemSelected: ItemInterface | null = null
+  onOpenDropdown: boolean = false
+
+  @HostListener('document:click', ['$event'])
+  onClick() {
+    this.onOpenDropdown = false
+  }
 
   constructor() {}
 
   ngOnInit() {}
 
   ngOnChanges(changes: SimpleChanges) {
-    if(changes.value || changes.list) {
-      this.itemSelected = this.value ? (this.list.find(l => l.id === this.value) || null) : null
+    if (changes.value || changes.list) {
+      this.itemSelected = this.value
+        ? this.list.find((l) => l.id === this.value) || null
+        : null
     }
   }
 
   onSelect(item: string | null) {
+    this.onOpenDropdown = false
     this.value = item
-    this.itemChanged.next(item)
+    this.valueChanged.next(item)
   }
 }
