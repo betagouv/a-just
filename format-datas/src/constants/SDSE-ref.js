c1fbcb9853495c1e895ed3ff3517c34e9ae94886
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-06-30 10:30:27 +0200

    add nanterre

--- format-datas/src/constants/SDSE-ref.js
+++ format-datas/src/constants/SDSE-ref.js
@@ -6,4 +6,12 @@ export const TAG_JURIDICTION_VALUE_COLUMN_NAME = 'NOM_COMPTEUR'
 export const I_ELST_LIST = {
   '00100167': 'TJ_LYON',
   '00963077': 'TJ_LYON',
+  '00100209': 'TJ_NANTERRE', // Nanterre
+  '00962960': 'TJ_NANTERRE', // Antony
+  '00962962': 'TJ_NANTERRE', // Asnières-sur-seine
+  '00962973': 'TJ_NANTERRE', // Boulogne-Billancourt
+  '00962986': 'TJ_NANTERRE', // Colombes
+  '00962988': 'TJ_NANTERRE', // Courbevoie
+  '00963050': 'TJ_NANTERRE', // Puteaux
+  '00963073': 'TJ_NANTERRE', // Vanves
 }
