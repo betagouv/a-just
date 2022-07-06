690f3890850aeb6d309738bbaa9874976a1725f6
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-07-06 17:26:40 +0200

    WIP radio button

--- front/src/app/components/radio-button/radio-button.component.ts
+++ front/src/app/components/radio-button/radio-button.component.ts
@@ -2,6 +2,8 @@ import {
   Component,
   ElementRef,
   EventEmitter,
+  HostBinding,
+  HostListener,
   Input,
   OnChanges,
   OnInit,
@@ -16,40 +18,29 @@ import {
 })
 export class RadioButtonComponent implements OnChanges, OnInit {
   @Input() title: string = ''
-  @Input() switchColor: string = ''
-  @Input() bgColor: string = 'white'
+  @Input() switchColor: string = '#7befb2'
+  @Input() @HostBinding('style.background-color') bgColor: string = 'white'
+  @Input() width: number = 25
+  @Input() height: number = 15
   @Output() valueChange = new EventEmitter()
   @Input() value: boolean = true
-
+  @HostListener('click') 
+  onClick() {
+    this.o.checked = !this.o.checked
+    this.valueChange.emit(this.o)
+  }
   o = {
     label: '',
     checked: true,
   }
 
-  elementRef: HTMLElement | undefined
-
-  constructor(element: ElementRef<HTMLElement>) {
-    this.elementRef = element.nativeElement
-  }
-
   ngOnInit(): void {
     this.o.label = this.title
-
-    const dotElement = (
-      this.elementRef!.getElementsByClassName(
-        'check__indicator'
-      ) as HTMLCollectionOf<HTMLElement>
-    )[0]
-    dotElement.style.setProperty('--boxAfterBackColor', this.switchColor)
 
     this.valueChange.emit(this.o)
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.o.checked = changes.value.currentValue
-  }
-
-  clicked() {
-    this.valueChange.emit(this.o)
   }
 }
