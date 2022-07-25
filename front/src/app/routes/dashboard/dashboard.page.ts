443280faae84e3df81fc20782a5bcebe54f5d93a
Jimmy <jimmy.chevallier@free.fr>
2022-07-25 16:54:35 +0200

    update loader excel file

--- front/src/app/routes/dashboard/dashboard.page.ts
+++ front/src/app/routes/dashboard/dashboard.page.ts
@@ -1,12 +1,35 @@
-import { Component, OnInit } from '@angular/core'
+import { AfterViewInit, Component, OnInit } from '@angular/core'
+import { MainClass } from 'src/app/libs/main-class'
 import { ExcelService } from 'src/app/services/excel/excel.service'
 
 @Component({
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
 })
-export class DashboardPage {
-  constructor(private excelService: ExcelService) {}
+export class DashboardPage extends MainClass implements AfterViewInit {
+  ngAfterViewInit(): void {
+    this.watch(
+      this.excelService.loading.subscribe((value: boolean) => {
+        if (value === true) {
+          ;(
+            document.getElementsByTagName('aj-wrapper')[0] as HTMLElement
+          ).style.cursor = 'wait'
+
+          document.getElementById('export-excel-button')!.style.cursor = 'wait'
+        } else {
+          ;(
+            document.getElementsByTagName('aj-wrapper')[0] as HTMLElement
+          ).style.cursor = 'auto'
+
+          document.getElementById('export-excel-button')!.style.cursor =
+            'pointer'
+        }
+      })
+    )
+  }
+  constructor(private excelService: ExcelService) {
+    super()
+  }
   dateStart: Date | null = null
   dateStop: Date | null = null
   today = new Date()
