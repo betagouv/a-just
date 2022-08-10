52ef22bde1ed8a3a23dd4ba215a5fe2f3f216c40
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-08-10 18:08:16 +0200

    WIP reworking activities

--- front/src/app/components/wrapper/wrapper.component.ts
+++ front/src/app/components/wrapper/wrapper.component.ts
@@ -31,6 +31,7 @@ export class WrapperComponent extends MainClass implements OnInit, OnDestroy {
   @Input() titleTemplate: TemplateRef<any> | undefined
   @Input() title: string = ''
   @Input() subtitle: string = ''
+  @Input() subtitleTemplate: TemplateRef<any> | undefined
   @Input() backUrl: string = ''
   @Input() backAnchor: string | undefined
   @Input() alignLeft: boolean | undefined
--- front/src/app/interfaces/activity.ts
+++ front/src/app/interfaces/activity.ts
@@ -3,7 +3,10 @@ import { ContentieuReferentielInterface } from "./contentieu-referentiel";
 export interface ActivityInterface {
 	periode: Date;
 	entrees: number;
+	originalEntrees?: number;
 	sorties: number;
+	originalSorties?: number;
 	stock: number;
+	originalStock?: number;
 	contentieux: ContentieuReferentielInterface;
 }
