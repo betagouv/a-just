f5ec20b8b2029a0b55e4ef6d10195119a78bd7fa
MrJimmyChevallier <jimmy.chevallier@free.fr>
2022-08-18 19:06:10 +0200

    debug simu

--- front/src/app/services/simulator/simulator.service.ts
+++ front/src/app/services/simulator/simulator.service.ts
@@ -187,9 +187,9 @@ export class SimulatorService extends MainClass {
         )
         console.log('inside', lastActivities, nbOfMonth)
       } while (
-        //lastActivities.length === 0 &&
-        (lastActivities![0]!.stock === null ||
-          lastActivities![0]!.stock !== 0) &&
+        (lastActivities.length === 0 ||
+          lastActivities![0]!.stock === null ||
+          lastActivities![0]!.stock === 0) &&
         nbOfMonth != -12
       )
 
