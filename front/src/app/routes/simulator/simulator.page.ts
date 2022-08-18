8509a7b7c0459ac7397bb88ddb6a31130244e102
MrJimmyChevallier <jimmy.chevallier@free.fr>
2022-08-18 02:55:11 +0200

    Revert "update simulator greff subtotal"
    This reverts commit 0caa53e60fae1f158692dd26e48077aef029a575.

--- front/src/app/routes/simulator/simulator.page.ts
+++ front/src/app/routes/simulator/simulator.page.ts
@@ -807,8 +807,6 @@ export class SimulatorPage extends MainClass implements OnDestroy, OnInit {
       totalOut: null,
       lastStock: null,
       etpMag: null,
-      etpFon: null,
-      etpCont: null,
       realTimePerCase: null,
       realDTESInMonths: null,
       realCoverage: null,
