fcf06b70d545f6846a4dbc6baef95e7d2effd115
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-08-18 10:50:55 +0200

    WIP calcultator to back

---
+++ api/src/routes-api/RouteCalculator.js
@@ -0,0 +1,36 @@
+import Route, { Access } from './Route'
+import { Types } from '../utils/types'
+import { USER_REMOVE_HR } from '../constants/log-codes'
+import { preformatHumanResources } from '../utils/ventilator'
+import { getCategoryColor } from '../constants/categories'
+import { sumBy } from 'lodash'
+import { emptyCalulatorValues } from '../constants/calculator'
+
+export default class RouteCalculator extends Route {
+  constructor (params) {
+    super({ ...params, model: 'HumanResources' })
+  }
+
+  @Route.Post({
+    bodyType: Types.object().keys({
+      backupId: Types.number().required(),
+      dateStart: Types.date().required(),
+      dateStop: Types.date().required(),
+      contentieuxIds: Types.array().required(),
+      optionBackupId: Types.number().required(),
+    }),
+    accesses: [Access.canVewHR],
+  })
+  async filterList (ctx) {
+    let { backupId, dateStart, dateStop, contentieuxIds, optionBackupId } = this.body(ctx)
+
+    if(!await this.models.HRBackups.haveAccess(backupId, ctx.state.user.id)) {
+      ctx.throw(401, 'Vous n\'avez pas accès à cette juridiction !')
+    }
+
+    const list = emptyCalulatorValues(await this.models.ContentieuxReferentiels.getReferentiels())
+    let 
+
+    this.sendOk(ctx, list)
+  }
+}
