51c05d53b2922e284e49ac427d09226929724ee3
MrJimmyChevallier <jimmy.chevallier@free.fr>
2022-09-13 19:10:37 +0200

    update calculator display date

--- front/src/app/components/referentiel-calculator/referentiel-calculator.component.ts
+++ front/src/app/components/referentiel-calculator/referentiel-calculator.component.ts
@@ -28,10 +28,21 @@ export class ReferentielCalculatorComponent extends MainClass {
   decimalToStringDate(decimal: number | null) {
     if (decimal != null) {
       decimal = fixDecimal(decimal)
+
       const strArray = String(decimal).split('.')
-      let minute = strArray[1]
-        ? String(Math.ceil((1 / 100) * +strArray[1] * 60))
-        : '00'
+
+      let extractedMinute = strArray[1] ? strArray[1] : 0
+
+      let minute =
+        String(extractedMinute).length === 1
+          ? extractedMinute + '0'
+          : extractedMinute
+
+      minute =
+        extractedMinute !== 0
+          ? String(Math.ceil((1 / 100) * +Number(minute) * 60))
+          : '00'
+
       minute = minute.length === 1 ? '0' + minute : minute
 
       return strArray[0] + 'h' + minute
