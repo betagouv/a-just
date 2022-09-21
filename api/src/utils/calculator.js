2250d98a1b2f786c9879c52cc599127703b7863a
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-09-21 16:04:26 +0200

    fix N/A

--- api/src/utils/calculator.js
+++ api/src/utils/calculator.js
@@ -113,37 +113,64 @@ const getActivityValues = (
   categories,
   optionsBackups
 ) => {
-  console.log('getActivityValues' + referentielId)
   activities = activities.filter(
     (a) =>
       month(a.periode).getTime() >= month(dateStart).getTime() &&
       month(a.periode).getTime() <= month(dateStop).getTime()
   )
-  const totalIn = (activities || []).some(e => e.entrees !== null && e.entrees !== 0) ? Math.floor(sumBy(activities, 'entrees') / nbMonth) : null
-  const totalOut = (activities || []).some(e => e.sorties !== null && e.entrees !== 0) ? Math.floor(sumBy(activities, 'sorties') / nbMonth) : null
+
+  const totalIn =
+    (activities || []).filter((e) => e.entrees !== null).length !== 0
+      ? Math.floor(sumBy(activities, 'entrees') / nbMonth)
+      : null
+
+  const totalOut =
+    (activities || []).filter((e) => e.sorties !== null).length !== 0
+      ? Math.floor(sumBy(activities, 'sorties') / nbMonth)
+      : null
   let lastStock = null
   if (activities.length) {
     const lastActivities = activities[activities.length - 1]
-    if (lastActivities.stock !== null && isSameMonthAndYear(lastActivities.periode, dateStop)) {
+    if (
+      lastActivities.stock !== null &&
+      isSameMonthAndYear(lastActivities.periode, dateStop)
+    ) {
       lastStock = lastActivities.stock
     }
   }
 
   const realCoverage = fixDecimal(totalOut / totalIn)
-  const realDTESInMonths = lastStock !== null ? fixDecimal(lastStock / totalOut) : null
+  const realDTESInMonths =
+    lastStock !== null ? fixDecimal(lastStock / totalOut) : null
 
-  const etpAffected = getHRPositions(hr, categories, referentielId, dateStart, dateStop)
-  const etpMag = etpAffected.length > 0 ? fixDecimal(etpAffected[0].totalEtp, 1000) : 0
-  const etpFon = etpAffected.length > 1 ? fixDecimal(etpAffected[1].totalEtp, 1000) : 0
-  const etpCont = etpAffected.length > 2 ? fixDecimal(etpAffected[2].totalEtp, 1000) : 0
+  const etpAffected = getHRPositions(
+    hr,
+    categories,
+    referentielId,
+    dateStart,
+    dateStop
+  )
+  const etpMag =
+    etpAffected.length > 0 ? fixDecimal(etpAffected[0].totalEtp, 1000) : 0
+  const etpFon =
+    etpAffected.length > 1 ? fixDecimal(etpAffected[1].totalEtp, 1000) : 0
+  const etpCont =
+    etpAffected.length > 2 ? fixDecimal(etpAffected[2].totalEtp, 1000) : 0
 
   // Temps moyens par dossier observé = (nb heures travaillées par mois) / (sorties moyennes par mois / etpt sur la periode)
   const realTimePerCase = fixDecimal(
-    ((config.nbDaysByMagistrat / 12) * config.nbHoursPerDay) / (totalOut / etpMag)
+    ((config.nbDaysByMagistrat / 12) * config.nbHoursPerDay) /
+      (totalOut / etpMag)
   )
 
   return {
-    ...calculateActivities(referentielId, totalIn, lastStock, etpMag, optionsBackups),
+    ...calculateActivities(
+      referentielId,
+      totalIn,
+      lastStock,
+      etpMag,
+      optionsBackups
+    ),
     totalIn,
     totalOut,
     lastStock,
@@ -176,7 +203,13 @@ const getHRPositions = (hr, categories, referentielId, dateStart, dateStop) => {
         return activities.some((s) => s.contentieux.id === referentielId)
       })
     ) {
-      const etptAll = getHRVentilation(hr[i], referentielId, categories, dateStart, dateStop)
+      const etptAll = getHRVentilation(
+        hr[i],
+        referentielId,
+        categories,
+        dateStart,
+        dateStop
+      )
 
       Object.values(etptAll).map((c) => {
         if (c.etpt) {
@@ -199,7 +232,13 @@ const getHRPositions = (hr, categories, referentielId, dateStart, dateStop) => {
   return sortBy(list, 'rank')
 }
 
-export const getHRVentilation = (hr, referentielId, categories, dateStart, dateStop) => {
+export const getHRVentilation = (
+  hr,
+  referentielId,
+  categories,
+  dateStart,
+  dateStop
+) => {
   const list = new Object()
   categories.map((c) => {
     list[c.id] = new Object({
@@ -218,11 +257,8 @@ export const getHRVentilation = (hr, referentielId, categories, dateStart, dateS
     // only working day
     if (workingDay(now)) {
       nbDay++
-      const { etp, situation, indispoFiltred, nextDeltaDate } = getEtpByDateAndPerson(
-        referentielId,
-        now,
-        hr
-      )
+      const { etp, situation, indispoFiltred, nextDeltaDate } =
+        getEtpByDateAndPerson(referentielId, now, hr)
       if (nextDeltaDate) {
         nextDateFinded = new Date(nextDeltaDate)
       }
@@ -236,7 +272,8 @@ export const getHRVentilation = (hr, referentielId, categories, dateStart, dateS
       const sumByInd = sumBy(indispoFiltred, 'percent')
       if (sumByInd !== 0) {
         indispoFiltred.map((c) => {
-          if (c.contentieux.id === referentielId) list[situation.category.id].indispo += c.percent
+          if (c.contentieux.id === referentielId)
+            list[situation.category.id].indispo += c.percent
         })
       }
     }
@@ -274,25 +311,36 @@ export const getHRVentilation = (hr, referentielId, categories, dateStart, dateS
   return list
 }
 
-const calculateActivities = (referentielId, totalIn, lastStock, etpAffected, optionsBackups) => {
+const calculateActivities = (
+  referentielId,
+  totalIn,
+  lastStock,
+  etpAffected,
+  optionsBackups
+) => {
   let calculateTimePerCase = null
   let calculateOut = null
   let calculateCoverage = null
   let calculateDTESInMonths = null
 
-  const findIndexOption = optionsBackups.findIndex((o) => o.contentieux.id === referentielId)
+  const findIndexOption = optionsBackups.findIndex(
+    (o) => o.contentieux.id === referentielId
+  )
 
   if (findIndexOption !== -1) {
-    calculateTimePerCase = optionsBackups[findIndexOption].averageProcessingTime
+    calculateTimePerCase =
+      optionsBackups[findIndexOption].averageProcessingTime
   }
 
   if (calculateTimePerCase) {
     calculateOut = Math.floor(
-      (((etpAffected * config.nbHoursPerDay) / calculateTimePerCase) * config.nbDaysByMagistrat) /
+      (((etpAffected * config.nbHoursPerDay) / calculateTimePerCase) *
+        config.nbDaysByMagistrat) /
         12
     )
     calculateCoverage = fixDecimal(calculateOut / (totalIn || 0))
-    calculateDTESInMonths = lastStock === null ? null : fixDecimal(lastStock / calculateOut)
+    calculateDTESInMonths =
+      lastStock === null ? null : fixDecimal(lastStock / calculateOut)
   } else {
     calculateOut = null
     calculateCoverage = null
--- api/src/utils/date.js
+++ api/src/utils/date.js
@@ -11,162 +11,162 @@ export const MONTH_LABEL = [
   'octobre',
   'novembre',
   'décembre',
-];
+]
 
 export const convertHourMinutesToTimestamps = (hour, minute) => {
-  return (hour || 1) * 60 * 60000 + (minute || 1) * 60000;
-};
+  return (hour || 1) * 60 * 60000 + (minute || 1) * 60000
+}
 
 export const dayName = (date) => {
   if (typeof date === 'string') {
-    date = new Date(date);
+    date = new Date(date)
   }
 
   switch (date.getDay()) {
-    case 1:
-      return 'lundi';
-    case 2:
-      return 'mardi';
-    case 3:
-      return 'mercredi';
-    case 4:
-      return 'jeudi';
-    case 5:
-      return 'vendredi';
-    case 6:
-      return 'samedi';
-    case 0:
-      return 'dimanche';
+  case 1:
+    return 'lundi'
+  case 2:
+    return 'mardi'
+  case 3:
+    return 'mercredi'
+  case 4:
+    return 'jeudi'
+  case 5:
+    return 'vendredi'
+  case 6:
+    return 'samedi'
+  case 0:
+    return 'dimanche'
   }
-};
+}
 
 export const monthName = (date) => {
   if (typeof date === 'string') {
-    date = new Date(date);
+    date = new Date(date)
   }
 
   switch (date.getMonth()) {
-    case 0:
-      return 'janvier';
-    case 1:
-      return 'février';
-    case 2:
-      return 'mars';
-    case 3:
-      return 'avril';
-    case 4:
-      return 'mai';
-    case 5:
-      return 'juin';
-    case 6:
-      return 'juillet';
-    case 7:
-      return 'aout';
-    case 8:
-      return 'septembre';
-    case 9:
-      return 'octobre';
-    case 10:
-      return 'novembre';
-    case 11:
-      return 'décembre';
+  case 0:
+    return 'janvier'
+  case 1:
+    return 'février'
+  case 2:
+    return 'mars'
+  case 3:
+    return 'avril'
+  case 4:
+    return 'mai'
+  case 5:
+    return 'juin'
+  case 6:
+    return 'juillet'
+  case 7:
+    return 'aout'
+  case 8:
+    return 'septembre'
+  case 9:
+    return 'octobre'
+  case 10:
+    return 'novembre'
+  case 11:
+    return 'décembre'
   }
-};
-
-export function today(date = new Date()) {
-  const now = new Date(date);
-  return new Date(now.getFullYear(), now.getMonth(), now.getDate());
 }
 
-export function month(date = new Date(), monthToAdd, lastDay) {
-  const now = new Date(date);
-  now.setUTCHours(0, 0, 0, 0);
+export function today (date = new Date()) {
+  const now = new Date(date)
+  return new Date(now.getFullYear(), now.getMonth(), now.getDate())
+}
+
+export function month (date = new Date(), monthToAdd, lastDay) {
+  const now = new Date(date)
+  now.setUTCHours(0, 0, 0, 0)
 
   if (monthToAdd && monthToAdd !== 0) {
-    now.setDate(1);
-    now.setMonth(now.getMonth() + monthToAdd);
+    now.setDate(1)
+    now.setMonth(now.getMonth() + monthToAdd)
   }
   if (lastDay) {
-    now.setMonth(now.getMonth() + 1);
-    now.setHours(-1);
-    return now;
+    now.setMonth(now.getMonth() + 1)
+    now.setHours(-1)
+    return now
   } else {
-    return generalizeTimeZone(new Date(now));
+    return new Date(date.getFullYear(), date.getMonth())
   }
 }
 
-export function generalizeTimeZone(date) {
-  if (date === undefined) return undefined;
-  else date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+export function generalizeTimeZone (date) {
+  if (date === undefined) return undefined
+  else date.setMinutes(date.getMinutes() - date.getTimezoneOffset())
 
-  return date;
+  return date
 }
 
-export function getNbMonth(dateStart, dateStop) {
-  let totalMonth = 0;
+export function getNbMonth (dateStart, dateStop) {
+  let totalMonth = 0
 
-  const now = new Date(dateStart);
+  const now = new Date(dateStart)
   do {
-    totalMonth++;
-    now.setMonth(now.getMonth() + 1);
-  } while (now.getTime() <= dateStop.getTime());
+    totalMonth++
+    now.setMonth(now.getMonth() + 1)
+  } while (now.getTime() <= dateStop.getTime())
 
   if (totalMonth <= 0) {
-    totalMonth = 1;
+    totalMonth = 1
   }
 
-  return totalMonth;
+  return totalMonth
 }
 
-export function isSameMonthAndYear(date1, date2) {
-  date1 = new Date(date1);
-  date2 = new Date(date2);
+export function isSameMonthAndYear (date1, date2) {
+  date1 = new Date(date1)
+  date2 = new Date(date2)
 
-  return date1.getMonth() === date2.getMonth() && date1.getFullYear() === date2.getFullYear();
+  return date1.getMonth() === date2.getMonth() && date1.getFullYear() === date2.getFullYear()
 }
 
-export function workingDay(date) {
-  return [1, 2, 3, 4, 5].indexOf(date.getDay()) !== -1;
+export function workingDay (date) {
+  return [1, 2, 3, 4, 5].indexOf(date.getDay()) !== -1
 }
 
-export function isDateBiggerThan(firstDate, secondDate) {
-  firstDate = new Date(firstDate);
-  secondDate = new Date(secondDate);
+export function isDateBiggerThan (firstDate, secondDate) {
+  firstDate = new Date(firstDate)
+  secondDate = new Date(secondDate)
 
-  return firstDate.getTime() >= secondDate.getTime();
+  return firstDate.getTime() >= secondDate.getTime()
 }
 
-export function getRangeOfMonthsAsObject(startDate, endDate, asObject = false) {
-  const dates = new Array();
-  const dateCounter = new Date(startDate);
-  let monthlyL = {};
+export function getRangeOfMonthsAsObject (startDate, endDate, asObject = false) {
+  const dates = new Array()
+  const dateCounter = new Date(startDate)
+  let monthlyL = {}
 
-  dateCounter.setDate(1);
+  dateCounter.setDate(1)
 
   while (dateCounter < endDate) {
     if (getShortMonthString(dateCounter) === 'Janv.')
       dates.push(
         `${getShortMonthString(dateCounter) + ' ' + dateCounter.getFullYear().toString().slice(-2)}`
-      );
-    else dates.push(`${getShortMonthString(dateCounter)}`);
+      )
+    else dates.push(`${getShortMonthString(dateCounter)}`)
 
-    const str = getShortMonthString(dateCounter) + dateCounter.getFullYear().toString().slice(-2);
+    const str = getShortMonthString(dateCounter) + dateCounter.getFullYear().toString().slice(-2)
     if (asObject) {
-      monthlyL[str] = { ...{} };
+      monthlyL[str] = { ...{} }
     }
-    dateCounter.setMonth(dateCounter.getMonth() + 1);
+    dateCounter.setMonth(dateCounter.getMonth() + 1)
   }
 
-  if (asObject) return { ...monthlyL };
+  if (asObject) return { ...monthlyL }
 
-  if (dates.length === 1) return [dates[0], dates[0]];
+  if (dates.length === 1) return [dates[0], dates[0]]
 
-  return dates;
+  return dates
 }
 
-export function getShortMonthString(date) {
+export function getShortMonthString (date) {
   if (typeof date === 'string') {
-    date = new Date(date);
+    date = new Date(date)
   }
   return [
     'Janv.',
@@ -181,64 +181,64 @@ export function getShortMonthString(date) {
     'Oct.',
     'Nov.',
     'Déc.',
-  ][date.getMonth()];
+  ][date.getMonth()]
 }
 
-export function nbOfDays(startDate, endDate) {
-  startDate.setHours(0, 0, 0, 0);
-  endDate.setHours(0, 0, 0, 0);
-  const start = startDate;
+export function nbOfDays (startDate, endDate) {
+  startDate.setHours(0, 0, 0, 0)
+  endDate.setHours(0, 0, 0, 0)
+  const start = startDate
 
-  if (start.getTime() === endDate.getTime()) return 0;
+  if (start.getTime() === endDate.getTime()) return 0
 
-  let nbOfDay = 0;
+  let nbOfDay = 0
 
   do {
-    nbOfDay++;
-    start.setDate(start.getDate() + 1);
-  } while (start.getTime() < endDate.getTime());
-  return nbOfDay;
+    nbOfDay++
+    start.setDate(start.getDate() + 1)
+  } while (start.getTime() < endDate.getTime())
+  return nbOfDay
 }
 
-export function nbWorkingDays(startDate, endDate) {
-  startDate = today(startDate);
-  endDate = today(endDate);
-  const start = new Date(startDate);
-  let nbOfDay = 0;
+export function nbWorkingDays (startDate, endDate) {
+  startDate = today(startDate)
+  endDate = today(endDate)
+  const start = new Date(startDate)
+  let nbOfDay = 0
   do {
     if (workingDay(start)) {
-      nbOfDay++;
+      nbOfDay++
     }
-    start.setDate(start.getDate() + 1);
-  } while (start.getTime() <= endDate.getTime());
-  return nbOfDay;
+    start.setDate(start.getDate() + 1)
+  } while (start.getTime() <= endDate.getTime())
+  return nbOfDay
 }
 
-export function checkIfDateIsNotToday(date) {
-  const today = new Date();
+export function checkIfDateIsNotToday (date) {
+  const today = new Date()
   return (
     date &&
     (date.getDate() !== today.getDate() ||
       date.getMonth() !== today.getMonth() ||
       date.getFullYear() !== today.getFullYear())
-  );
+  )
 }
 
-export function stringToDecimalDate(str) {
+export function stringToDecimalDate (str) {
   if (str !== null || str !== '') {
-    const strArray = str.split('h');
-    return (parseInt(strArray[0]) * 60 + parseInt(strArray[1])) / 60;
+    const strArray = str.split('h')
+    return (parseInt(strArray[0]) * 60 + parseInt(strArray[1])) / 60
   }
 }
 
-export function decimalToStringDate(decimal) {
+export function decimalToStringDate (decimal) {
   if (decimal != null) {
-    const strArray = String(decimal).split('.');
+    const strArray = String(decimal).split('.')
     const decimalMinute =
-      strArray[1] && +strArray[1].length === 1 ? +strArray[1] * 10 : +strArray[1];
-    let minute = strArray[1] ? String(Math.ceil((1 / 100) * decimalMinute * 60)) : '00';
-    minute = minute.length === 1 ? '0' + minute : minute;
-    return strArray[0] + 'h' + minute;
+      strArray[1] && +strArray[1].length === 1 ? +strArray[1] * 10 : +strArray[1]
+    let minute = strArray[1] ? String(Math.ceil((1 / 100) * decimalMinute * 60)) : '00'
+    minute = minute.length === 1 ? '0' + minute : minute
+    return strArray[0] + 'h' + minute
   }
-  return '';
+  return ''
 }
