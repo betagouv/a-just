8d84c23841626585c435bf14acbbd74f117b5be2
MrJimmyChevallier <jimmy.chevallier@free.fr>
2022-10-05 14:58:22 +0200

    update wip

--- api/src/routes-api/RouteContentieuxOptions.js
+++ api/src/routes-api/RouteContentieuxOptions.js
@@ -1,9 +1,9 @@
-import Route, { Access } from './Route'
-import { Types } from '../utils/types'
+import Route, { Access } from './Route';
+import { Types } from '../utils/types';
 
 export default class RouteContentieuxOptions extends Route {
-  constructor (params) {
-    super({ ...params, model: 'ContentieuxOptions' })
+  constructor(params) {
+    super({ ...params, model: 'ContentieuxOptions' });
   }
 
   @Route.Post({
@@ -13,30 +13,34 @@ export default class RouteContentieuxOptions extends Route {
     }),
     accesses: [Access.canVewContentieuxOptions],
   })
-  async getAll (ctx) {
-    let { juridictionId, backupId } = this.body(ctx)
+  async getAll(ctx) {
+    let { juridictionId, backupId } = this.body(ctx);
     const backups = await this.model.models.OptionsBackups.getBackup(
       ctx.state.user.id,
       juridictionId
-    )
-    backupId = backups.find(b => b.id === backupId) ? backupId : (backups.length ? backups[backups.length - 1].id : null)
+    );
+    backupId = backups.find((b) => b.id === backupId)
+      ? backupId
+      : backups.length
+      ? backups[backups.length - 1].id
+      : null;
 
     this.sendOk(ctx, {
       backups,
       backupId,
-    })
+    });
   }
 
   @Route.Delete({
     path: 'remove-backup/:backupId',
     accesses: [Access.canVewContentieuxOptions],
   })
-  async removeBackup (ctx) {
-    const { backupId } = ctx.params
+  async removeBackup(ctx) {
+    const { backupId } = ctx.params;
 
-    await this.model.models.OptionsBackups.removeBackup(backupId)
+    await this.model.models.OptionsBackups.removeBackup(backupId);
 
-    this.sendOk(ctx, 'OK')
+    this.sendOk(ctx, 'OK');
   }
 
   @Route.Post({
@@ -47,26 +51,16 @@ export default class RouteContentieuxOptions extends Route {
     }),
     accesses: [Access.canVewContentieuxOptions],
   })
-  async duplicateBackup (ctx) {
-    const { backupId, backupName, juridictionId } = this.body(ctx)
+  async duplicateBackup(ctx) {
+    const { backupId, backupName, juridictionId } = this.body(ctx);
 
-    if (
-      await this.models.OptionsBackups.haveAccess(
-        backupId,
-        juridictionId,
-        ctx.state.user.id
-      )
-    ) {
+    if (await this.models.OptionsBackups.haveAccess(backupId, juridictionId, ctx.state.user.id)) {
       this.sendOk(
         ctx,
-        await this.model.models.OptionsBackups.duplicateBackup(
-          backupId,
-          backupName,
-          juridictionId
-        )
-      )
+        await this.model.models.OptionsBackups.duplicateBackup(backupId, backupName, juridictionId)
+      );
     } else {
-      this.sendOk(ctx, null)
+      this.sendOk(ctx, null);
     }
   }
 
@@ -79,8 +73,8 @@ export default class RouteContentieuxOptions extends Route {
     }),
     accesses: [Access.canVewContentieuxOptions],
   })
-  async saveBackup (ctx) {
-    const { backupId, list, backupName, juridictionId } = this.body(ctx)
+  async saveBackup(ctx) {
+    const { backupId, list, backupName, juridictionId } = this.body(ctx);
 
     if (
       (backupId &&
@@ -89,22 +83,18 @@ export default class RouteContentieuxOptions extends Route {
           juridictionId,
           ctx.state.user.id
         ))) ||
-      (!backupId &&
-        (await this.models.HRBackups.haveAccess(
-          juridictionId,
-          ctx.state.user.id
-        )))
+      (!backupId && (await this.models.HRBackups.haveAccess(juridictionId, ctx.state.user.id)))
     ) {
       const newId = await this.model.models.OptionsBackups.saveBackup(
         list,
         backupId,
         backupName,
         juridictionId
-      )
+      );
 
-      this.sendOk(ctx, newId)
+      this.sendOk(ctx, newId);
     } else {
-      this.sendOk(ctx, null)
+      this.sendOk(ctx, null);
     }
   }
 
@@ -116,39 +106,33 @@ export default class RouteContentieuxOptions extends Route {
     }),
     accesses: [Access.canVewContentieuxOptions],
   })
-  async renameBackup (ctx) {
-    const { backupId, backupName, juridictionId } = this.body(ctx)
+  async renameBackup(ctx) {
+    const { backupId, backupName, juridictionId } = this.body(ctx);
 
-    if (
-      await this.models.OptionsBackups.haveAccess(
-        backupId,
-        juridictionId,
-        ctx.state.user.id
-      )
-    ) {
-      await this.model.models.OptionsBackups.renameBackup(backupId, backupName)
+    if (await this.models.OptionsBackups.haveAccess(backupId, juridictionId, ctx.state.user.id)) {
+      await this.model.models.OptionsBackups.renameBackup(backupId, backupName);
 
-      this.sendOk(ctx, 'Ok')
+      this.sendOk(ctx, 'Ok');
     } else {
-      this.sendOk(ctx, null)
+      this.sendOk(ctx, null);
     }
   }
 
   @Route.Get({
     accesses: [Access.isAdmin],
   })
-  async getAllAdmin (ctx) {
-    const list = await this.models.OptionsBackups.adminGetAll()
+  async getAllAdmin(ctx) {
+    const list = await this.models.OptionsBackups.adminGetAll();
 
     const juridictions = await this.models.HRBackups.findAll({
       attributes: ['id', 'label'],
       raw: true,
-    })
+    });
 
     this.sendOk(ctx, {
       list,
       juridictions,
-    })
+    });
   }
 
   @Route.Post({
@@ -158,33 +142,60 @@ export default class RouteContentieuxOptions extends Route {
     }),
     accesses: [Access.isAdmin],
   })
-  async updateBackup (ctx) {
-    const { id, juridictions } = this.body(ctx)
+  async updateBackup(ctx) {
+    const { id, juridictions } = this.body(ctx);
 
-    await this.model.models.OptionsBackupJuridictions.changeRules(
-      id,
-      juridictions
-    )
+    await this.model.models.OptionsBackupJuridictions.changeRules(id, juridictions);
 
-    this.sendOk(ctx, 'Ok')
+    this.sendOk(ctx, 'Ok');
   }
 
   @Route.Get({
     path: 'get-backup-details/:backupId',
     accesses: [Access.canVewContentieuxOptions],
   })
-  async getBackupDetails (ctx) {
-    const { backupId } = ctx.params
+  async getBackupDetails(ctx) {
+    const { backupId } = ctx.params;
 
     if (
-      await this.models.OptionsBackups.haveAccessWithoutJuridiction(
-        backupId,
-        ctx.state.user.id
-      )
+      await this.models.OptionsBackups.haveAccessWithoutJuridiction(backupId, ctx.state.user.id)
     ) {
-      this.sendOk(ctx, await this.model.getAllById(backupId))
+      this.sendOk(ctx, await this.model.getAllById(backupId));
     } else {
-      this.sendOk(ctx, null)
+      this.sendOk(ctx, null);
+    }
+  }
+
+  @Route.Post({
+    bodyType: Types.object().keys({
+      backupId: Types.number(),
+      contentieuxId: Types.number().required(),
+      //Types.number().required(),
+    }),
+    accesses: [Access.canVewContentieuxOptions],
+  })
+  async getLastUpdate(ctx) {
+    const { backupId, contentieuxId, juridictionId } = this.body(ctx);
+
+    if (
+      (backupId &&
+        (await this.models.OptionsBackups.haveAccess(
+          backupId,
+          juridictionId,
+          ctx.state.user.id
+        ))) ||
+      (!backupId && (await this.models.HRBackups.haveAccess(juridictionId, ctx.state.user.id)))
+    ) {
+      const newId = await this.model.models.OptionsBackups.saveBackup(
+        list,
+        backupId,
+        backupName,
+        juridictionId
+      );
+
+      this.sendOk(ctx, newId);
+    } else {
+      this.sendOk(ctx, null);
     }
   }
 }
