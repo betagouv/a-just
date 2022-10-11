fa5f056ad36b223ca4a614b76e8450d500871e33
MrJimmyChevallier <jimmy.chevallier@free.fr>
2022-10-11 15:50:05 +0200

    wip

--- front/src/app/interfaces/contentieu-referentiel.ts
+++ front/src/app/interfaces/contentieu-referentiel.ts
@@ -1,19 +1,20 @@
 export interface ContentieuReferentielInterface {
-	id: number;
-	label: string;
-	childrens?: ContentieuReferentielInterface[];
-	totalAffected?: number;
-	percent?: number;
-	selected?: boolean;
-	rank?: number;
-	in?: number | null;
-	originalIn?: number | null;
-	out?: number | null;
-	originalOut?: number | null;
-	stock?: number | null;
-	originalStock?: number | null;
-	showActivityGroup?: boolean;
-	showOptionGroup?: boolean;
-	averageProcessingTime: number | null;
-	parent?: ContentieuReferentielInterface;
+  id: number
+  label: string
+  childrens?: ContentieuReferentielInterface[]
+  totalAffected?: number
+  percent?: number
+  selected?: boolean
+  rank?: number
+  in?: number | null
+  originalIn?: number | null
+  out?: number | null
+  originalOut?: number | null
+  stock?: number | null
+  originalStock?: number | null
+  showActivityGroup?: boolean
+  showOptionGroup?: boolean
+  averageProcessingTime: number | null
+  parent?: ContentieuReferentielInterface
+  childIsVisible?: boolean
 }
