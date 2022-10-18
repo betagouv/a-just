8bd7be399ba967abd5daf8a51ba2ddd158a75073
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-10-18 15:37:26 +0200

    fix calculator interface

--- front/src/app/routes/calculator/calculator.page.ts
+++ front/src/app/routes/calculator/calculator.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core'
 import { orderBy } from 'lodash'
 import { dataInterface } from 'src/app/components/select/select.component'
+import { WrapperComponent } from 'src/app/components/wrapper/wrapper.component'
 import { CalculatorInterface } from 'src/app/interfaces/calculator'
 import { ContentieuReferentielInterface } from 'src/app/interfaces/contentieu-referentiel'
 import { DocumentationInterface } from 'src/app/interfaces/documentation'
@@ -17,6 +18,7 @@ import { month } from 'src/app/utils/dates'
   styleUrls: ['./calculator.page.scss'],
 })
 export class CalculatorPage extends MainClass implements OnDestroy, OnInit {
+  @ViewChild('wrapper') wrapper: WrapperComponent | undefined
   referentiel: ContentieuReferentielInterface[] = []
   referentielIds: number[] = this.calculatorService.referentielIds.getValue()
   dateStart: Date | null = null
@@ -26,10 +28,9 @@ export class CalculatorPage extends MainClass implements OnDestroy, OnInit {
   datasFilted: CalculatorInterface[] = []
   isLoading: boolean = false
   maxDateSelectionDate: Date | null = null
-  isLoadingLastMonth: boolean = false
   categorySelected: string = 'magistrats'
   documentation: DocumentationInterface = {
-    title: 'Calculator',
+    title: 'Calculateur',
     path: 'https://a-just.gitbook.io/documentation-deploiement/calculateur/quest-ce-que-cest',
   }
   lastCategorySelected: string = ''
@@ -55,7 +56,11 @@ export class CalculatorPage extends MainClass implements OnDestroy, OnInit {
     this.watch(
       this.calculatorService.dateStart.subscribe((date) => {
         this.dateStart = date
-        this.onLoad()
+        if (date === null) {
+          this.onCheckLastMonth()
+        } else {
+          this.onLoad()
+        }
       })
     )
     this.watch(
@@ -90,26 +95,35 @@ export class CalculatorPage extends MainClass implements OnDestroy, OnInit {
           )
         }
 
-        if (this.isLoadingLastMonth === false) {
-          this.isLoadingLastMonth = true
-
-          this.activitiesService.getLastMonthActivities().then((date) => {
-            date = new Date(date ? date : '')
-            const max = month(date, 0, 'lastday')
-            this.maxDateSelectionDate = max
-            console.log(max)
-
-            this.calculatorService.dateStart.next(month(max, -2))
-            this.calculatorService.dateStop.next(max)
-            this.isLoadingLastMonth = false
-          })
-        }
+        this.onCheckLastMonth()
       })
     )
   }
 
   ngOnDestroy() {
     this.watcherDestroy()
+  }
+
+  onCheckLastMonth() {
+    if (
+      this.calculatorService.dateStart.getValue() === null &&
+      this.referentiel.length
+    ) {
+      this.activitiesService.getLastMonthActivities().then((date) => {
+        if(date === null) {
+          date = new Date()
+        }
+
+        console.log(date)
+        date = new Date(date ? date : '')
+        const max = month(date, 0, 'lastday')
+        this.maxDateSelectionDate = max
+        console.log(max)
+
+        this.calculatorService.dateStart.next(month(max, -2))
+        this.calculatorService.dateStop.next(max)
+      })
+    }
   }
 
   onLoad() {
@@ -119,12 +133,16 @@ export class CalculatorPage extends MainClass implements OnDestroy, OnInit {
       this.calculatorService.referentielIds.getValue().length &&
       this.dateStart !== null &&
       this.dateStop !== null &&
-      this.maxDateSelectionDate &&
       this.isLoading === false
     ) {
       this.isLoading = true
       this.calculatorService
-        .filterList(this.categorySelected, this.lastCategorySelected === this.categorySelected ? this.selectedFonctionsIds : null)
+        .filterList(
+          this.categorySelected,
+          this.lastCategorySelected === this.categorySelected
+            ? this.selectedFonctionsIds
+            : null
+        )
         .then(({ list, fonctions }) => {
           if (this.lastCategorySelected !== this.categorySelected) {
             this.fonctions = fonctions.map((f: HRFonctionInterface) => ({
@@ -203,5 +221,28 @@ export class CalculatorPage extends MainClass implements OnDestroy, OnInit {
   onChangeFonctionsSelected(fonctionsId: string[] | number[]) {
     this.selectedFonctionsIds = fonctionsId.map((f) => +f)
     this.onLoad()
+  }
+
+  onShowPanel(type: string) {
+    switch (type) {
+      case 'données renseignées':
+        this.wrapper?.onForcePanelHelperToShow({
+          title: 'Données renseignées',
+          path: 'https://a-just.gitbook.io/documentation-deploiement/calculateur/visualiser-son-activite-grace-aux-donnees-renseignees',
+        })
+        break
+      case 'activité constatée':
+        this.wrapper?.onForcePanelHelperToShow({
+          title: 'Activité constatée',
+          path: 'https://a-just.gitbook.io/documentation-deploiement/calculateur/indicateurs-issus-de-lactivite-constatee',
+        })
+        break
+      case 'activité calculée':
+        this.wrapper?.onForcePanelHelperToShow({
+          title: 'Activité calculée',
+          path: 'https://a-just.gitbook.io/documentation-deploiement/calculateur/comparer-son-activite-grace-a-lactivite-calculee',
+        })
+        break
+    }
   }
 }
