40f906b9d0dc5d15b54b3f24136bfd78e8752fe8
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-11-08 14:36:17 +0100

    add to sendinblue users

--- api/config/staging.js
+++ api/config/staging.js
@@ -3,4 +3,7 @@ module.exports = {
   serverUrl: process.env.SERVER_URL,
   frontUrl: process.env.FRONT_URL || 'http://localhost:4200',
   preloadHumanResourcesDatas: true,
+  sendingBlue: {
+    usersListId: 8,
+  },
 }
