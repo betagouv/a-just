dc30d185a2f6df6f2cd32a62ccd6c56aff1b280d
MrJimmyChevallier <jimmy.chevallier@free.fr>
2022-11-14 02:16:41 +0100

    wip

--- api/src/routes-api/RouteSimulator.js
+++ api/src/routes-api/RouteSimulator.js
@@ -33,7 +33,7 @@ export default class RouteSimulator extends Route {
     console.log({ hr: hr.length })
 
     console.time('simulator-1.1')
-    hr = await filterByCategory(hr, categoryId, functionIds)
+    //hr = await filterByCategory(hr, categoryId, functionIds)
     console.timeEnd('simulator-1.1')
 
     let counter = 0
@@ -48,12 +48,14 @@ export default class RouteSimulator extends Route {
     const categories = await this.models.HRCategories.getAll()
     console.timeEnd('simulator-2')
 
+    console.log(categories)
+
     console.time('simulator-3')
     const activities = await this.models.Activities.getAll(backupId)
     console.timeEnd('simulator-3')
 
     console.time('simulator-4')
-    const situation = await getSituation(referentielId, hr, activities, categories, dateStart, dateStop)
+    const situation = await getSituation(referentielId, hr, activities, categories, dateStart, dateStop, categoryId)
     console.timeEnd('simulator-4')
 
     this.sendOk(ctx, { situation, categories, hr })
@@ -66,17 +68,22 @@ export default class RouteSimulator extends Route {
       simulation: Types.object().required(),
       dateStart: Types.date().required(),
       dateStop: Types.date().required(),
+      selectedCategoryId: Types.number().required(),
     }),
     accesses: [Access.canVewHR],
   })
   async toSimulate (ctx) {
-    let { backupId, params, simulation, dateStart, dateStop } = this.body(ctx)
+    let { backupId, params, simulation, dateStart, dateStop, selectedCategoryId } = this.body(ctx)
 
     if (!(await this.models.HRBackups.haveAccess(backupId, ctx.state.user.id))) {
       ctx.throw(401, "Vous n'avez pas accès à cette juridiction !")
     }
 
-    const simulatedSituation = execSimulation(params, simulation, dateStart, dateStop)
+    const categories = await this.models.HRCategories.getAll()
+
+    let sufix = 'By' + categories.find((element) => element.id === selectedCategoryId).label
+
+    const simulatedSituation = execSimulation(params, simulation, dateStart, dateStop, sufix)
     this.sendOk(ctx, simulatedSituation)
   }
 }
