76281414e48d6686c9ed830c4f393223e4105d31
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-11-24 14:32:16 +0100

    filter user to mail list

---
+++ api/src/db/seeders/development/202211241430-add-users-to-sendinblue-bis.js
@@ -0,0 +1,12 @@
+const { sentEmailSendinblueUserList } = require('../../../utils/email')
+
+module.exports = {
+  up: async (queryInterface, Sequelize, models) => {
+    const users = await models.Users.findAll()
+
+    for (let i = 0; i < users.length; i++) {
+      await sentEmailSendinblueUserList(users[i].dataValues, await models.UserVentilations.getUserVentilations(users[i].dataValues.id).length)
+    }
+  },
+  down: (/*queryInterface , Sequelize*/) => {},
+}
