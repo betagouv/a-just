1df964c022cec196292c5337b71b8b8f7eb36ffa
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-12-02 10:21:43 +0100

    fix user to sendinblue

--- api/src/db/seeders/development/202211241434-add-users-to-sendinblue-bis.js
+++ api/src/db/seeders/development/202211241434-add-users-to-sendinblue-bis.js
@@ -1,11 +1,11 @@
-const { sentEmailSendinblueUserList } = require('../../../utils/email')
+import { sentEmailSendinblueUserList } from '../../../utils/email'
 
 module.exports = {
   up: async (queryInterface, Sequelize, models) => {
     const users = await models.Users.findAll()
 
     for (let i = 0; i < users.length; i++) {
-      await sentEmailSendinblueUserList(users[i].dataValues, await models.UserVentilations.getUserVentilations(users[i].dataValues.id).length)
+      await sentEmailSendinblueUserList(users[i].dataValues, (await models.UserVentilations.getUserVentilations(users[i].dataValues.id)).length)
     }
   },
   down: (/*queryInterface , Sequelize*/) => {},
