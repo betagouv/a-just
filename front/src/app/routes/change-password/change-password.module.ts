24ae37fc1c399c08c6a0dd4a436c304f04c09564
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-12-02 14:11:19 +0100

    change password

--- front/src/app/routes/change-password/change-password.module.ts
+++ front/src/app/routes/change-password/change-password.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ComponentsModule } from 'src/app/components/components.module';
 import { ChangePassword } from './change-password.page';
 import { ChangePasswordPageModule } from './change-password.routing';
 
 @NgModule({
 	declarations: [ ChangePassword ],
-	imports: [ ChangePasswordPageModule, FormsModule, ReactiveFormsModule, RouterModule ]
+	imports: [ ChangePasswordPageModule, FormsModule, ReactiveFormsModule, RouterModule, ComponentsModule ]
 })
 export class ChangePasswordModule {}
