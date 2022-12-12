e97cc7c30d1561c2307f1d7b318926fdae2d0681
MrJimmyChevallier <jimmy.chevallier@free.fr>
2022-12-12 17:26:23 +0100

    update admin branch

--- front-admin/src/app/routes/imports/imports.page.ts
+++ front-admin/src/app/routes/imports/imports.page.ts
@@ -64,25 +64,6 @@ export class ImportsPage {
     });
   }
 
-  async onSendHRMatricule(form: any) {
-    const file = form.file.files[0];
-
-    if (!file) {
-      alert('Vous devez saisir une fichier !');
-      return;
-    }
-
-    const options = {
-      file: await exportFileToString(file),
-    };
-
-    this.importService.importHRMatricule(options).then(() => {
-      alert('OK !');
-      form.reset();
-      this.onLoad();
-    });
-  }
-
   async onSendActivity(form: any) {
     const backupId = form.backupId.value;
     const file = form.file.files[0];
