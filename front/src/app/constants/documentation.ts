1f7247145a4737f7e601e167f257f2a073edf0fa
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-12-14 18:00:46 +0100

    change human documentation

--- front/src/app/constants/documentation.ts
+++ front/src/app/constants/documentation.ts
@@ -6,3 +6,11 @@ export const DOCUMENTATION_URL = 'https://a-just.gitbook.io/documentation-deploi
  * Lien de la calculate
  */
 export const CALCULATE_DOWNLOAD_URL = '/assets/calculatrice%20de%20ventilation%20du%20temps%20par%20activit%C3%A9%20A-JUST%20MAG%20et%20GRF.xlsx'
+/**
+ * Lien pour créer une nouvelle fiche
+ */
+export const DOCUMENTATION_CREATE_NEW_PERSON = 'https://a-just.gitbook.io/documentation-deploiement/ventilateur/creer-une-nouvelle-fiche'
+/**
+ * Lien pour modifier une fiche
+ */
+export const DOCUMENTATION_UPDATE_PERSON = 'https://a-just.gitbook.io/documentation-deploiement/ventilateur/enregistrer-une-nouvelle-situation'
