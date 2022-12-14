d47b5f564bbc53e34bfbef8ce56f194072f9f7fa
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-12-14 15:38:00 +0100

    wip doc

--- front/src/app/routes/calculator/referentiel-calculator/referentiel-calculator.component.ts
+++ front/src/app/routes/calculator/referentiel-calculator/referentiel-calculator.component.ts
@@ -1,7 +1,10 @@
 import { Component, HostBinding, Input } from '@angular/core'
 import { CalculatorInterface } from 'src/app/interfaces/calculator'
 import { MainClass } from 'src/app/libs/main-class'
-import { decimalToStringDate } from 'src/app/utils/dates'
+
+/**
+ * Composant d'une ligne du calculateur
+ */
 
 @Component({
   selector: 'aj-referentiel-calculator',
@@ -9,25 +12,42 @@ import { decimalToStringDate } from 'src/app/utils/dates'
   styleUrls: ['./referentiel-calculator.component.scss'],
 })
 export class ReferentielCalculatorComponent extends MainClass {
+  /**
+   * Un item de la liste du calculateur
+   */
   @Input() calculator: CalculatorInterface | null = null
+  /**
+   * Champ qui est trié, purement visuel
+   */
   @Input() sortBy: string = ''
+  /**
+   * Type de catégorie choisie
+   */
   @Input() categorySelected: string = ''
+  /**
+   * Affiche ou non les enfants de force
+   */
   @Input() forceToShowChildren: boolean = false
+  /**
+   * Connexion au css pour forcer l'affichage des enfants
+   */
   @HostBinding('class.show-children') showChildren: boolean =
     (this.calculator && this.calculator.childIsVisible) || false
 
+    /**
+     * Constructor
+     */
   constructor() {
     super()
   }
 
+  /**
+   * Switch la visibilité des enfants
+   */
   onToggleChildren() {
     if (this.calculator) {
       this.showChildren = !this.showChildren
       this.calculator.childIsVisible = this.showChildren
     }
-  }
-
-  decimalToStringDate(decimal: number | null) {
-    return decimalToStringDate(decimal)
   }
 }
