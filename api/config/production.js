c74aadd9ec2117ee4917c5338e614a70c9b807b4
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-12-16 11:41:58 +0100

    hide preload datas

--- api/config/production.js
+++ api/config/production.js
@@ -2,7 +2,7 @@ module.exports = {
   envName: 'PROD',
   serverUrl: process.env.SERVER_URL,
   frontUrl: process.env.FRONT_URL || 'http://localhost:4200',
-  preloadHumanResourcesDatas: true,
+  preloadHumanResourcesDatas: false,
   sendingBlue: {
     usersListId: 8,
   },
