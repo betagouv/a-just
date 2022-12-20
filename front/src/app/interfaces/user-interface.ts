821c6382e3b23237bbae60cf13cea0d8b98d1093
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-12-20 11:52:48 +0100

    wip doc

--- front/src/app/interfaces/user-interface.ts
+++ front/src/app/interfaces/user-interface.ts
@@ -1,10 +1,37 @@
+/**
+ * Interface d'un utilisateur connecté
+ */
 export interface UserInterface {
+	/**
+	 * Id de la base
+	 */
 	id?: number;
+	/**
+	 * Email de connection
+	 */
 	email?: string;
+	/**
+	 * Role (1 admin, 0 user)
+	 */
 	role?: number;
+	/**
+	 * Compte bloqué ou non
+	 */
 	status?: number;
+	/**
+	 * Prénom utilisateur
+	 */
 	firstName?: string;
+	/**
+	 * Nom utilisateur
+	 */
 	lastName?: string;
+	/**
+	 * Token de connection généré par JWT
+	 */
 	token?: string;
+	/**
+	 * Liste des pages sur lequels il a accès
+	 */
 	access?: number[];
 }
