ba3b6148f51c8fb7f4ab6c93197a240f8ce2cd19
François-Xavier Montigny <fxaviermontigny@gmail.com>
2022-12-20 13:48:16 +0100

    change calculate file

--- front/src/app/constants/documentation.ts
+++ front/src/app/constants/documentation.ts
@@ -5,7 +5,7 @@ export const DOCUMENTATION_URL = 'https://a-just.gitbook.io/documentation-deploi
 /**
  * Lien de la calculate
  */
-export const CALCULATE_DOWNLOAD_URL = '/assets/calculatrice%20de%20ventilation%20du%20temps%20par%20activit%C3%A9%20A-JUST%20MAG%20et%20GRF.xlsx'
+export const CALCULATE_DOWNLOAD_URL = '/assets/Calculatrice_de_ventilation_du_temps_par_activité_A-JUST_MAG_et_GRF_20221219.xlsx'
 /**
  * Lien pour créer une nouvelle fiche
  */
