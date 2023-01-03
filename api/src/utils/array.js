933a8528d9c722efa0c771b3f2aed5aa355571df
François-Xavier Montigny <fxaviermontigny@gmail.com>
2023-01-03 16:26:50 +0100

    Add logs to utils

--- api/src/utils/array.js
+++ api/src/utils/array.js
@@ -1,3 +1,8 @@
+/**
+ * Création d'une copie d'un tableau
+ * @param {*} arrayToCopy
+ * @returns
+ */
 export const copyArray = (arrayToCopy) => {
   return JSON.parse(JSON.stringify(arrayToCopy))
 }
