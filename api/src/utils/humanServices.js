93fa7f8b00b14f09e833a8d0858549929109ee95
MrJimmyChevallier <jimmy.chevallier@free.fr>
2023-01-04 20:41:52 +0100

    update doc utils

--- api/src/utils/humanServices.js
+++ api/src/utils/humanServices.js
@@ -1,13 +1,10 @@
 import { orderBy, uniqBy } from 'lodash'
 import { today } from './date'
 
-export async function getHumanRessourceList (
-  preformatedAllHumanResource,
-  contentieuxIds = undefined,
-  categoriesIds,
-  date,
-  endPeriodToCheck = undefined
-) {
+/**
+ * Retourne une liste de human ressource
+ */
+export async function getHumanRessourceList (preformatedAllHumanResource, contentieuxIds = undefined, categoriesIds, date, endPeriodToCheck = undefined) {
   const list = preformatedAllHumanResource.filter((hr) => {
     let isOk = true
 
@@ -29,7 +26,6 @@ export async function getHumanRessourceList (
       }
     }
 
-    
     return isOk
   })
   console.timeEnd('step3')
@@ -38,9 +34,7 @@ export async function getHumanRessourceList (
   if (!contentieuxIds) return list
 
   return list.filter((h) => {
-    const idsOfactivities = h.currentActivities.map(
-      (a) => (a.contentieux && a.contentieux.id) || 0
-    )
+    const idsOfactivities = h.currentActivities.map((a) => (a.contentieux && a.contentieux.id) || 0)
     for (let i = 0; i < idsOfactivities.length; i++) {
       if (contentieuxIds.indexOf(idsOfactivities[i]) !== -1) {
         return true
@@ -51,6 +45,12 @@ export async function getHumanRessourceList (
   })
 }
 
+/**
+ * Filtre les activités par date
+ * @param {*} list
+ * @param {*} date
+ * @returns liste d'activité filtrée
+ */
 export function filterActivitiesByDate (list, date) {
   list = orderBy(list || [], ['dateStart'], ['desc'])
   list = list.filter((a) => {
@@ -61,10 +61,7 @@ export function filterActivitiesByDate (list, date) {
       (dateStart === null && dateStop === null) ||
       (dateStart && dateStart.getTime() <= date.getTime() && dateStop === null) ||
       (dateStart === null && dateStop && dateStop.getTime() >= date.getTime()) ||
-      (dateStart &&
-        dateStart.getTime() <= date.getTime() &&
-        dateStop &&
-        dateStop.getTime() >= date.getTime())
+      (dateStart && dateStart.getTime() <= date.getTime() && dateStop && dateStop.getTime() >= date.getTime())
     )
   })
 
