49d2942a2eb8171e0376b0cdfba9723fc8deea52
MrJimmyChevallier <jimmy.chevallier@free.fr>
2023-01-11 10:36:13 +0100

    update  doc + clean consol.log()

--- front/documentation/js/menu-wc.js
+++ front/documentation/js/menu-wc.js
@@ -575,25 +575,6 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                 </ul>
                 </li>
                     <li class="chapter">
-                        <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#components-links"' :
-                            'data-target="#xs-components-links"' }>
-                            <span class="icon ion-md-cog"></span>
-                            <span>Components</span>
-                            <span class="icon ion-ios-arrow-down"></span>
-                        </div>
-                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
-                            <li class="link">
-                                <a href="components/DateSelectBlueComponent.html" data-type="entity-link" >DateSelectBlueComponent</a>
-                            </li>
-                            <li class="link">
-                                <a href="components/LegendLabelComponent.html" data-type="entity-link" >LegendLabelComponent</a>
-                            </li>
-                            <li class="link">
-                                <a href="components/RadioButtonComponent.html" data-type="entity-link" >RadioButtonComponent</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="chapter">
                         <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#classes-links"' :
                             'data-target="#xs-classes-links"' }>
                             <span class="icon ion-ios-paper"></span>
