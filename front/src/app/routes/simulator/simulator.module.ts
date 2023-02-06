0c2413ab148e9cec5e96012620ba95add53af2ed
MrJimmyChevallier <jimmy.chevallier@free.fr>
2023-02-06 18:59:53 +0100

    add popin simulateur

--- front/src/app/routes/simulator/simulator.module.ts
+++ front/src/app/routes/simulator/simulator.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { SimulatorPageModule } from './simulator.routing';
-import { SimulatorPage } from './simulator.page';
-import { RouterModule } from '@angular/router';
-import { ComponentsModule } from 'src/app/components/components.module';
-import { MaterialModule } from 'src/app/libs/material.module';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { SimulatorPageModule } from './simulator.routing'
+import { SimulatorPage } from './simulator.page'
+import { RouterModule } from '@angular/router'
+import { ComponentsModule } from 'src/app/components/components.module'
+import { MaterialModule } from 'src/app/libs/material.module'
+import { CommonModule } from '@angular/common'
+import { MatTooltipModule } from '@angular/material/tooltip'
 
 @NgModule({
   declarations: [SimulatorPage],
@@ -14,6 +15,7 @@ import { CommonModule } from '@angular/common';
     ComponentsModule,
     MaterialModule,
     CommonModule,
+    MatTooltipModule,
   ],
 })
 export class SimulatorModule {}
