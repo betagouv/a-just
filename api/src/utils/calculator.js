46b1c97b4cbeefdbdc3c97a45a4bd78a311b38c3
MrJimmyChevallier <jimmy.chevallier@free.fr>
2023-02-08 16:33:53 +0100

    align calculation on calculator

--- api/src/utils/calculator.js
+++ api/src/utils/calculator.js
@@ -337,8 +337,12 @@ const calculateActivities = (referentielId, totalIn, lastStock, magEtpAffected, 
   }
 
   if (magCalculateTimePerCase) {
-    magCalculateOut = Math.floor((((magEtpAffected * config.nbHoursPerDayAndMagistrat) / magCalculateTimePerCase) * config.nbDaysByMagistrat) / 12)
-    fonCalculateOut = Math.floor((((fonEtpAffected * config.nbHoursPerDayAndFonctionnaire) / fonCalculateTimePerCase) * config.nbDaysByFonctionnaire) / 12)
+    magCalculateOut = Math.floor(Math.floor(magEtpAffected * config.nbHoursPerDayAndMagistrat * (config.nbDaysByMagistrat / 12)) / magCalculateTimePerCase)
+    fonCalculateOut = Math.floor(
+      Math.floor(fonEtpAffected * config.nbHoursPerDayAndFonctionnaire * (config.nbDaysByFonctionnaire / 12)) / fonCalculateTimePerCase
+    )
+    //magCalculateOut = Math.floor((((magEtpAffected * config.nbHoursPerDayAndMagistrat) / magCalculateTimePerCase) * config.nbDaysByMagistrat) / 12)
+    //fonCalculateOut = Math.floor((((fonEtpAffected * config.nbHoursPerDayAndFonctionnaire) / fonCalculateTimePerCase) * config.nbDaysByFonctionnaire) / 12)
     magCalculateCoverage = fixDecimal(magCalculateOut / (totalIn || 0))
     fonCalculateCoverage = fixDecimal(fonCalculateOut / (totalIn || 0))
     magCalculateDTESInMonths = lastStock === null ? null : fixDecimal(lastStock / magCalculateOut)
