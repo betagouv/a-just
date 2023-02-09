ce0e159c39db540e9885d7c15c29bbbae862cbd6
MrJimmyChevallier <jimmy.chevallier@free.fr>
2023-02-09 17:29:50 +0100

    change round values

--- api/src/utils/calculator.js
+++ api/src/utils/calculator.js
@@ -144,9 +144,9 @@ const getActivityValues = (dateStart, dateStop, activities, referentielId, nbMon
   const realDTESInMonths = lastStock !== null ? fixDecimal(lastStock / totalOut, 100) : null
 
   const etpAffected = getHRPositions(hr, categories, referentielId, dateStart, dateStop)
-  const etpMag = etpAffected.length > 0 ? fixDecimal(etpAffected[0].totalEtp, 1000) : 0
-  const etpFon = etpAffected.length > 1 ? fixDecimal(etpAffected[1].totalEtp, 1000) : 0
-  const etpCont = etpAffected.length > 2 ? fixDecimal(etpAffected[2].totalEtp, 1000) : 0
+  const etpMag = etpAffected.length > 0 ? fixDecimal(etpAffected[0].totalEtp, 100) : 0
+  const etpFon = etpAffected.length > 1 ? fixDecimal(etpAffected[1].totalEtp, 100) : 0
+  const etpCont = etpAffected.length > 2 ? fixDecimal(etpAffected[2].totalEtp, 100) : 0
 
   // Temps moyens par dossier observé = (nb heures travaillées par mois) / (sorties moyennes par mois / etpt sur la periode)
   const magRealTimePerCase = fixDecimal(((config.nbDaysByMagistrat / 12) * config.nbHoursPerDayAndMagistrat) / (totalOut / etpMag))
@@ -337,6 +337,7 @@ const calculateActivities = (referentielId, totalIn, lastStock, magEtpAffected, 
   }
 
   if (magCalculateTimePerCase) {
+    console.log('Calc=>', magEtpAffected, config, magCalculateTimePerCase)
     magCalculateOut = Math.floor(Math.floor(magEtpAffected * config.nbHoursPerDayAndMagistrat * (config.nbDaysByMagistrat / 12)) / magCalculateTimePerCase)
     fonCalculateOut = Math.floor(
       Math.floor(fonEtpAffected * config.nbHoursPerDayAndFonctionnaire * (config.nbDaysByFonctionnaire / 12)) / fonCalculateTimePerCase
