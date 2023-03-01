754f255e6997ea3568e95b0fa868176f69a3ded0
rzafari42 <redwane.zafari@hotmail.com>
2023-03-01 19:08:42 +0100

    changed logger to adapt to datadog needs

--- api/src/utils/log.js
+++ api/src/utils/log.js
@@ -1,46 +1,43 @@
-import winston from "winston";
-import config from "config";
-import Sentry from "winston-sentry-log";
-import packageJson from "../../package.json";
+import winston from 'winston'
+import config from 'config'
+import Sentry from 'winston-sentry-log'
+import packageJson from '../../package.json'
 
 const logger = winston.createLogger({
-  // level: 'info',
+  level: 'info',
+  exitOnError: false,
   format: winston.format.json(),
   // defaultMeta: { service: 'user-service' },
   transports: config.sentryApi
     ? [
-        new Sentry({
-          config: {
-            dsn: config.sentryApi,
-            environment: process.env.NODE_ENV || "developpement",
-            release: `${packageJson.name}@${packageJson.version}`,
-          },
-          level: "error",
-        }),
-        //
-        // - Write all logs with level `error` and below to `error.log`
-        // - Write all logs with level `info` and below to `combined.log`
-        //
-        //new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        //new winston.transports.File({ filename: 'combined.log' }),
-      ]
+      new Sentry({
+        config: {
+          dsn: config.sentryApi,
+          environment: process.env.NODE_ENV || 'developpement',
+          release: `${packageJson.name}@${packageJson.version}`,
+        },
+        level: 'error',
+      }),
+      //
+      // - Write all logs with level `error` and below to `error.log`
+      // - Write all logs with level `info` and below to `combined.log`
+      //
+      //new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      //new winston.transports.File({ filename: 'combined.log' }),
+      new winston.transports.File({ filename: 'error.log' }),
+    ]
     : [],
-});
+})
 
 logger.add(
   new winston.transports.Console({
     format: winston.format.combine(
       winston.format.colorize(),
-      winston.format.printf(
-        ({ level, message, ...meta }) =>
-          `${level}: ${JSON.stringify(message, null, 4)} ${JSON.stringify(
-            meta
-          )}\n`
-      )
+      winston.format.printf(({ level, message, ...meta }) => `${level}: ${JSON.stringify(message, null, 4)} ${JSON.stringify(meta)}\n`)
     ),
   })
-);
+)
 
-export default logger;
-export const log = (...args) => console.log(...args);
-export const logError = (...args) => logger.error(args);
+export default logger
+export const log = (...args) => console.log(...args)
+export const logError = (...args) => logger.error(args)
