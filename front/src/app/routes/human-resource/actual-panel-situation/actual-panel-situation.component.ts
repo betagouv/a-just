ae651e66d27551a51a59fed8b6ccdbd91e9dd9b2
François-Xavier Montigny <fxaviermontigny@gmail.com>
2023-03-29 18:01:33 +0200

    update profil comment panel

--- front/src/app/routes/human-resource/actual-panel-situation/actual-panel-situation.component.ts
+++ front/src/app/routes/human-resource/actual-panel-situation/actual-panel-situation.component.ts
@@ -5,16 +5,12 @@ import {
   OnChanges,
   Output,
 } from '@angular/core'
-import { sumBy } from 'lodash'
-import { HRFonctionInterface } from 'src/app/interfaces/hr-fonction'
 import { HumanResourceInterface } from 'src/app/interfaces/human-resource-interface'
 import { RHActivityInterface } from 'src/app/interfaces/rh-activity'
 import { MainClass } from 'src/app/libs/main-class'
 import { HRCommentService } from 'src/app/services/hr-comment/hr-comment.service'
 import { HumanResourceService } from 'src/app/services/human-resource/human-resource.service'
 import { today } from 'src/app/utils/dates'
-import { fixDecimal } from 'src/app/utils/numbers'
-import { etpLabel } from 'src/app/utils/referentiel'
 
 /**
  * Panneau de la situation actuelle
@@ -78,29 +74,15 @@ export class ActualPanelSituationComponent
    */
   timeWorked: string = ''
   /**
-   * Commentaire de la fiche
-   */
-  comment: string = ''
-  /**
-   * Date de mise à jours du commentaire
-   */
-  commentUpdatedAt: Date | null = null
-  /**
-   * instance créé lors de la modification d'une fiche
-   */
-  timeoutUpdateComment: any = null
-  /**
    * Liste des activités de la situation
    */
   activities: RHActivityInterface[] = []
 
   /**
    * Constructeur
-   * @param hRCommentService
    * @param humanResourceService 
    */
   constructor(
-    private hRCommentService: HRCommentService,
     private humanResourceService: HumanResourceService
   ) {
     super()
@@ -115,41 +97,5 @@ export class ActualPanelSituationComponent
       today()
     )
     this.activities = (findSituation && findSituation.activities) || []
-
-    this.onLoadComment()
-  }
-
-  /**
-   * Chargement du commentaire d'une fiche
-   */
-  onLoadComment() {
-    if (this.currentHR) {
-      this.hRCommentService.getHRComment(this.currentHR.id).then((result) => {
-        this.comment = (result && result.comment) || ''
-        this.commentUpdatedAt =
-          result && result.updatedAt ? new Date(result.updatedAt) : null
-      })
-    }
-  }
-
-  /**
-   * Mise à jour du commentaire de la fiche
-   * @param comment 
-   */
-  updateComment(comment: string) {
-    if (this.timeoutUpdateComment) {
-      clearTimeout(this.timeoutUpdateComment)
-      this.timeoutUpdateComment = null
-    }
-
-    this.timeoutUpdateComment = setTimeout(() => {
-      if (this.currentHR) {
-        this.hRCommentService
-          .updateHRComment(this.currentHR.id, comment)
-          .then((result) => {
-            this.commentUpdatedAt = result ? new Date(result) : null
-          })
-      }
-    }, 1000)
   }
 }
