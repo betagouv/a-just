c9e5909155c72e352ef5df45d126dcaa5b6659b7
François-Xavier Montigny <fxaviermontigny@gmail.com>
2023-04-14 11:58:58 +0200

    limit juridiction we can view

--- api/src/models/TableTj.js
+++ api/src/models/TableTj.js
@@ -1,19 +1,39 @@
 export default (sequelizeInstance, Model) => {
-  Model.getAll = () => {
-    return Model.findAll({
+  Model.getAll = async () => {
+    return await Model.findAll({
       attributes: ['id', ['i_elst', 'iElst'], 'label', 'latitude', 'longitude', 'population', 'enabled'],
       order: [['label', 'asc']],
     })
   }
 
-  Model.getAllVisibles = () => {
-    return Model.findAll({
+  Model.getAllVisibles = async () => {
+    return await Model.findAll({
       attributes: ['id', ['i_elst', 'iElst'], 'label', 'latitude', 'longitude', 'population', 'enabled'],
       where: {
         enabled: true,
       },
       order: [['label', 'asc']],
     })
+  }
+
+  /**
+   * Retourne les juridictions qui peuvent s'afficher si seulement elles sont bloqués via ce tableau
+   * @param {*} juridictionLabel
+   * @returns
+   */
+  Model.isVisible = async (juridictionLabel) => {
+    const TJFinded = await Model.findOne({
+      where: {
+        label: juridictionLabel,
+      },
+      raw: true,
+    })
+
+    if (!TJFinded || (TJFinded && TJFinded.enabled)) {
+      return true
+    }
+
+    return false
   }
 
   return Model
