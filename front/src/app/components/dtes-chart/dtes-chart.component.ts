d67be8fd01952161a78170da448994957e230c93
MrJimmyChevallier <jimmy.chevallier@free.fr>
2023-05-09 00:13:17 +0200

    test chart

--- front/src/app/components/dtes-chart/dtes-chart.component.ts
+++ front/src/app/components/dtes-chart/dtes-chart.component.ts
@@ -587,7 +587,25 @@ export class DtesChartComponent {
     setTimeout(()=>{
       this.myChart = new Chart(
         document.getElementById('dtes-chart') as ChartItem,
-        config
+        {
+          type: 'line',
+          data: {
+            datasets: [{
+                data: [0, 0],
+            }, {
+                data: [0, 1]
+            }, {
+                data: [1, 0],
+                showLine: true // overrides the `line` dataset default
+            }, {
+                type: 'scatter', // 'line' dataset default does not affect this dataset since it's a 'scatter'
+                data: [1, 1]
+            }]
+        },
+          options: {},
+          plugins: []
+        }
+        //config
       )
     },10000)
 
