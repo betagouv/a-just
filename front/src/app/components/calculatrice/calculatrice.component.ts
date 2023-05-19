e904da484c9bf2a33219bf6e84055431205e173b
MrJimmyChevallier <jimmy.chevallier@free.fr>
2023-05-19 16:29:17 +0200

    finish design front

--- front/src/app/components/calculatrice/calculatrice.component.ts
+++ front/src/app/components/calculatrice/calculatrice.component.ts
@@ -1,25 +1,43 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
-const calculatriceTabs = ['Vacations','Volumes']
+const calculatriceTabs = ['Vacations', 'Volumes']
 @Component({
   selector: 'aj-calculatrice',
   templateUrl: './calculatrice.component.html',
-  styleUrls: ['./calculatrice.component.scss']
+  styleUrls: ['./calculatrice.component.scss'],
 })
 export class CalculatriceComponent {
-/**
- * Choix du paneau ouvert
- */
-  selectedTab = 'Vacations'
+  /**
+   * Option radio button
+   */
+  radioItems = 'semaine mois an'.split(' ')
+
+  /**
+   * Model utilisé pour les calculs d'ETPT
+   */
+  model = { 
+    vacation : { value : null, option:'semaine',unit: null},
+    volume : { value : null, option:'semaine'},
+    selectedTab : 'vacation'
+  }
 
   /**
    * Ouvre l'onglet cliqué
-   * @param tabName 
+   * @param tabName
    */
-  openTab(tabName : string) {
-    this.selectedTab = tabName
+  openTab(tabName: string) {
+    this.model.selectedTab = tabName
   }
 
-  radioItems = 'one two three'.split(' ');
-  model = { options: 'two' };
+  validateNumber(e: any) {
+    const charCode = e.which ? e.which : e.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false
+    }
+    return true
+  }
+
+  print() {
+    console.log(this.model)
+  }
 }
