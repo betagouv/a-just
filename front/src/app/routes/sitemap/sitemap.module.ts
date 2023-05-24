201364f37d2e079becf318f5741e8d6bbb4574b8
François-Xavier Montigny <fxaviermontigny@gmail.com>
2023-05-24 14:28:05 +0200

    decreasee size of build

--- front/src/app/routes/sitemap/sitemap.module.ts
+++ front/src/app/routes/sitemap/sitemap.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { ComponentsModule } from 'src/app/components/components.module';
 import { SiteMapPage } from './sitemap.page';
 import { SiteMapPageModule } from './sitemap.routing';
+import { WrapperNoConnectedModule } from 'src/app/components/wrapper-no-connected/wrapper-no-connected.module';
 
 @NgModule({
 	declarations: [ SiteMapPage ],
-	imports: [ SiteMapPageModule, FormsModule, ReactiveFormsModule, RouterModule, ComponentsModule ]
+	imports: [ SiteMapPageModule, FormsModule, ReactiveFormsModule, RouterModule, WrapperNoConnectedModule ]
 })
 export class SiteMapModule {}
