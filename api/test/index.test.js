6a2f9c72f9111d59901fe81764a5bf78d2e27c88
François-Xavier Montigny <fxaviermontigny@gmail.com>
2023-05-30 17:32:10 +0200

    update tests

--- api/test/index.test.js
+++ api/test/index.test.js
@@ -1,63 +1,74 @@
-import { default as server } from "../src/index"; //'../dist/index'
-import routeIndex from "./api/Route.test";
-/*import routeUser from './api/RouteUser.test'
+import { default as server } from '../src/index'
+import routeUser from './api/RouteUser.test'
 import routeChangeUserData from './api/RouteChangeUserData.test'
-import routeCalcultator from './api/RouteCalculateur.test'*/
-import routeVentilateur from "./api/RouteVentilateur.test";
-import axios from "axios";
-import { assert } from "chai";
+import routeCalcultator from './api/RouteCalculateur.test'
+import routeVentilateur from './api/RouteVentilateur.test'
+import axios from 'axios'
+import { assert } from 'chai'
 
-console.log("EnvName:", config.envName);
-let server = null;
-if (config.envName === "test") {
-  console.log("TEST ENV");
-  server = await import("../src/index");
-} else {
-  console.log("PROD ENV");
-  server = await import("../dist/index");
-}
+console.warn = () => {}
+console.error = () => {}
 
 /*import routeConnexion from './api/RouteConnexion'
 import routeImport from './api/RouteImports.test'
 import routeHR from './api/RouteHR.test'
 import routeActivities from './api/RouteActivities.test'
 import RouteContentieuxOptions from './api/RouteContentieuxOptions.test'*/
-import config from "config";
-import { USER_ADMIN_EMAIl, USER_ADMIN_PASSWORD } from "./constants/admin";
-//import { USER_TEST_EMAIL, USER_TEST_FIRSTNAME, USER_TEST_FONCTION, USER_TEST_LASTNAME, USER_TEST_PASSWORD } from './constants/user'
+import config from 'config'
+import { USER_ADMIN_EMAIl, USER_ADMIN_PASSWORD } from './constants/admin'
+import { USER_TEST_EMAIL, USER_TEST_FIRSTNAME, USER_TEST_FONCTION, USER_TEST_LASTNAME, USER_TEST_PASSWORD } from './constants/user'
 
-describe("Test server is ready", () => {
+const datas = {
+  adminToken: null,
+  userId: null,
+  userToken: null,
+}
+
+describe('Test server is ready', () => {
   beforeEach(() => {
     //sinon.stub(console, 'error').returns(undefined)
-  });
+  })
   before((done) => {
-    console.log("BEFORE WAITING SERVER");
+    console.log('BEFORE WAITING SERVER')
     server.isReady = function () {
-      console.log("config", config);
-      done();
-    };
-  });
+      console.log('config', config)
 
-  let adminToken = null;
-  //let userId = null
-  let userToken = null;
+      done()
+    }
+  })
 
-  /**
-   * Connect Admin
-   */
-  it("Login - Login admin", async () => {
-    const email = USER_ADMIN_EMAIl;
-    const password = USER_ADMIN_PASSWORD;
+  it('Login - Login admin', async () => {
+    const email = USER_ADMIN_EMAIl
+    const password = USER_ADMIN_PASSWORD
 
     // Connexion de l'admin
     const response = await axios.post(`${config.serverUrl}/auths/login-admin`, {
       email,
       password,
-    });
+    })
+    console.log(datas)
     // Récupération du token associé pour l'identifier
-    adminToken = response.status === 201 && response.data.token;
-    assert.strictEqual(response.status, 201);
-  });
+    datas.adminToken = response.status === 201 && response.data.token
+    console.log('adminToken', datas.adminToken)
+    assert.strictEqual(response.status, 201)
+  })
+
+  /**
+   * Connect Admin
+   */
+  /*it('Login - Login admin', async () => {
+    const email = USER_ADMIN_EMAIl
+    const password = USER_ADMIN_PASSWORD
+
+    // Connexion de l'admin
+    const response = await axios.post(`${config.serverUrl}/auths/login-admin`, {
+      email,
+      password,
+    })
+    // Récupération du token associé pour l'identifier
+    adminToken = response.status === 201 && response.data.token
+    assert.strictEqual(response.status, 201)
+  })*/
 
   /**
    *  Vérification que l'utilisateur peut bien s'inscrire si toutes les information obligatoires sont corrects
@@ -87,22 +98,24 @@ describe("Test server is ready", () => {
     })
     userToken = response.status === 201 && response.data.token
     userId = response.data.user.id
+    console.log('USER-TOKEN_00:', userToken)
+    console.log('ADMIN-TOKEN_00:', adminToken)
 
     assert.isOk(userToken, 'response 201 and user token created')
   })*/
 
-  // TROUVER UN MOYEN D'ATTENDRE QUE USERTOKEN ET ADMINTOKEN SOIENT SET AVANT DE LANCER LES TESTS SUIVANTS
-
-  routeIndex();
-  /*routeUser(userToken, adminToken)
+  routeUser(datas)
   routeChangeUserData()
-  routeCalcultator()*/
-  routeVentilateur();
+  routeCalcultator()
+  routeVentilateur()
+
+  // TROUVER UN MOYEN D'ATTENDRE QUE USERTOKEN ET ADMINTOKEN SOIENT SET AVANT DE LANCER LES TESTS SUIVANTS
 
   /*routeImport()
   routeHR()
   routeActivities()
   RouteContentieuxOptions()*/
+
   /**
    * Vérification qu'un admin puisse bien surrpimer le compte d'un utilisateur
    */
@@ -118,6 +131,6 @@ describe("Test server is ready", () => {
   })*/
 
   after(function () {
-    server.done();
-  });
-});
+    server.done()
+  })
+})
