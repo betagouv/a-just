e009b0a5512ed658da7e35fe44626a8024191753
François-Xavier Montigny <fxaviermontigny@gmail.com>
2023-06-02 14:00:12 +0200

    add category tags to panoram

--- front/src/app/routes/panorama/panorama.page.ts
+++ front/src/app/routes/panorama/panorama.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { MainClass } from 'src/app/libs/main-class'
+import { UserService } from 'src/app/services/user/user.service'
+import { userCanViewContractuel, userCanViewGreffier, userCanViewMagistrat } from 'src/app/utils/user'
 
 /**
  * Page de la liste des fiches (magistrats, greffier ...)
@@ -8,11 +10,48 @@ import { MainClass } from 'src/app/libs/main-class'
   templateUrl: './panorama.page.html',
   styleUrls: ['./panorama.page.scss'],
 })
-export class PanoramaPage extends MainClass {
+export class PanoramaPage extends MainClass implements OnInit, OnDestroy {
+  /**
+   * Date selected
+   */
+  dateSelected: Date = new Date()
+  /**
+   * Peux voir l'interface magistrat
+   */
+  canViewMagistrat: boolean = false
+  /**
+   * Peux voir l'interface greffier
+   */
+  canViewGreffier: boolean = false
+  /**
+   * Peux voir l'interface contractuel
+   */
+  canViewContractuel: boolean = false
+
   /**
    * Constructor
    */
-  constructor() {
+  constructor(private userService: UserService) {
     super()
+  }
+
+   /**
+   * Initialisation des datas au chargement de la page
+   */
+   ngOnInit() {
+    this.watch(
+      this.userService.user.subscribe((u) => {
+        this.canViewMagistrat = userCanViewMagistrat(u)
+        this.canViewGreffier = userCanViewGreffier(u)
+        this.canViewContractuel = userCanViewContractuel(u)
+      })
+    )
+  }
+
+  /**
+   * Destruction du composant
+   */
+  ngOnDestroy() {
+    this.watcherDestroy()
   }
 }
