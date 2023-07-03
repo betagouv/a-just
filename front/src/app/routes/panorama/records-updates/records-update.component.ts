8bcadc0317ed36a79e229de7153313e7bc7578b5
rzafari42 <redwane.zafari@hotmail.com>
2023-07-03 11:10:46 +0200

    Panorama: records update section almost done (missing links)

--- front/src/app/routes/panorama/records-updates/records-update.component.ts
+++ front/src/app/routes/panorama/records-updates/records-update.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core'
 import { MainClass } from 'src/app/libs/main-class'
 import { UserService } from 'src/app/services/user/user.service'
+import { HRCategorySelectedInterface } from 'src/app/interfaces/hr-category'
 import { userCanViewContractuel, userCanViewGreffier, userCanViewMagistrat } from 'src/app/utils/user'
 
 /**
@@ -12,7 +13,85 @@ import { userCanViewContractuel, userCanViewGreffier, userCanViewMagistrat } fro
   styleUrls: ['./records-update.component.scss'],
 })
 export class RecordsUpdateComponent extends MainClass implements OnInit, OnDestroy {
-
+  /**
+   * liste des catégories d'effectif
+   */
+  categoriesList: HRCategorySelectedInterface[] = [ {
+    id: 1,
+    label: "Siège",
+    textColor: '#000091',
+    bgColor: '#e3e3fd',
+    hoverColor: 'blue',
+    selected: true,
+    etpt: 119.48,
+    nbPersonal: 122,
+    labelPlural: "",
+    headerLabel: 'Siège',
+    percentAllocated: 71.28,
+    lastUpdate: '12 Avril 2023',
+    poste: [ {
+      name: 'titulaires',
+      selected: true,
+      etpt: 105.48,
+      nbPersonal: 105,
+    }, {
+      name: 'placés',
+      selected: true,
+      etpt: 10.52,
+      nbPersonal: 12,
+    },  {
+      name: 'contractuels',
+      selected: true,
+      etpt: 3.48,
+      nbPersonal: 5,
+    }],
+  },
+  {
+    id: 2,
+    label: "Greffe",
+    textColor: '#a558a0',
+    bgColor: '#fee7fc',
+    hoverColor: 'purple',
+    selected: true,
+    etpt: 6.20,
+    nbPersonal: 9,
+    labelPlural: "",
+    headerLabel: 'Greffe',
+    percentAllocated: 20,
+    lastUpdate: '24 Mars 2023',
+    poste: [ {
+      name: 'titulaires',
+      selected: true,
+      etpt: 105.48,
+      nbPersonal: 105,
+    }, {
+      name: 'placés',
+      selected: true,
+      etpt: 10.52,
+      nbPersonal: 12,
+    },  {
+      name: 'contractuels',
+      selected: true,
+      etpt: 3.48,
+      nbPersonal: 5,
+    }],
+  },
+  {
+    id: 3,
+    label: "Autour du magistrat",
+    textColor: '#796830',
+    bgColor: '#fef6e3',
+    hoverColor: 'yellow',
+    selected: true,
+    etpt: 4.20,
+    nbPersonal: 5,
+    labelPlural: "",
+    headerLabel: 'Autour du magistrat',
+    percentAllocated: 0,
+    lastUpdate: '16 Mars 2023',
+    poste: [ ],
+  }
+]
 
   /**
    * Constructor
