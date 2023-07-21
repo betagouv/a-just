11eaf9cda95b74905a68b7672103f6240e61d805
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2023-07-21 09:13:48 +0200

    add ETP to update

--- front/src/app/constants/colors.ts
+++ front/src/app/constants/colors.ts
@@ -2,6 +2,10 @@
  * RED COLOR
  */
 export const RED_COLOR = 'rgb(255, 86, 85)'
+/**
+ * PLACEHOLDER_COLOR
+ */
+export const PLACEHOLDER_COLOR = 'rgb(246, 246, 246)'
 
 export const GET_COLOR = (color: string, opacity: number = 1) => {
   if (color.startsWith('rgb(')) {
