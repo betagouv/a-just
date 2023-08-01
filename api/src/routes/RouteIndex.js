0b0cd618d5d5ec9a8f36843a5c8143cf7bd49aff
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2023-08-01 12:03:35 +0200

    update ssl

--- api/src/routes/RouteIndex.js
+++ api/src/routes/RouteIndex.js
@@ -1,6 +1,7 @@
 import { createReadStream, existsSync } from 'fs'
 import mime from 'mime'
 import Route from './Route'
+import config from 'config'
 
 @Route.Route({
   routeBase: '',
@@ -15,6 +16,16 @@ export default class RouteIndex extends Route {
     path: '*',
   })
   async readFile(ctx) {
+    console.log('config', config)
+    if (!config.forceURL && ctx.request?.header?.referer && ctx.request.header.referer.startsWith('http:')) {
+      ctx.res
+        .writeHead(301, {
+          Location: ctx.request.header.referer.replace('http', 'https'),
+        })
+        .end()
+    }
+
+    console.log(ctx)
     let file = `${__dirname}/../front${decodeURIComponent(ctx.request.url)}`
     const fileSplited = file.split('?')
     file = fileSplited.length > 1 ? fileSplited.slice(0, -1).join('?') : file
