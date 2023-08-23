1b6ec66cebe6ec253e12debaa8b394fd759ff49d
rzafari42 <redwane.zafari@hotmail.com>
2023-08-23 10:45:34 +0200

    xlsx module added

--- front-admin/src/app/services/extracts-data/extracts-data.service.ts
+++ front-admin/src/app/services/extracts-data/extracts-data.service.ts
@@ -16,8 +16,15 @@ export class ExtractsDataService {
     }
   }
 
-  extractdata({backupId, dateStart, dateStop} : {backupId: number, dateStart: Date, dateStop: Date}) {
+  /**
+   * LIste des juridictions ayant ajusté leur données d'activité
+   */
+  getJuridictionAjusted({dateStart, dateStop} : {dateStart: Date, dateStop: Date}) {
+    console.log('[exrtractData][line 20] ')
+    return this.serverService.post(`extractor/juridiction-ajusted-data-list` , { dateStart, dateStop }).then(res => console.log('res:', res)).catch(err => console.log('err:', err))
+  }
 
+  extractdata({backupId, dateStart, dateStop} : {backupId: number, dateStart: Date, dateStop: Date}) {
     return this.serverService.post(`extractor/filter-list-act`, {
       backupId,
       dateStart,
