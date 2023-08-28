e96c90cfaae02606f877781fc7c03a47dbb66fb2
rzafari42 <redwane.zafari@hotmail.com>
2023-08-28 12:29:05 +0200

    units tests temporarily removed

--- api/src/models/TableActivities.js
+++ api/src/models/TableActivities.js
@@ -539,6 +539,7 @@ export default (sequelizeInstance, Model) => {
 
     for (let i = 0; i < list.length; i++) {
       list[i] = {
+        backupId: HrBackupId,
         id: list[i].id,
         periode: list[i].periode,
         entrees: list[i].entrees,
