8572a08d96595b0954ce88c9d0f2ec04813c5fd4
MrJimmyChevallier <103670713+MrJimmyChevallier@users.noreply.github.com>
2023-10-19 12:57:56 +0200

    add suppression NAC

--- front-admin/src/app/app.module.ts
+++ front-admin/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
--- front-admin/src/app/components/components.module.ts
+++ front-admin/src/app/components/components.module.ts
@@ -5,15 +5,25 @@ import { WrapperComponent } from './wrapper/wrapper.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../libs/material.module';
 import { PopupComponent } from './popup/popup.component';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { SelectComponent } from './select/select.component';
+import { MatListModule } from '@angular/material/list';
 
 const list = [
   WrapperComponent,
   PopupComponent,
+  SelectComponent
 ];
 
 @NgModule({
   declarations: [...list],
-  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, MaterialModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, MaterialModule, MatSelectModule,
+    MatOptionModule,
+    MatIconModule,
+    MatListModule
+  ],
   exports: list,
 })
-export class ComponentsModule {}
+export class ComponentsModule { }
