1a5d15c2dd4201dd51a01004602c8684fea38666
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2023-10-25 17:55:49 +0200

    welcome page

---
+++ front/src/app/routes/welcome/welcome.page.ts
@@ -0,0 +1,44 @@
+import { AfterViewInit, Component } from '@angular/core'
+import { MEETING_URL } from 'src/app/constants/pages'
+
+/**
+ * Page pour onboarder des nouveaux utilisateurs
+ */
+@Component({
+  templateUrl: './welcome.page.html',
+  styleUrls: ['./welcome.page.scss'],
+})
+export class WelcomePage implements AfterViewInit {
+  MEETING_URL = MEETING_URL
+
+  constructor() {}
+
+  ngAfterViewInit() {
+    const my_awesome_script = document.createElement('script')
+    my_awesome_script.setAttribute('type', 'text/javascript')
+    my_awesome_script.setAttribute(
+      'src',
+      'https://assets.calendly.com/assets/external/widget.js'
+    )
+    document.head.appendChild(my_awesome_script)
+
+    this.loadCalendly()
+  }
+
+  loadCalendly() {
+    // @ts-ignore
+    if (window.Calendly) {
+      // @ts-ignore
+      window.Calendly.initInlineWidget({
+        url: 'https://calendly.com/support-a-just/support',
+        parentElement: document.getElementById('calendly'),
+        prefill: {},
+        utm: {},
+      })
+    } else {
+      setTimeout(() => {
+        this.loadCalendly()
+      }, 200)
+    }
+  }
+}
--- front/src/app/routes/welcome/welcome.routing.ts
+++ front/src/app/routes/welcome/welcome.routing.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NeedMeetingPage } from './need-meeting.page';
+import { WelcomePage } from './welcome.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: NeedMeetingPage,
+    component: WelcomePage,
   },
 ];
 
@@ -13,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class NeedMeetingPageModule {}
+export class WelcomePageModule {}
