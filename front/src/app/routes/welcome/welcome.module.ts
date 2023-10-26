3da37359d6768c1517988df8bf36143a09d2b243
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2023-10-26 14:07:09 +0200

    fix calendly

--- front/src/app/routes/welcome/welcome.module.ts
+++ front/src/app/routes/welcome/welcome.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { WelcomePage } from './welcome.page'
 import { WelcomePageModule } from './welcome.routing'
-import { ComponentsModule } from 'src/app/components/components.module'
 import { CommonModule } from '@angular/common'
 import { MaterialModule } from 'src/app/libs/material.module'
 import { WrapperModule } from 'src/app/components/wrapper/wrapper.module'
-import { PipesModule } from 'src/app/pipes/pipes.module'
+import { WrapperNoConnectedModule } from 'src/app/components/wrapper-no-connected/wrapper-no-connected.module'
 
 @NgModule({
   declarations: [
@@ -15,11 +14,10 @@ import { PipesModule } from 'src/app/pipes/pipes.module'
   imports: [
     WelcomePageModule,
     RouterModule,
-    ComponentsModule,
+    WrapperNoConnectedModule,
     CommonModule,
     MaterialModule,
-    WrapperModule,
-    PipesModule
+    WrapperModule
   ],
 })
 export class WelcomeModule {}
