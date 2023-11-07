54ed67a02c495c4d80d6e2d3ad5799d68ab9b06e
MrJimmyChevallier <103670713+MrJimmyChevallier@users.noreply.github.com>
2023-11-07 12:06:43 +0100

    add centre d'aide final

--- front/src/app/routes/help-center/help-center.page.ts
+++ front/src/app/routes/help-center/help-center.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core'
 
 import { GitBookAPI } from '@gitbook/api';
+import { DocCardInterface } from 'src/app/components/doc-card/doc-card.component';
+import { CALCULATE_DOWNLOAD_URL, DATA_GITBOOK, DOCUMENTATION_URL } from 'src/app/constants/documentation';
 
 /**
  * Contact
@@ -31,6 +33,65 @@ export class HelpCenterPage {
    * Focus barre de recherche
    */
   focusOn = false
+  /**
+   * URL de la nomenclature
+   */
+  NOMENCLATURE_DOWNLOAD_URL = '/assets/nomenclature-A-Just.html'
+  /**
+   * Cards documentation
+   */
+  docCards: Array<DocCardInterface> = [
+    {
+      tag: 'Documentation',
+      title: 'Le guide utilisateur',
+      description: 'Retrouvez la présentation des grandes fonctionnalités d’A-JUST que vous soyez débutant ou utilisateur avancé !',
+      image: '/assets/images/avatar.svg',
+      url: DOCUMENTATION_URL
+    },
+    {
+      tag: 'Documentation',
+      title: 'Le data-book',
+      description: 'Ce guide détaille la source, et les requêtes permettant la préalimentation de chacune des « données logiciel » de la rubrique « Données d\'activité. ».',
+      image: '/assets/images/data-visualization.svg',
+      url: DATA_GITBOOK
+    },
+    {
+      tag: 'Documentation',
+      title: 'La nomenclature',
+      description: 'Vous permet de visualiser globalement et en détail le contenu de chaque contentieux et sous-contentieux. Au civil, vous y retrouverez la liste des NAC prises en compte dans chaque rubrique.',
+      image: '/assets/images/system.svg',
+      url: this.NOMENCLATURE_DOWNLOAD_URL
+    }
+  ]
+  /**
+   * Cards outils
+   */
+  docTools: Array<DocCardInterface> = [
+    {
+      tag: 'Les outils A-JUST',
+      title: 'La calculatrice de ventilation des ETPT',
+      description: '',
+      image: '/assets/images/coding.svg',
+      url: CALCULATE_DOWNLOAD_URL,
+    },
+    {
+      tag: 'Les outils A-JUST',
+      title: 'L’extracteur de données d’effectifs',
+      description: '',
+      image: '/assets/images/Tableur.svg',
+      url: '/dashboard',
+      localUrl: true
+    },
+    {
+      tag: 'Les outils A-JUST',
+      title: 'L’extracteur de données d’activités',
+      description: '',
+      image: '/assets/images/Tableur2.svg',
+      url: '/dashboard',
+      localUrl: true
+    }
+  ]
+
 
   /**
    * Constructeur
@@ -79,4 +140,5 @@ export class HelpCenterPage {
   delay() {
     setTimeout(() => { this.focusOn = false }, 200)
   }
+  //AJOUTER CARTE INDIVIDUEL COMPOSANT ET DEMANDER EXPORT MICKAEL
 }
