c44929fc10a2ef24c2348c922885eab8e9595237
MrJimmyChevallier <103670713+MrJimmyChevallier@users.noreply.github.com>
2023-11-09 11:42:45 +0100

    add security check for gitbook

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -20,6 +20,7 @@ export const environment = {
   supportEmail: 'support-utilisateurs@a-just.fr',
   mapboxToken: 'pk.eyJ1IjoiZnhiZXRhIiwiYSI6ImNsZzZldHhvMTAzanEzc3BrYXJ5dXA1czYifQ.FlyDfsXjidTxi7XE50a07w',
   mapboxStyle: 'mapbox://styles/fxbeta/clm7xuvg4012d01pb1jc6887h',
+  gitbookToken: 'gb_api_rHt5wBv5WBzk2mjUuO2QymdKNpNujdBD8TLfYpsA'
   //typeId: process.env['TYPE_ID'] || 0,
 }
 
