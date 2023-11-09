219d37cf857f93cc34dc07d078aac234106f352c
MrJimmyChevallier <103670713+MrJimmyChevallier@users.noreply.github.com>
2023-11-09 13:25:11 +0100

    remove logs

--- front/src/app/components/wrapper/wrapper.component.ts
+++ front/src/app/components/wrapper/wrapper.component.ts
@@ -260,7 +260,6 @@ export class WrapperComponent extends MainClass implements OnDestroy {
    * @returns
    */
   isSelected(item: any) {
-    console.log(item)
     return `/${item.path}` === window.location.pathname
   }
 
