e463be2fb63446d7dc9b7cf058c00ae29fa6e652
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2023-11-17 10:35:38 +0100

    add logs

--- api/src/routes-api/RouteHumanResources.js
+++ api/src/routes-api/RouteHumanResources.js
@@ -243,7 +243,7 @@ export default class RouteHumanResources extends Route {
 
     const allCategories = await this.models.HRCategories.getAll()
 
-    console.log('contentieuxIds', contentieuxIds)
+    console.log('contentieuxIds', contentieuxIds, categoriesIds.length, allCategories.length)
     if (categoriesIds && categoriesIds.length === allCategories.length && (!contentieuxIds || contentieuxIds.length === 0)) {
       // memorize first execution by user
       this.models.Logs.addLog(EXECUTE_VENTILATION, ctx.state.user.id)
