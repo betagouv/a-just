d0c6158aaed075bb2968c6b45c79f132f2d71bfb
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2023-12-26 12:07:02 +0100

    force to download doc

--- front/src/app/utils/system.ts
+++ front/src/app/utils/system.ts
@@ -14,3 +14,14 @@ export function iIOS() {
   // iPad on iOS 13 detection
   || (navigator.userAgent.includes("Mac"))
 }
+
+/**
+ * Force to download file
+ * @param filePath 
+ */
+export function downloadFile(filePath: string){
+  var link=document.createElement('a');
+  link.href = filePath;
+  link.download = filePath.substr(filePath.lastIndexOf('/') + 1);
+  link.click();
+}
