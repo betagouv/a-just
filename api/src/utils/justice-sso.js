63d7db5bc5c8e425b3dafb11e2a6ce15c679ee5b
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2024-01-18 16:21:35 +0100

    add local sso interface

--- api/src/utils/justice-sso.js
+++ api/src/utils/justice-sso.js
@@ -3,10 +3,10 @@ import saml2 from 'saml2-js'
 import config from 'config'
 
 var sp_options = {
-  entity_id: `${config.ssl.url}/metadata.xml`,
-  private_key: readFileSync('key-file.pem').toString(),
-  certificate: readFileSync('cert-file.crt').toString(),
-  assert_endpoint: `${config.ssl.url}/assert`,
+  entity_id: `${config.serverUrl}/saml/metadata.xml`,
+  private_key: readFileSync(`${__dirname}/../../certificats/privateKey.pem`).toString(),
+  certificate: readFileSync(`${__dirname}/../../certificats/certificate.crt`).toString(),
+  assert_endpoint: `${config.serverUrl}/saml/assert`,
   force_authn: true,
   auth_context: { comparison: 'exact', class_refs: ['urn:oasis:names:tc:SAML:1.0:am:password'] },
   nameid_format: 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
@@ -15,11 +15,51 @@ var sp_options = {
 }
 
 // Call service provider constructor with options
-var sp = new saml2.ServiceProvider(sp_options)
+export const sp = new saml2.ServiceProvider(sp_options)
 
-// Example use of service provider.
-// Call metadata to get XML metatadata used in configuration.
-var metadata = sp.create_metadata()
+// Create identity provider
+var idp_options = {
+  sso_login_url: `${config.sso.url}`,
+  sso_logout_url: `${config.sso.url}/logout`,
+  //certificates: [readFileSync('cert-file1.crt').toString(), fs.readFileSync('cert-file2.crt').toString()],
+}
+export const idp = new saml2.IdentityProvider(idp_options)
 
-console.log('META DATA sp', sp)
-console.log('META DATA', metadata)
+export const loginSSO = () => {
+  return new Promise((resolve, reject) => {
+    sp.create_login_request_url(idp, {}, function (err, login_url, request_id) {
+      console.log(idp, err, login_url, request_id)
+      if (err != null) {
+        reject(err)
+      } else {
+        resolve(login_url)
+      }
+    })
+  })
+}
+
+export const logoutSSO = (options) => {
+  return new Promise((resolve, reject) => {
+    sp.create_logout_request_url(idp, options, function (err, logout_url) {
+      console.log(idp, err, logout_url)
+      if (err != null) {
+        reject(err)
+      } else {
+        resolve(logout_url)
+      }
+    })
+  })
+}
+
+export const postAssertSSO = (requestBody) => {
+  return new Promise((resolve, reject) => {
+    sp.post_assert(idp, { request_body: requestBody }, function (err, saml_response) {
+      console.log(saml_response)
+      if (err != null) {
+        reject(err)
+      } else {
+        resolve({ nameId: saml_response.user.name_id, sessionIndex: saml_response.user.session_index })
+      }
+    })
+  })
+}
