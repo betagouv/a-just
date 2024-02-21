98114d80295cee7570c03e8c2898a621167213f7
MrJimmyChevallier <103670713+MrJimmyChevallier@users.noreply.github.com>
2024-02-21 17:37:58 +0100

    Add ielts modified with exceptions CA

--- api/src/db/seeders/sandbox/202402191353-create-ca-fonction-gref-only.js
+++ api/src/db/seeders/sandbox/202402191353-create-ca-fonction-gref-only.js
@@ -35,6 +35,90 @@ module.exports = {
       {
         "FONCTION": "CM PP",
         "FONCTION_NL": "Chargé de mission au cabinet du premier président"
+      },
+      {
+        "FONCTION_NL": "A",
+        "FONCTION": "A"
+      },
+      {
+        "FONCTION_NL": "CHEF DE CABINET",
+        "FONCTION": "CHCAB"
+      },
+      {
+        "FONCTION_NL": "B GREFFIER",
+        "FONCTION": "B"
+      },
+      {
+        "FONCTION_NL": "SA",
+        "FONCTION": "SA"
+      },
+      {
+        "FONCTION_NL": "CB",
+        "FONCTION": "CB"
+      },
+      {
+        "FONCTION_NL": "CT",
+        "FONCTION": "CT"
+      },
+      {
+        "FONCTION_NL": "A PLACÉ",
+        "FONCTION": "A PLACÉ"
+      },
+      {
+        "FONCTION_NL": "B GREFFIER PLACÉ",
+        "FONCTION": "B GREF  PLACÉ"
+      },
+      {
+        "FONCTION_NL": "B PLACÉ",
+        "FONCTION": "B PLACÉ"
+      },
+      {
+        "FONCTION_NL": "CB PLACÉ",
+        "FONCTION": "CB PLACÉ"
+      },
+      {
+        "FONCTION_NL": "CT PLACÉ",
+        "FONCTION": "CT PLACÉ"
+      },
+      {
+        "FONCTION_NL": "GREFFIER RESERVISTE",
+        "FONCTION": "GRES"
+      },
+      {
+        "FONCTION_NL": "CONTRACTUEL A",
+        "FONCTION": "CONT A"
+      },
+      {
+        "FONCTION_NL": "CONTRACTUEL B",
+        "FONCTION": "CONT B"
+      },
+      {
+        "FONCTION_NL": "CONTRACTUEL C",
+        "FONCTION": "CONT C"
+      },
+      {
+        "FONCTION_NL": "CONTRACTUEL CB",
+        "FONCTION": "CONT CB"
+      },
+      {
+        "FONCTION_NL": "CONTRACTUEL CT",
+        "FONCTION": "CONT CT"
+      },
+      {
+        "FONCTION_NL": "VACATAIRE",
+        "FONCTION": "VAC"
+      },
+      {
+        "FONCTION_NL": "CONTRACTUEL A JUSTICE DE PROXIMITE",
+        "FONCTION": "CONT A JP"
+      },
+      {
+        "FONCTION_NL": "CONTRACTUEL B JUSTICE DE PROXIMITE",
+        "FONCTION": "CONT B JP"
+      },
+      {
+        "FONCTION_NL": "CONTRACTUEL C JUSTICE DE PROXIMITE",
+        "FONCTION": "CONT C JP"
       }
     ]
 
