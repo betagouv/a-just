72868b6c3462288b5890ab27bf1fac8e61838ad6
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2024-03-13 11:10:18 +0100

    change SSO variables

--- api/config/default.js
+++ api/config/default.js
@@ -151,6 +151,8 @@ module.exports = {
     testUrl: process.env.SSO_TEST_URL,
     privateKey: process.env.SSO_PRIVATE_KEY,
     publicKey: process.env.SSO_PUBLIC_KEY,
+    ssoExternalPublicKey: process.env.SSO_EXTERNAL_PUBLIC_KEY,
+    ssoExternalPrivateKey: process.env.SSO_EXTERNAL_PRIVATE_KEY,
   },
   login: {
     shareAuthCode: false,
@@ -170,7 +172,7 @@ module.exports = {
     overwrite: true /** (boolean) can overwrite or not (default true) */,
     httpOnly: true /** (boolean) httpOnly or not (default true) */,
     signed: true /** (boolean) signed or not (default true) */,
-    rolling: false /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */,
+    rolling: false,
     renew: false /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/,
     secure: false /** (boolean) secure cookie*/,
     sameSite: null /** (string) session cookie sameSite options (default null, don't set it) */,
