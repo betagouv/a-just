177f5b6eaff064e8cdd8c33ddc7b3d92d90413cd
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2024-03-19 10:37:18 +0100

    add saml to prod

---
+++ api/src/routes-api/RouteSaml.js
@@ -0,0 +1,143 @@
+import Route from './Route'
+import { Types } from '../utils/types'
+import { loginSSO, logoutSSO, postAssertSSO, sp } from '../utils/justice-sso'
+import config from 'config'
+import { SAML_STATUS_EMPTY, SAML_STATUS_PENDING } from '../constants/saml'
+import { USER_USER_LOGIN } from '../constants/log-codes'
+
+/**
+ * Route des SAML2
+ */
+export default class RouteSaml extends Route {
+  /**
+   * Constructeur
+   * @param {*} params
+   */
+  constructor (params) {
+    super({ ...params, model: 'Users' })
+  }
+
+  /**
+   * metadata saml
+   */
+  @Route.Get({
+    path: '/metadata.xml',
+  })
+  async metadata (ctx) {
+    ctx.type = 'application/xml'
+    ctx.body = sp.create_metadata()
+  }
+
+  /**
+   * login saml
+   */
+  @Route.Get()
+  async login (ctx) {
+    try {
+      const loginUrl = await loginSSO()
+      if (loginUrl) {
+        ctx.res
+          .writeHead(302, {
+            Location: loginUrl,
+          })
+          .end()
+      }
+    } catch (err) {
+      ctx.throw(401, err)
+    }
+  }
+
+  /**
+   * logout saml
+   */
+  @Route.Get()
+  async logout (ctx) {
+    try {
+      if (!ctx.session.sso.assert) {
+        throw 'Erreur de session'
+      }
+
+      const logoutUrl = await logoutSSO(ctx.session.sso.assert)
+      if (logoutUrl) {
+        ctx.res
+          .writeHead(302, {
+            Location: logoutUrl,
+          })
+          .end()
+      }
+    } catch (err) {
+      ctx.throw(401, err)
+    }
+  }
+
+  /**
+   * Get current session of user
+   * @param {*} request_body
+   */
+  @Route.Post({
+    bodyType: Types.any(),
+  })
+  async assertReturn (ctx) {
+    try {
+      const assert = await postAssertSSO(this.body(ctx))
+      if (assert) {
+        ctx.session.sso = assert
+      }
+
+      ctx.redirect(config.frontUrl)
+    } catch (err) {
+      ctx.throw(401, err)
+    }
+  }
+
+  /**
+   * Get SSO Url
+   */
+  @Route.Get()
+  async getTestUrl (ctx) {
+    this.sendOk(ctx, config.sso.testUrl)
+  }
+
+  /**
+   * Get SSO Url
+   */
+  @Route.Get()
+  async status (ctx) {
+    const email = (ctx.session.sso?.email || '').toLowerCase()
+    const firstName = ctx.session.sso?.firstName || ''
+    const lastName = ctx.session.sso?.lastName || ''
+
+    const userInDb = await this.model.userPreviewWithEmail(email)
+    if (userInDb) {
+      await ctx.loginUser(userInDb, 7)
+      await this.models.Logs.addLog(USER_USER_LOGIN, userInDb.id, {
+        userId: userInDb.id,
+      })
+      await super.addUserInfoInBody(ctx, userInDb.id)
+      ctx.session.sso = null
+      this.sendCreated(ctx)
+    } else if (email) {
+      this.sendOk(ctx, {
+        status: SAML_STATUS_PENDING,
+        datas: {
+          email,
+          firstName,
+          lastName,
+        },
+      })
+    } else {
+      this.sendOk(ctx, {
+        status: SAML_STATUS_EMPTY,
+      })
+    }
+  }
+
+  /**
+   * Vide la session en cour
+   */
+  @Route.Get()
+  async cleanSession (ctx) {
+    ctx.session.sso = null
+    this.sendOk(ctx, 'OK')
+  }
+}
