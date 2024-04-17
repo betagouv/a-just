a0c3d41c6d3ac029263c4ca6b28e35d78c6263fb
Redwane <56769614+rzafari42@users.noreply.github.com>
2024-04-17 18:16:07 +0200

    Added Console log for completion calculation

--- front/src/app/routes/activities/activities.page.ts
+++ front/src/app/routes/activities/activities.page.ts
@@ -512,8 +512,8 @@ export class ActivitiesPage extends MainClass implements OnDestroy {
                   nbToComplete += 1
                 if (child.valueQualityStock === "to_complete" && child.originalStock === null && child.stock === null)
                   nbToComplete += 1
-                if (child.label === 'Intérêts civils')
-                  console.log('Calc:', (((childNotEmpty + nbToComplete) * 100) / childToCount) - elem.completion)
+                if (child.label === 'Intérêts civils' || child.label === 'Référés civils')
+                  console.log('Calc + ' , child.label , ': ', (((childNotEmpty + nbToComplete) * 100) / childToCount) - elem.completion)
                 child.possibleGainCompletion = (Math.round(((childNotEmpty + nbToComplete) * 100) / childToCount) - elem.completion) || 0;
               }
             }
