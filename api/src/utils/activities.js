6d4cb1a7e874ca786e509a4bb10f73111cd71c4c
Redwane <56769614+rzafari42@users.noreply.github.com>
2024-04-26 12:20:39 +0200

    Fixed an issue about 'to_verify' data which were considered as updates while not

--- api/src/utils/activities.js
+++ api/src/utils/activities.js
@@ -1,11 +1,45 @@
 import { VALUE_QUALITY_TO_VERIFY } from "../constants/activities"
 
 /**
+ * Permet de vérifier parmis les enfants d'un contentieux, 
+ * si il existe des donées modifiées et si parmis ces données 
+ * il y a uniquement des données de qualité 'A verifier' 
+ * qui sont bien vérifier (cad -> nouvelle valeur = original)
+ * @param {*} childs 
+ * @param {*} referentiels 
+ * @param {*} type 
+ * @param {*} originalType 
+ * @param {*} qualityType 
+ * @returns 
+ */
+export const checkTotalUpdates = (childs, referentiels, type, originalType, qualityType) => {
+
+  let setTotal = false
+  for (let child of childs) {
+    if (child[type] !== null) {
+      if (child[type] === child[originalType]) {
+        const ref = referentiels.find(elem => elem.id === child.contentieux_id)
+        if (ref[qualityType] === VALUE_QUALITY_TO_VERIFY) {
+          setTotal = false
+        } else {
+          setTotal = true
+          break
+        }
+      } else {
+        setTotal = true
+        break
+      }
+    }
+  }
+  return setTotal
+}
+
+/**
  * Formalise les activités d'un contentieux en fonction de ses sous contentieux
  * @param {*} childs
  * @returns
  */
-export const calculMainValuesFromChilds = (childs) => {
+export const calculMainValuesFromChilds = (childs, referentiels) => {
   const returnObject = {
     entrees: preformatActivitiesArray(childs, ['entrees', 'original_entrees']),
     original_entrees: preformatActivitiesArray(childs, ['original_entrees']),
@@ -15,29 +49,24 @@ export const calculMainValuesFromChilds = (childs) => {
     original_stock: preformatActivitiesArray(childs, ['original_stock']),
   }
 
-  if (childs.every((c) => c.original_entrees === null)) {
+  if (!checkTotalUpdates(childs, referentiels, 'original_entrees', 'original_entrees', 'value_quality_in')) {
     returnObject.original_entrees = null
   }
 
-  if (childs.every((c) => c.entrees === null)) {
+  if (!checkTotalUpdates(childs, referentiels, 'entrees', 'original_entrees', 'value_quality_in'))
     returnObject.entrees = null
-  }
 
-  if (childs.every((c) => c.original_sorties === null)) {
+  if (!checkTotalUpdates(childs, referentiels, 'original_sorties', 'original_sorties', 'value_quality_out'))
     returnObject.original_sorties = null
-  }
 
-  if (childs.every((c) => c.sorties === null)) {
+  if (!checkTotalUpdates(childs, referentiels, 'sorties', 'original_sorties', 'value_quality_out'))
     returnObject.sorties = null
-  }
 
-  if (childs.every((c) => c.original_stock === null)) {
+  if (!checkTotalUpdates(childs, referentiels, 'original_stock', 'original_stock', 'value_quality_stock'))
     returnObject.original_stock = null
-  }
 
-  if (childs.every((c) => c.stock === null)) {
+  if (!checkTotalUpdates(childs, referentiels, 'stock', 'stock', 'value_quality_stock'))
     returnObject.stock = null
-  }
 
   return returnObject
 }
