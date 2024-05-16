7ca8d6699dac81f068bd9c84b110b709d772dc8f
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2024-05-16 12:34:38 +0200

    fix help tour

--- front/src/app/routes/workforce/workforce.page.ts
+++ front/src/app/routes/workforce/workforce.page.ts
@@ -295,7 +295,7 @@ export class WorkforcePage extends MainClass implements OnInit, OnDestroy {
       target: '.menu-item.tools',
       //target: '.menu .sub-tools > p:nth-child(2n)',
       title: 'Découvrez nos outils :',
-      intro: 'La <b>calculatrice</b> vous permet d\'obtenir rapidement le pourcentage de temps de travail qu’un agent (magistrat, ou fonctionnaire) consacre à une activité.<br/><br/>La <b>nomenclature</b> A-JUST vous permet de visualiser à quel contentieux sont rattachés, à titre principal, les différents sous-contentieux en fonction des codes NAC.'
+      intro: 'La <b>calculatrice</b> vous permet d\'obtenir rapidement le pourcentage de temps de travail qu’un agent (magistrat, ou fonctionnaire) consacre à une activité.<br/><br/>La <b>nomenclature</b> permet par les codes NAC de connaître la nature des affaires prises en compte dans un sous-contentieux : c\'est la colonne vertébrale d\'A-JUST !'
       /*intro:
         "vous permet de convertir en <b>pourcentage d'ETPT la part de temps de travail</b> qu'un agent consacre à une activité.",
         beforeLoad: async (intro: any) => {
