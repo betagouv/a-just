acb6afb9b03f8f213e6026167ded7f5565ce8e8f
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2024-05-29 11:15:50 +0200

    add intro js videos

--- front/src/app/routes/activities/activities.page.ts
+++ front/src/app/routes/activities/activities.page.ts
@@ -205,6 +205,12 @@ export class ActivitiesPage extends MainClass implements OnDestroy {
           }
         }*/
     },
+    {
+      target: '#wrapper-contener',
+      title: "Découvrir la fonctionnalité",
+      intro:
+        "<p>Consultez la vidéo ci-dessous pour plus de détails sur le fonctionnement de l'écran des données d’activité.</p><iframe src='https://app.videas.fr/57d76050-fb40-4cf4-bb75-5bb5ec636019/' style=\"width:100%;margin-top:16px;height:370px;\" frameborder=\"0\" allow=\"autoplay; fullscreen; picture-in-picture\" allowfullscreen></iframe>",
+    },
   ]
 
   /**
