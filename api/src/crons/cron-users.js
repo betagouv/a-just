5ffc65fdbee5cd73e1b5914696adae083b723360
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2024-06-05 17:09:57 +0200

    remove old agents and users

--- api/src/crons/cron-users.js
+++ api/src/crons/cron-users.js
@@ -4,13 +4,11 @@ const userCron = async (env) => {
   // check monday at 4 pm
   const syncAllDaysAt6 = new CronJob('0 6 * * *', async function () {
     console.log('START CRONS : CHECK REMOVED ACCOUNT')
-    //await env.models.Users.checkAccountToRemove()
+    await env.models.Users.checkAccountToAnonymise()
     console.log('END CRONS : CHECK REMOVED ACCOUNT')
   })
 
   syncAllDaysAt6.start()
-
-  //await env.models.Users.checkAccountToRemove()
 }
 
 export default userCron
