8e20b19de485a8b5e08e8aef0cba837ec22ca656
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2024-06-06 09:08:42 +0200

    enable 2 auth to admin

--- front-admin/src/app/routes/login/login.module.ts
+++ front-admin/src/app/routes/login/login.module.ts
@@ -3,9 +3,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { LoginPage } from './login.page';
 import { LoginPageModule } from './login.routing';
+import { ComponentsModule } from 'src/app/components/components.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-	declarations: [ LoginPage ],
-	imports: [ LoginPageModule, FormsModule, ReactiveFormsModule, RouterModule ]
+	declarations: [LoginPage],
+	imports: [LoginPageModule, CommonModule, FormsModule, ReactiveFormsModule, RouterModule, ComponentsModule]
 })
-export class LoginModule {}
+export class LoginModule { }
--- front-admin/src/app/routes/login/login.page.ts
+++ front-admin/src/app/routes/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { FormControl, FormGroup } from "@angular/forms";
 import { Router } from "@angular/router";
+import { LOGIN_STATUS_GET_CODE } from "src/app/constants/login";
 import { AuthService } from "src/app/services/auth/auth.service";
 import { UserService } from "src/app/services/user/user.service";
 
@@ -13,8 +14,12 @@ export class LoginPage {
     email: new FormControl(),
     password: new FormControl(),
   });
+  /**
+   * Popin to required code
+   */
+  needToGetCode: string | null = null
 
-  constructor(private authService: AuthService, private userService: UserService, private router: Router) {}
+  constructor(private authService: AuthService, private userService: UserService, private router: Router) { }
 
   ngOnInit() {
     this.userService.me().then((data) => {
@@ -26,8 +31,32 @@ export class LoginPage {
 
   onSubmit() {
     let { email, password } = this.form.value;
-    this.authService.loginAdmin({ email, password }).then(() => {
-      this.router.navigate(["/users"]);
+    this.authService.loginAdmin({ email, password }).then((returnLogin) => {
+      if (
+        returnLogin &&
+        returnLogin.data &&
+        returnLogin.data.status === LOGIN_STATUS_GET_CODE
+      ) {
+        this.needToGetCode = returnLogin.data.datas.code || ''
+      } else {
+        this.router.navigate(["/users"]);
+      }
     });
+  }
+
+  onContinuToLogin(action: any, input: any) {
+    console.log(action, input.value)
+    switch (action.id) {
+      case 'connect':
+        this.authService
+          .completeLogin({ code: input.value })
+          .then((returnLogin) => {
+            this.router.navigate(["/users"]);
+          })
+        break
+      default:
+        this.needToGetCode = null
+        break
+    }
   }
 }
