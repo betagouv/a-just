d583bf189432dec08ebdb0edb0eb9815fc8a5632
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2024-06-19 10:26:00 +0200

    add self video to server

--- api/src/crons/cron-agents.js
+++ api/src/crons/cron-agents.js
@@ -4,12 +4,12 @@ const agentCron = async (env) => {
   // check monday at 4 pm
   const syncAllDaysAt6 = new CronJob('0 6 * * *', async function () {
     console.log('START CRONS : CHECK AGENT TO REMOVE')
-    await env.models.HumanResources.checkAgentToAnonymise()
+    //await env.models.HumanResources.checkAgentToAnonymise()
     console.log('END CRONS : CHECK AGENT TO REMOVE')
   })
 
   syncAllDaysAt6.start()
-  await env.models.HumanResources.checkAgentToAnonymise()
+  //await env.models.HumanResources.checkAgentToAnonymise()
 }
 
 export default agentCron
