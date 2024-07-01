56d80f217f3c0d1e77eba9db5b337b17efa6e508
MrJimmyChevallier <103670713+MrJimmyChevallier@users.noreply.github.com>
2024-07-01 14:57:48 +0200

    wip

---
+++ api/src/db/seeders/development/202407011430-add-ca-jirs.js
@@ -0,0 +1,22 @@
+import { juridictionJIRS } from "../../../constants/juridiction-jirs"
+import { isCa } from "../../../utils/ca"
+
+module.exports = {
+  up: async (queryInterface, Sequelize, models) => {
+    if (isCa()) {
+      const tj = await models.HRBackups.findAll()
+      for (let i = 0; i < tj.length; i++) {
+        if (juridictionJIRS.includes(tj[i].dataValues.label)) {
+          await tj[i].update({
+            jirs: true,
+          })
+        } else {
+          await tj[i].update({
+            jirs: false,
+          })
+        }
+      }
+    }
+  },
+  down: (/*queryInterface , Sequelize*/) => { },
+}
