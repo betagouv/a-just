6512d67e5779ec05548f2e47eae2e5da5c907177
MrJimmyChevallier <103670713+MrJimmyChevallier@users.noreply.github.com>
2024-07-02 09:11:07 +0200

    update jirs or jirs ca

--- api/src/db/seeders/development/202407011430-add-ca-jirs-hors-jirs.js
+++ api/src/db/seeders/development/202407011430-add-ca-jirs-hors-jirs.js
@@ -4,6 +4,11 @@ import { isCa } from "../../../utils/ca"
 module.exports = {
   up: async (queryInterface, Sequelize, models) => {
     if (isCa()) {
+      const tableCA = await models.TJ.findAll()
+      for (let i = 0; i < tableCA.length; i++) {
+        await models.HRBackups.findOrCreateLabel(tableCA[i].label)
+      }
+
       const tj = await models.HRBackups.findAll()
       for (let i = 0; i < tj.length; i++) {
         if (juridictionJIRS.includes(tj[i].dataValues.label)) {
