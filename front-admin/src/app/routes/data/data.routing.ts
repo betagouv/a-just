7bd3b7b1d9498e80fab33c9241ade2811942f955
Redwane <56769614+rzafari42@users.noreply.github.com>
2024-07-23 17:06:29 +0200

    Added functionality to compare data etween month before import

--- front-admin/src/app/routes/data/data.routing.ts
+++ front-admin/src/app/routes/data/data.routing.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DataAnalysePage } from './data-analyse.page';
+import { DataPage } from './data.page';
 
 const routes: Routes = [
 	{
 		path: '',
-		component: DataAnalysePage
+		component: DataPage
 	}
 ];
 
@@ -13,4 +13,4 @@ const routes: Routes = [
 	imports: [ RouterModule.forChild(routes) ],
 	exports: [ RouterModule ]
 })
-export class DataAnalysePageModule {}
+export class DataPageModule {}
