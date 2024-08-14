ab378dca07f1738a5cf4b48216f3882bdab172a5
MrJimmyChevallier <103670713+MrJimmyChevallier@users.noreply.github.com>
2024-08-14 18:00:16 +0200

    change column flex value for cockpit

--- front/src/app/services/calculator/calculator.service.ts
+++ front/src/app/services/calculator/calculator.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core'
 import { BehaviorSubject } from 'rxjs'
 import { CalculatorInterface } from 'src/app/interfaces/calculator'
 import { MainClass } from 'src/app/libs/main-class'
-import { setTimeToMidDay } from 'src/app/utils/dates'
+import { month, setTimeToMidDay } from 'src/app/utils/dates'
 import { ContentieuxOptionsService } from '../contentieux-options/contentieux-options.service'
 import { ServerService } from '../http-server/server.service'
 import { HumanResourceService } from '../human-resource/human-resource.service'
@@ -36,16 +36,16 @@ export class CalculatorService extends MainClass {
    * Liste des réferentiels sélectionnées
    */
   referentielIds: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([])
- /**
-   * Liste des fonctions selectionnées
-   */
- selectedFonctionsIds: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([])
- /**
-   * Liste des fonctions selectionnées
-   */
- categorySelected: BehaviorSubject<string | null> = new BehaviorSubject<string | null>(null)
+  /**
+    * Liste des fonctions selectionnées
+    */
+  selectedFonctionsIds: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([])
+  /**
+    * Liste des fonctions selectionnées
+    */
+  categorySelected: BehaviorSubject<string | null> = new BehaviorSubject<string | null>(null)
 
- 
+
   /**
    * Constructeur
    * @param serverService 
@@ -72,16 +72,26 @@ export class CalculatorService extends MainClass {
    * @returns 
    */
   filterList(categorySelected: string, selectedFonctionsIds: number[] | null) {
+    console.log('FILTER LIST')
+    console.log('BACK Start', this.dateStart.getValue())
+    console.log('BACK Stop', this.dateStop.getValue())
     return this.serverService
       .post(`calculator/filter-list`, {
         backupId: this.humanResourceService.backupId.getValue(),
-        dateStart:  setTimeToMidDay(this.dateStart.getValue()),
-        dateStop:  setTimeToMidDay(this.dateStop.getValue()),
+        dateStart: setTimeToMidDay(this.dateStart.getValue()),
+        dateStop: setTimeToMidDay(this.dateStop.getValue()),
         contentieuxIds: this.referentielIds.getValue(),
         optionBackupId: this.contentieuxOptionsService.backupId.getValue(),
         categorySelected,
         selectedFonctionsIds,
       })
       .then((data) => data.data || [])
+  }
+
+  changePeriodSelected(monthToAdd: number) {
+    this.dateStart.next(month(this.dateStart.getValue(), monthToAdd))
+    console.log('step1', this.dateStop.getValue())
+    this.dateStop.next(month(this.dateStop.getValue(), monthToAdd, 'lastDay'))
+    console.log('step2', this.dateStop.getValue())
   }
 }
