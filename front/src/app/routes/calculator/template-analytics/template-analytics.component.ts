c7f5aff1c0b9e563976528a5cf613ecee8a28209
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2024-08-16 11:39:26 +0200

    WIP compare calculator with referentiel

---
+++ front/src/app/routes/calculator/template-analytics/template-analytics.component.ts
@@ -0,0 +1,84 @@
+import { Component, Input, OnDestroy, OnInit } from '@angular/core'
+import { ContentieuReferentielInterface } from 'src/app/interfaces/contentieu-referentiel'
+import { MainClass } from 'src/app/libs/main-class'
+import { HumanResourceService } from 'src/app/services/human-resource/human-resource.service'
+import { ReferentielService } from 'src/app/services/referentiel/referentiel.service'
+
+export interface AnalyticsLine {
+  title: string;
+  description?: string;
+  type: string;
+  values: number[][];
+  lineMax: number;
+  variations: {
+    label: string;
+    isOption?: boolean;
+    values: (number | string | null)[];
+    subTitle?: string;
+    showArrow?: boolean;
+  }[];
+  optionsVisibles?: boolean;
+}
+
+
+/**
+ * Composant de la page en vue analytique
+ */
+
+@Component({
+  selector: 'aj-template-analytics',
+  templateUrl: './template-analytics.component.html',
+  styleUrls: ['./template-analytics.component.scss'],
+})
+export class TemplateAnalyticsComponent extends MainClass implements OnInit, OnDestroy {
+  /**
+   * Référentiel
+   */
+  referentiel: ContentieuReferentielInterface[] = []
+  /**
+   * Templates
+   */
+  @Input() lines: AnalyticsLine[] = []
+
+  /**
+   * Constructor
+   */
+  constructor(
+    private humanResourceService: HumanResourceService,
+    private referentielService: ReferentielService
+  ) {
+    super()
+  }
+
+  ngOnInit() {
+    this.watch(
+      this.humanResourceService.contentieuxReferentiel.subscribe((c) => {
+        this.referentiel = c.filter(
+          (r) =>
+            this.referentielService.idsIndispo.indexOf(r.id) === -1 &&
+            this.referentielService.idsSoutien.indexOf(r.id) === -1
+        )
+      })
+    )
+  }
+
+  /**
+   * Suppresion des observables lors de la suppression du composant
+   */
+  ngOnDestroy() {
+    this.watcherDestroy()
+  }
+
+  ngOnChanges() {
+  }
+
+  getOptionsToShow(line: AnalyticsLine) {
+    const options = line.variations || []
+    return line.optionsVisibles ? options.filter(o => o.isOption === true || o.isOption === undefined) : options.filter(o => o.isOption === false || o.isOption === undefined)
+  }
+
+  hasOption(line: AnalyticsLine) {
+    const options = line.variations || []
+    return options.filter(o => o.isOption === true).length
+  }
+}
