b6feec72fbb684827c885a18f3c9a9e277021db9
Redwane <56769614+rzafari42@users.noreply.github.com>
2024-10-03 12:59:35 +0200

    Removed Hubspot live chat for staging

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -19,12 +19,12 @@ export const environment = {
   matomoTM: null,
   forceSSL: false,
   supportEmail: 'support-utilisateurs@a-just.fr',
-  mapboxToken: 'pk.eyJ1IjoiZnhiZXRhIiwiYSI6ImNsZzZldHhvMTAzanEzc3BrYXJ5dXA1czYifQ.FlyDfsXjidTxi7XE50a07w',
+  mapboxToken:
+    'pk.eyJ1IjoiZnhiZXRhIiwiYSI6ImNsZzZldHhvMTAzanEzc3BrYXJ5dXA1czYifQ.FlyDfsXjidTxi7XE50a07w',
   mapboxStyle: 'mapbox://styles/fxbeta/clm7xuvg4012d01pb1jc6887h',
   gitbookToken: 'gb_api_rHt5wBv5WBzk2mjUuO2QymdKNpNujdBD8TLfYpsA',
   gitbookId: 'S99g6aJCtkSrC9hKXFqV',
   enableSSO: true,
-  hubspotAccoundId: '26493393',
   //typeId: process.env['TYPE_ID'] || 0,
 }
 
