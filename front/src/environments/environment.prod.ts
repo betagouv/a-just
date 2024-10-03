3874a7a6f3c3a8b2d95401b0703fd8a2a398d0fc
Redwane <56769614+rzafari42@users.noreply.github.com>
2024-10-03 11:50:41 +0200

    removed hubspot live chat

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import packageJSON from '../../../package.json';
+import packageJSON from '../../../package.json'
 
 /**
  * Variables global avec url serveur différent ou matomo qui change
@@ -16,11 +16,11 @@ export const environment = {
   matomoTM: 'a9rOumyV',
   forceSSL: true,
   supportEmail: 'support-utilisateurs@a-just.fr',
-  mapboxToken: 'pk.eyJ1IjoiZnhiZXRhIiwiYSI6ImNsZzZldWR4dDA0aDEzZHBjazUxeWR5ZXYifQ.EHCQXWZ-t7RHL8EpCRXIng',
+  mapboxToken:
+    'pk.eyJ1IjoiZnhiZXRhIiwiYSI6ImNsZzZldWR4dDA0aDEzZHBjazUxeWR5ZXYifQ.EHCQXWZ-t7RHL8EpCRXIng',
   mapboxStyle: 'mapbox://styles/fxbeta/clg6ed492002m01lafhmrc98n',
   gitbookToken: 'gb_api_rHt5wBv5WBzk2mjUuO2QymdKNpNujdBD8TLfYpsA',
   gitbookId: 'S99g6aJCtkSrC9hKXFqV',
   enableSSO: true,
-  hubspotAccoundId: '26493393',
   //typeId: process.env['TYPE_ID'] || 0,
-};
+}
