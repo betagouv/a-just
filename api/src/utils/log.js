c32bff8664762043edfab5ed1fe7932a83a572cc
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2024-10-10 12:30:44 +0200

    add add logs

--- api/src/utils/log.js
+++ api/src/utils/log.js
@@ -12,7 +12,7 @@ logger.add(
   new winston.transports.Console({
     format: winston.format.combine(
       winston.format.colorize(),
-      winston.format.printf(({ level, message }) => `${level}: ${message}`)
+      winston.format.printf(({ level, message }) => `${level}: ${JSON.stringify(message)}`)
     ),
   })
 )
