2844d6f5668b1d12f32883e61234ab421b3dfd6e
MrJimmyChevallier <103670713+MrJimmyChevallier@users.noreply.github.com>
2024-12-17 12:30:00 +0100

    fill script ca update code import

--- api/src/db/seeders/development/202412091720-migration-bdd-code-import-and-jirs.js
+++ api/src/db/seeders/development/202412091720-migration-bdd-code-import-and-jirs.js
@@ -2,8 +2,57 @@ import { isCa } from "../../../utils/ca";
 
 module.exports = {
   up: async (queryInterface, Sequelize, models) => {
-    if (isCa()) {}
+    if (isCa()) {
     
+      const findSixUn = await models.ContentieuxReferentiels.findOne({
+        where: {
+          code_import: "6.1.",
+        },
+      });
+
+      if (findSixUn) {
+        await findSixUn.update({
+          code_import: "12.7.",
+        });
+      }
+
+      const findSixDeux = await models.ContentieuxReferentiels.findOne({
+        where: {
+          code_import: "6.2.",
+        },
+      });
+
+      if (findSixDeux) {
+        await findSixUn.update({
+          code_import: "12.8.",
+        });
+      }
+
+      const findSixQuatre = await models.ContentieuxReferentiels.findOne({
+        where: {
+          code_import: "6.4.",
+        },
+      });
+
+      if (findSixQuatre) {
+        await findSixUn.update({
+          code_import: "12.9.",
+        });
+      }
+
+      const findTroisQuatre = await models.ContentieuxReferentiels.findOne({
+        where: {
+          code_import: "3.4.",
+        },
+      });
+
+      if (findTroisQuatre) {
+        await findSixUn.update({
+          code_import: "4.7.",
+        });
+      }
+    }
+
   },
   down: (/*queryInterface , Sequelize*/) => {},
 };
