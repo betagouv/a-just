9721b7f02f977a754c0e03bcf2195e03a814ac89
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2024-12-20 13:26:55 +0100

    update greffier function rank

---
+++ api/src/db/seeders/development/202412201323-update-fonctions-a-greffier-rank.js
@@ -0,0 +1,50 @@
+module.exports = {
+  up: async (queryInterface, Sequelize, models) => {
+    await models.HRFonctions.update(
+      {
+        code: "A GREFFIER",
+        label: "A GREFFIER",
+      },
+      {
+        where: {
+          category_detail: "A GREFFIER",
+        },
+      }
+    );
+
+    const findFunction = await models.HRFonctions.findOne({
+      where: {
+        label: "A GREFFIER",
+      },
+    });
+    const findFunctionChef = await models.HRFonctions.findOne({
+      where: {
+        label: "CHEF DE CABINET",
+      },
+      raw: true,
+    });
+    if (findFunction && findFunctionChef) {
+      await findFunction.update({
+        rank: findFunctionChef.rank,
+      });
+    }
+
+    const findFunction2 = await models.HRFonctions.findOne({
+      where: {
+        label: "ATTACHÉ DE JUSTICE",
+      },
+    });
+    const findFunctionChef2 = await models.HRFonctions.findOne({
+      where: {
+        label: "ASSISTANT DE JUSTICE",
+      },
+      raw: true,
+    });
+    if (findFunction2 && findFunctionChef2) {
+      await findFunction2.update({
+        rank: findFunctionChef2.rank,
+      });
+    }
+  },
+  down: (/*queryInterface , Sequelize*/) => {},
+};
