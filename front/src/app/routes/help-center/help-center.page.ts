5b756b7e7352858aa89a903f32f9506f55d68175
MrJimmyChevallier <103670713+MrJimmyChevallier@users.noreply.github.com>
2025-01-07 17:33:25 +0100

    temporally add back token

--- front/src/app/routes/help-center/help-center.page.ts
+++ front/src/app/routes/help-center/help-center.page.ts
@@ -197,7 +197,7 @@ export class HelpCenterPage implements OnInit, AfterViewInit {
    * Constructeur
    */
   constructor() {
-    this.gitToken = import.meta.env.NG_APP_GITBOOK_TOKEN;
+    this.gitToken = environment.gitbookToken;
     this.gitbook = new GitBookAPI({
       authToken: this.gitToken,
     });
