f66806199cc6fa26b19460002dc843af2c8e3980
MrJimmyChevallier <103670713+MrJimmyChevallier@users.noreply.github.com>
2025-03-24 20:08:43 +0100

    add logs extractor

--- api/src/utils/date.js
+++ api/src/utils/date.js
@@ -370,7 +370,7 @@ export function setTimeToMidDay (date) {
  * @param {*} date2 
  * @returns 
  */
-export function isDateGreaterOrEqual(date1, date2) {
+export function isDateGreaterOrEqual (date1, date2) {
   const d1 = new Date(date1.getFullYear(), date1.getMonth(), date1.getDate());
   const d2 = new Date(date2.getFullYear(), date2.getMonth(), date2.getDate());
 
