6614c7f26b2782822ac7b21dea48d9be44e98927
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2025-04-01 12:50:51 +0200

    change etp to null or 0 on edit agent

--- front/src/app/components/speedometer/speedometer.component.ts
+++ front/src/app/components/speedometer/speedometer.component.ts
@@ -127,7 +127,6 @@ export class SpeedometerComponent extends MainClass implements OnInit {
       this.percent === null || isNaN(this.percent)
         ? 'N/R'
         : Math.floor(this.percent) + '%';
-    console.log(this.percent, typeof this.percent, this.percentString);
     this.onDraw();
   }
 
