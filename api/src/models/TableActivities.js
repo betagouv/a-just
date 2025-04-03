2ebeb24eb41e4f10ee1709e615c5adab03286484
Redwane <56769614+rzafari42@users.noreply.github.com>
2025-04-03 10:55:18 +0200

    Update the way we calculate stock on activuty data

--- api/src/models/TableActivities.js
+++ api/src/models/TableActivities.js
@@ -536,26 +536,24 @@ export default (sequelizeInstance, Model) => {
               date,
               hrBackupId
             );
+
             if (previousStockValue !== null) {
               if (
                 findAllChild[i].entrees !== null ||
                 findAllChild[i].sorties !== null ||
                 previousStockValue.type === "calculate"
               ) {
-                const entrees = preformatActivitiesArray(
-                  [findAllChild[i]],
-                  ["entrees", "original_entrees"]
-                );
-                const sorties = preformatActivitiesArray(
-                  [findAllChild[i]],
-                  ["sorties", "original_sorties"]
-                );
+                const entrees = preformatActivitiesArray([findAllChild[i]],["entrees", "original_entrees"]);
+                const sorties = preformatActivitiesArray([findAllChild[i]],["sorties", "original_sorties"]);
 
-                const stock = previousStockValue.type === "calculate" ? previousStockValue.stock : preformatActivitiesArray(
-                  [findAllChild[i]],
-                  ["original_stock"]
-                );
-                currentStock = stock + (entrees || 0) - (sorties || 0);
+                if (previousStockValue.type === "calculate") {
+                  const stock = previousStockValue.stock ?? 0
+                  currentStock = (stock ?? 0) + (entrees ?? 0) - (sorties ?? 0);
+                }
+                else {
+                  const stock = preformatActivitiesArray([findAllChild[i]],["original_stock"])
+                  currentStock = (stock ?? 0) + ( entrees - (findAllChild[i].original_entrees ?? 0)) - (sorties - (findAllChild[i].original_sorties ?? 0));
+                }
               } else {
                 currentStock = findAllChild[i].original_stock;
               }
