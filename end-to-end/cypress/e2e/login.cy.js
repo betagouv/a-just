660cb3df28d9e4d834cc28a187c3b0e77ef5f703
Redwane <56769614+rzafari42@users.noreply.github.com>
2025-04-07 18:23:57 +0200

    New e2e tests added on login and signup page

--- end-to-end/cypress/e2e/login.cy.js
+++ end-to-end/cypress/e2e/login.cy.js
@@ -14,6 +14,36 @@ describe('Login Page', () => {
         // .should('contain.text', 'Se connecter avec Pages Blanches')
     })
 
+    it('Try to connect with an invalid email', () => {
+      cy.get('form')
+        .should('contain.text', 'Vous avez déjà un compte')
+        .get('h3')
+        .should('contain.text', 'Se connecter avec son compte')
+        .get('input[type=email]').type('invalideemail@mail.com')
+        .get('input[type=password]').type(user.password)
+        .get('form').submit()
+        cy.on("window:alert", (alert) => {
+          expect(alert).to.equal("Email ou mot de passe incorrect")
+          }
+        )
+        cy.get('.error-message').should('contain.text', 'Email ou mot de passe incorrect')
+    })
+
+    it('Try to connect with an invalid password', () => {
+      cy.get('form')
+        .should('contain.text', 'Vous avez déjà un compte')
+        .get('h3')
+        .should('contain.text', 'Se connecter avec son compte')
+        .get('input[type=email]').type(user.email)
+        .get('input[type=password]').type('invalidpassword!8')
+        .get('form').submit()
+        cy.on("window:alert", (alert) => {
+          expect(alert).to.equal("Email ou mot de passe incorrect.")
+          }
+        )
+        cy.get('.error-message').should('contain.text', 'Email ou mot de passe incorrect.')
+    })
+
     it('should verify login functionality with username and password and button can be clicked', () => {
       cy.get('form')
         .should('contain.text', 'Vous avez déjà un compte')
--- end-to-end/cypress/e2e/signup.cy.js
+++ end-to-end/cypress/e2e/signup.cy.js
@@ -2,7 +2,7 @@ import user from '../../fixtures/user.json'
 
 describe('Signup Page', () => {
 
-  beforeEach(() => {
+  before(() => {
     cy.clearAllLocalStorage();
     cy.visit(`/connexion`)
     cy.get('.signup')
@@ -19,7 +19,39 @@ describe('Signup Page', () => {
   //     .should('contain.text', 'Embarquer avec Pages Blanches')
   // })
 
+  it('Check that we have an error if email is different from @justice.gouv.fr or @*.gouv.fr', () => {
+    cy.get('input[formControlName="firstName"]').type("UserTestFirstname")
+    cy.get('input[formControlName="lastName"]').type("UserTestLastname")
+    cy.get('input[formControlName="email"]').type("userTest@test.mail.fr")
+    cy.get('input[formControlName="password"]').type("1xDrv9&!")
+    cy.get('input[formControlName="passwordConf"]').type("1xDrv9&!")
+    cy.get('input[formControlName="checkbox"]').check();
+
+    cy.on("window:alert", (alert) => {
+      expect(alert).to.equal("Vous devez saisir une adresse e-mail professionnelle")
+      }
+    )
+    cy.visit(`/inscription`)
+  })
+
+  it('Check that we have an error if password is not strong enough', () => {
+    cy.get('input[formControlName="firstName"]').type("UserTestFirstname")
+    cy.get('input[formControlName="lastName"]').type("UserTestLastname")
+    cy.get('input[formControlName="email"]').type("userTest@justice.gouv.fr")
+    cy.get('input[formControlName="password"]').type("easyPassword!")
+    cy.get('input[formControlName="passwordConf"]').type("easyPassword")
+    cy.get('input[formControlName="checkbox"]').check();
+    cy.get('.next-step').click()
+
+    cy.on("window:alert", (alert) => {
+      expect(alert).to.equal("Vous devez saisir un mot de passe qui remplit les critères obligatoires")
+      }
+    )
+    cy.visit(`/inscription`)
+  })
+
   it('Check that we can fill the form to signup ', () => {
+
       cy.get('input[formControlName="firstName"]').type("UserTestFirstname")
       cy.get('input[formControlName="lastName"]').type("UserTestLastname")
       cy.get('input[formControlName="email"]').type("userTest@justice.gouv.fr")
@@ -34,5 +66,5 @@ describe('Signup Page', () => {
       cy.get('select').eq(1).select(1);
       cy.get('input[type=submit]').click()
       cy.location('pathname').should('eq', '/bienvenue')
-  })  
+  })
 })
