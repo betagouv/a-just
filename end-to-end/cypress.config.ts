097733b66cebfc663673c2895373f000dfc97397
Redwane <56769614+rzafari42@users.noreply.github.com>
2025-04-07 16:53:01 +0200

    Try to debug

--- end-to-end/cypress.config.ts
+++ end-to-end/cypress.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "cypress";
 import { verifyDownloadTasks } from "cy-verify-downloads";
+import path from "path";
+import fs from "fs";
 
 export default defineConfig({
   component: {
@@ -14,6 +16,29 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on("task", verifyDownloadTasks);
+      on("task", {
+        logDownloads() {
+          const downloadPath = path.resolve(
+            __dirname,
+            "..",
+            "e2e/cypress/downloads"
+          );
+          if (fs.existsSync(downloadPath)) {
+            const files = fs.readdirSync(downloadPath);
+            console.log(
+              "📥 Fichiers dans le dossier de téléchargement :",
+              files
+            );
+          } else {
+            console.log(
+              "❌ Dossier de téléchargement introuvable :",
+              downloadPath
+            );
+          }
+          return null;
+        },
+        ...verifyDownloadTasks,
+      });
     },
     baseUrl: process.env.CYPRESS_BASE_URL
       ? process.env.CYPRESS_BASE_URL
