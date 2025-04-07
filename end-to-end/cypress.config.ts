13bf3b7f88a11bde243f489fec54e1477b26d497
Redwane <56769614+rzafari42@users.noreply.github.com>
2025-04-07 17:04:26 +0200

    Commented test w/ download

--- end-to-end/cypress.config.ts
+++ end-to-end/cypress.config.ts
@@ -16,29 +16,6 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on("task", verifyDownloadTasks);
-      on("task", {
-        logDownloads() {
-          const downloadPath = path.resolve(
-            __dirname,
-            "..",
-            "e2e/cypress/downloads"
-          );
-          if (fs.existsSync(downloadPath)) {
-            const files = fs.readdirSync(downloadPath);
-            console.log(
-              "📥 Fichiers dans le dossier de téléchargement :",
-              files
-            );
-          } else {
-            console.log(
-              "❌ Dossier de téléchargement introuvable :",
-              downloadPath
-            );
-          }
-          return null;
-        },
-        ...verifyDownloadTasks,
-      });
     },
     baseUrl: process.env.CYPRESS_BASE_URL
       ? process.env.CYPRESS_BASE_URL
