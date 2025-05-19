0dd33f17c57482b49ccacf854104b5306f9ef618
Redwane <56769614+rzafari42@users.noreply.github.com>
2025-05-19 22:23:55 +0200

    E2E: improved panorama test

--- end-to-end/cypress/e2e/panorama.cy.js
+++ end-to-end/cypress/e2e/panorama.cy.js
@@ -1,171 +1,133 @@
-
 describe('Panorama page', () => {
   before(() => {
     cy.login()
-    // cy.visit('/panorama')
-  })
-    
-  it('Check that we have the menu', () => {
-    cy.get('.menu-scrollable')
-    cy.get('a')
-        .should('contain.text', 'Panorama')
-        .should('contain.text', 'Ventilateur')
-        .should('contain.text', "Données d\'activité")
-        .should('contain.text', 'Cockpit')
-        .should('contain.text', 'Simulateur')
-        .should('contain.text', 'Outils')
-        .should('contain.text', 'Aide')
-        .should('contain.text', 'Nous contacter')
-    // Submenu for tools
-    cy.get('.tools')
-        .click()
-        .get('.sub-tools')
-        .should('contain.text', 'La nomenclature')
-        .should('contain.text', 'La calculatrice')
-        .should('contain.text', "Les extracteurs")
-        .should('contain.text', 'Référentiels de temps moyens')
-
   })
 
-    it("Check that we have two tabs = 'Effectifs' and 'Données d'activité", () => {
-      cy.get('.panorama-container')
-        .get('.header-panorama')
-        .should('contain.text', 'Effectifs')
-        .should('contain.text', "Données d'activité")
+  it('Should display main menu and submenu items', () => {
+    cy.get('.menu-scrollable a').as('menuLinks')
+
+    cy.get('@menuLinks').should('contain.text', 'Panorama')
+    cy.get('@menuLinks').should('contain.text', 'Ventilateur')
+    cy.get('@menuLinks').should('contain.text', "Données d'activité")
+    cy.get('@menuLinks').should('contain.text', 'Cockpit')
+    cy.get('@menuLinks').should('contain.text', 'Simulateur')
+    cy.get('@menuLinks').should('contain.text', 'Outils')
+    cy.get('@menuLinks').should('contain.text', 'Aide')
+    cy.get('@menuLinks').should('contain.text', 'Nous contacter')
+
+    cy.get('.tools').click()
+    cy.get('.sub-tools').within(() => {
+      cy.contains('La nomenclature')
+      cy.contains('La calculatrice')
+      cy.contains('Les extracteurs')
+      cy.contains('Référentiels de temps moyens')
+    })
+  })
+
+  it("Should show 'Effectifs' and 'Données d'activité' tabs", () => {
+    cy.get('.panorama-container .header-panorama')
+      .should('contain.text', 'Effectifs')
+      .and('contain.text', "Données d'activité")
+  })
+
+  it("Should display the 3 'Effectifs' sections", () => {
+    cy.get('.workforce-panel').within(() => {
+      cy.get('h3').eq(0).should('contain.text', 'Composition des effectifs')
+      cy.get('h3').eq(1).should('contain.text', 'Actualisation des fiches')
     })
 
-  it("Check that we have three sections on 'Effectifs' = 'Composition des effectifs' + 'Actualisation des fiches' + 'changement dans les effectifs' ", () => {
-    cy.get('.container-panorama')
-      .get('.workforce-panel').within(() => {
-        cy.get('h3')
-          .first()
-          .contains('Composition des effectifs')
-        cy.get('h3')
-          .eq(1)
-          .contains('Actualisation des fiches')
-      })
-      .get('.workforce-change')
-      .contains(/changement(s)? dans les effectifs/)
+    cy.get('.workforce-change').should('contain.text', 'changement').and('contain.text', 'effectifs')
   })
 
-  it("Check that we have three sections inside 'Les données d’activité' = 'Dernières modifications' + 'Dernières données disponibles' + 'Les données d'activité à compléter' ", () => {
-    cy.get('.container-panorama')
-        .get('.activities-panel')
-        .find('activities-last-modifications')
-        .find('h3')
-        .should('contain.text', 'Dernières modifications')
-      
-    cy.get('.container-panorama')
-      .get('.activities-panel')
-      .get('.left-panel')
-      .find('activities-last-disponibilities')
-      .find('h3')
-      .should('contain.text', 'Dernières données disponibles')
-
-    cy.get('.container-panorama')
-      .get('.activities-panel')
-      .get('.left-panel')
-      .find('activities-to-complete')
-      .find('h3')
-      .should('contain.text', "Les données d'activité à compléter")
+  it("Should display the 3 'Données d’activité' sections", () => {
+    cy.get('activities-last-modifications h3').should('contain.text', 'Dernières modifications')
+    cy.get('activities-last-disponibilities h3').should('contain.text', 'Dernières données disponibles')
+    cy.get('activities-to-complete h3').should('contain.text', "Les données d'activité à compléter")
   })
 
-  it("Check that we have 3 cards inside 'Composition des effectifs' = 'Siege' + 'Greffe' + 'Autour dumagistrat' ", () => {
-    cy.get('.container-panorama')
-      .get('.workforce-panel')
-      .get('.cards')
-      .get('.category')
-      .get('.header')
-      .get('.header-wrapper')
-      .get('.categoryType')
+  it("Should display 3 cards in 'Composition des effectifs'", () => {
+    cy.get('.workforce-panel .cards .categoryType')
       .should('contain.text', 'Siège')
-      .should('contain.text', 'Greffe')
-      .should('contain.text', 'Autour du magistrat')
+      .and('contain.text', 'Greffe')
+      .and('contain.text', 'Autour du magistrat')
   })
 
-  it("Check that for the two first cards ('Siege' and 'Greffe') inside 'Composition des effectifs' we have three sub-categories (Titulaires + Placés + Contractuels) and 'Personnels CLE' ", () => {
-    cy.get('.container-panorama')
-      .get('.workforce-panel')
-      .get('.cards')
-      .get('.category')
-      .get('.middle')
-      .should('contain.text', 'Personnels CLE')
-      .get('.footer')
-      .get('.subCategory')
-      .get('.categoryType')
-      .should('contain.text', 'Titulaire')
-      .should('contain.text', 'Placé')
-      .should('contain.text', 'Contractuel')
+  it("Should show sub-categories and CLE for first 2 agent categories", () => {
+    cy.get('.workforce-panel .cards .category').each(($el, index) => {
+      if (index < 2) {
+        cy.wrap($el).within(() => {
+          cy.get('.middle').should('contain.text', 'Personnels CLE')
+          cy.get('.footer .subCategory .categoryType').as('subcats')
+          cy.get('@subcats').should('contain.text', 'Titulaire')
+          cy.get('@subcats').should('contain.text', 'Placé')
+          cy.get('@subcats').should('contain.text', 'Contractuel')
+        })
+      }
+    })
   })
 
-  it("Check that if we add a CLE value for each agent category, they are well saved", () => {
-    const cleValues = ['4', '32', '10'];
+  it("Should save CLE values correctly", () => {
+    const cleValues = ['4', '32', '10']
 
-    cy.get('.container-panorama')
-    .find('.workforce-panel workforce-composition')
-    .within(() => {
-      cleValues.forEach((value, index) => {
-        cy.get('.cards')
-          .find('.category')
-          .eq(index)
-          .within(() => {
-            cy.get('.middle input').clear().type(value);
-          });
-      });
-    });
+    cleValues.forEach((value, index) => {
+      cy.get('.workforce-panel workforce-composition .cards .category')
+        .eq(index)
+        .find('.middle input')
+        .clear()
+        .type(value)
+    })
 
-    cy.wait(1000)
+    cy.reload()
 
-    cy.reload().then(() => {
-      cy.get('.container-panorama')
-        .find('.workforce-panel workforce-composition')
-        .within(() => {
-          cleValues.forEach((expectedValue, index) => {
-            cy.get('.cards')
-              .find('.category')
-              .eq(index)
-              .within(() => {
-                cy.get('.middle input').should('have.value', expectedValue);
-              });
-          });
-        });
-    });
+    cleValues.forEach((expectedValue, index) => {
+      cy.get('.workforce-panel workforce-composition .cards .category')
+        .eq(index)
+        .find('.middle input')
+        .should('have.value', expectedValue)
+    })
   })
 
-  it("Check that we have 3 cards in 'Actualisation des fiches'", () => {
-    cy.get('.container-panorama')
-      .get('.workforce-panel')
-      .get('.category')
-      .get('.content-wrapper')
+  it("Should display 3 cards in 'Actualisation des fiches'", () => {
+    cy.get('.workforce-panel .category .content-wrapper')
       .should('contain.text', 'Siège')
-      .should('contain.text', 'Greffe')
-      .should('contain.text', 'Autour du magistrat')
+      .and('contain.text', 'Greffe')
+      .and('contain.text', 'Autour du magistrat')
   })
 
-  it("Check that we have 3 columns in 'Actualisation des fiches'", () => {
-    cy.get('.container-panorama')
-      .get('.workforce-panel')
-      .get('.header-titles')
-      .get('.label')
-      .should('contain.text', 'Effectifs')
-      .get('.percent')
-      .should('contain.text', '% ventilé')
-      .get('.update-title')
-      .should('contain.text', 'Dernière MAJ')
+  it("Should show 3 columns in 'Actualisation des fiches'", () => {
+    cy.get('.workforce-panel .header-titles').within(() => {
+      cy.get('.label').should('contain.text', 'Effectifs')
+      cy.get('.percent').should('contain.text', '% ventilé')
+      cy.get('.update-title').should('contain.text', 'Dernière MAJ')
+    })
   })
 
-  it("Check that we can click on a category inside 'Actualisation des fiches' and be redirected to 'Ventilateur' with only 'Siege' filter activated", () => {
-    cy.get('.container-panorama')
-      .get('.workforce-panel')
-      .get('.category')
-      .get('.content-wrapper')
-      .get('.arrow')
-      .get('.dark-arrow').first() // get "Siege" category
+  const verifyRedirectionWithFilter = (index, expectedFilterIndex) => {
+    cy.get('.workforce-panel .category .dark-arrow')
+      .eq(index)
       .click()
-    cy.location('pathname')
-      .should('eq', '/ventilations')
-    cy.get('.radio-border-left').first()
-      .find('aj-radio-button')
-      .should('have.class', 'selected')
+
+    cy.location('pathname').should('eq', '/ventilations')
+
+    cy.get('.radio-border-left').each(($el, idx) => {
+      const isSelected = idx === expectedFilterIndex
+      cy.wrap($el).find('aj-radio-button')
+        .should(isSelected ? 'have.class' : 'not.have.class', 'selected')
+    })
+
+    cy.visit('/panorama')
+    cy.location('pathname').should('eq', '/panorama')
+  }
+
+  it("Should redirect with only 'Siège' filter active", () => {
+    verifyRedirectionWithFilter(0, 0)
+  })
+
+  it("Should redirect with only 'Greffe' filter active", () => {
+    verifyRedirectionWithFilter(1, 1)
+  })
+
+  it("Should redirect with only 'EAM' filter active", () => {
+    verifyRedirectionWithFilter(2, 2)
   })
 })
