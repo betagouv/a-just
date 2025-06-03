493de0289f928139b722fe8240678d479ea6a18e
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2025-06-03 10:36:55 +0200

    build cluster

---
+++ api/src/cluster.js
@@ -0,0 +1,15 @@
+const cluster = require('cluster');
+const os = require('os');
+
+if (cluster.isMaster) {
+  const numCPUs = os.cpus().length;
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    cluster.fork(); // relance un worker si un meurt
+  });
+} else {
+  require('./index.js'); // ou ton point d'entrée
+}
