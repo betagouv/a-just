29a0a2ac8dffc5ec1efcaf224b68830b5738f321
Redwane <56769614+rzafari42@users.noreply.github.com>
2025-06-13 17:59:40 +0200

    Added nomenclature-tj-droit-local on CSP url to ignore

--- api/src/constants/csp.js
+++ api/src/constants/csp.js
@@ -1,4 +1,4 @@
 /**
  * Liste des url qui ignore les règles CSP
  */
-export const CSP_URL_IGNORE_RULES = ['/assets/nomenclature-A-Just.html', '/assets/nomenclature-A-Just-CA.html', '/assets/nomenclature-A-Just-droit-local.html', '/admin', '/ap-bo', '/contact', '/api/saml/metadata.xml']
+export const CSP_URL_IGNORE_RULES = ['/assets/nomenclature-A-Just.html', '/assets/nomenclature-A-Just-CA.html', '/assets/nomenclature-A-Just-droit-local.html', '/assets/nomenclature-A-Just-TJ-droit-local.html', '/admin', '/ap-bo', '/contact', '/api/saml/metadata.xml']
