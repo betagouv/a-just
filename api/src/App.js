76df6de8550ae133ff043bc83dbdf5741ab5a1b7
MrJimmyChevallier <103670713+MrJimmyChevallier@users.noreply.github.com>
2025-06-22 15:26:38 +0200

    secure redis

--- api/src/App.js
+++ api/src/App.js
@@ -20,6 +20,137 @@ import * as Sentry from '@sentry/node'
 import authBasic from 'http-auth'
 import { writeFileSync } from 'fs'
 import { getFullKey, getRedisClient, loadOrWarmHR, waitForRedis } from './utils/redis'
+import sequelize, { Sequelize } from 'sequelize'
+
+const cspConfig = {
+  // https://github.com/helmetjs/helmet
+  contentSecurityPolicy: {
+    directives: {
+      'media-src': ["'self'", 'https://client.crisp.chat'],
+      'connect-src': [
+        'https://api.gitbook.com',
+        'https://www.google-analytics.com/j/collect',
+        "'self'",
+        'https://api.mapbox.com',
+        'https://events.mapbox.com',
+        'https://stats.beta.gouv.fr',
+        'https://forms-eu1.hsforms.com',
+        'https://hubspot-forms-static-embed-eu1.s3.amazonaws.com',
+        'https://stats.beta-gouv.cloud-ed.fr',
+        'https://*.hotjar.com',
+        'https://*.hotjar.io',
+        'wss://*.hotjar.com',
+        '*.justice.gouv.fr',
+        'https://client.crisp.chat',
+        'https://storage.crisp.chat',
+        'wss://client.relay.crisp.chat',
+        'wss://stream.relay.crisp.chat',
+      ],
+      'font-src': ["'self'", 'https://fonts.gstatic.com', 'data:', 'https://*.hotjar.com', 'https://client.crisp.chat'],
+      'img-src': [
+        "'self'",
+        'data:',
+        'https://js-eu1.hsforms.net',
+        'https://api.hubspot.com',
+        'https://forms-eu1.hsforms.com',
+        'https://forms.hsforms.com',
+        'https://www.ionos.fr',
+        'https://img.freepik.com',
+        'https://image.noelshack.com',
+        'https://i.goopics.net/',
+        'https://client.crisp.chat',
+        'https://image.crisp.chat',
+        'https://storage.crisp.chat',
+      ],
+      // "img-src": [
+      //   "'self'",
+      //   "data:",
+      //   "https://js-eu1.hsforms.net",
+      //   "https://api.hubspot.com",
+      //   "https://forms-eu1.hsforms.com",
+      //   "https://forms.hsforms.com",
+      //   "https://www.ionos.fr",
+      //   "https://img.freepik.com",
+      //   "https://image.noelshack.com",
+      //   "https://i.goopics.net/",
+      // ],
+      'script-src': [
+        "'self'",
+        'https://*.hsforms.net',
+        '*.beta.gouv.fr',
+        '*.a-just.incubateur.net',
+        '*.calendly.com',
+        '*.google-analytics.com',
+        '*.hotjar.com',
+        "'sha256-jq7VWlK1R1baYNg3rH3wI3uXJc6evRSm19ho/ViohcE='",
+        "'sha256-92TNq2Axm9gJIJETcB7r4qpDc3JjxqUYF1fKonG4mvg='",
+        "'sha256-WXdHEUxHRTHqWKtUCBtUckcV5wN4y9jQwkZrGjfqr40='",
+        "'sha256-9jsqNCkYsDU3te2WUjv9qXV1DKXI1vT9hz3g7nNens8='",
+        "'sha256-Z/I+tLSqFCDH08E3fvI/F+QNinxE6TM+KmCxNmRcAAw='",
+        "'sha256-tBBLGYs6fvYemOy9hpbgu6tIIJNpdIZpuGpDXkhGTVw='",
+        "'sha256-HVge3cnZEH/UZtmZ65oo81F6FB06/nfTNYudQkA58AE='",
+        "'sha256-6x6g2SYysPsSMI15om2cLqbYnqaoyjXQD+Aivk9OP4U='",
+        "'sha256-A+0b+HOyTgrPPZgW1Tcb6UJIvj7fs09WPLWFtyqq1ks='",
+        //...scriptSha1Generate([`${__dirname}/front/index.html`]),
+        'https://client.crisp.chat',
+        'https://settings.crisp.chat',
+      ],
+      'default-src': ["'none'"],
+      'style-src': [
+        "'self'",
+        ...styleSha1Generate([`${__dirname}/front/index.html`]),
+        'cdnjs.cloudflare.com',
+        "'sha256-Ks+4bfA56EzWbsVt5/a+A7rCibdXWRQVb7y2dkDLIZM='",
+        "'sha256-MKASWYfd3dGFQes9nQT5XnslE3xYlnUb4cHpxhk4fag='",
+        "'sha256-eK2nDKvEyw7RbvnsAc4UTeSvLsouV8qnHxl0X48dCbs='",
+        'https://client.crisp.chat',
+        "'sha256-7Vo533bZB5hNdpZy9SiCUDc3JcgD9jqDXEc9aVAk5nY='",
+        "'sha256-UP0QZg7irvSMvOBz9mH2PIIE28+57UiavRfeVea0l3g='",
+        "'sha256-Mj/pDR/CuVebTo+8zwX6PU1+MXNnrzFL+dgRa0Q0JF0='",
+        "'sha256-BUZLvafdn4L6W6euGkBpnDrFVzIGLdSRjgp2e2gC+NE='",
+      ],
+      'worker-src': ['blob:'],
+      'frame-src': [
+        'https://app.videas.fr/',
+        'https://docs.a-just.beta.gouv.fr',
+        'https://meta.a-just.beta.gouv.fr',
+        'https://forms-eu1.hsforms.com/',
+        'https://calendly.com',
+        'https://game.crisp.chat',
+      ],
+      'worker-src': ['blob:'],
+      'frame-src': [
+        'https://app.videas.fr/',
+        'https://docs.a-just.beta.gouv.fr',
+        'https://meta.a-just.beta.gouv.fr',
+        'https://forms-eu1.hsforms.com/',
+        'https://calendly.com',
+      ],
+      'object-src': ["'self'"],
+      //'report-uri': ['/api/csp/report'],
+      'base-uri': ["'self'"],
+      'form-action': ["'self'", '*.hsforms.com'],
+      'upgrade-insecure-requests': [],
+    },
+    //reportOnly: true,
+  },
+  crossOriginEmbedderPolicy: false,
+  crossOriginOpenerPolicy: false,
+  crossOriginResourcePolicy: false,
+  originAgentCluster: false,
+  referrerPolicy: false,
+  strictTransportSecurity: {
+    maxAge: 31536000,
+    includeSubDomains: false,
+  },
+  xContentTypeOptions: 'nosniff',
+  xDnsPrefetchControl: false,
+  xDownloadOptions: false,
+  xFrameOptions: { action: 'deny' },
+  xPermittedCrossDomainPolicies: false,
+  xPoweredBy: false,
+  //xXssProtection: 1, don't work
+}
 
 const os = require('os')
 console.log('HOST NAME', os.hostname())
@@ -71,55 +202,60 @@ export default class App extends AppBase {
   async start() {
     const isPrimaryInstance = os.hostname().includes('web-1') || !os.hostname().includes('web')
 
+    // 🧱 Initialisation Sequelize + modèles
     this.models = db.initModels()
     this.routeParam.models = this.models
     this.routeParam.replicaModels = this.replicaModels
     this.koaApp.context.sequelize = db.instance
     this.koaApp.context.models = this.models
-    // (session) - required for cookie signature generation
+
+    // 🔄 Attendre que PostgreSQL soit prêt (évite crash au boot sur toutes les instances)
+    await this.waitForPostgres(db.instance)
+
+    // 🔑 Sessions
     this.koaApp.keys = ['oldsdfsdfsder secdsfsdfsdfret key']
     this.koaApp.proxy = true
 
+    // 🧠 Migration + seed uniquement sur l’instance principale
     if (isPrimaryInstance) {
-      db.migrations().then(() => {
-        db.seeders().then(async () => {
-          startCrons(this) // démarrage des crons
-          console.log('--- IS READY ---', config.port)
-          this.isReady()
-          // ❗ Warmup lancé en arrière-plan pour éviter timeout de Scalingo
-          setTimeout(() => {
-            this.warmupRedisCache().catch((err) => {
-              console.error('❌ Erreur warmup Redis (décalé) :', err)
-            })
-          }, 1000)
+      await db.migrations()
+      await db.seeders()
+
+      // ⏰ Démarrage des crons + warmup
+      startCrons(this)
+      console.log('--- IS READY ---', config.port)
+      this.isReady()
+
+      // ⏳ Warmup redis différé pour éviter timeout Scalingo
+      setTimeout(() => {
+        this.warmupRedisCache().catch((err) => {
+          console.error('❌ Erreur warmup Redis (décalé) :', err)
         })
-      })
+      }, 1000)
     } else {
       console.log('--- IS READY ---', config.port)
       this.isReady()
     }
 
+    // 🔒 Limiteur de requêtes
     const limiter = RateLimit.middleware({
-      interval: { min: 5 }, // 5 minutes = 5*60*1000
-      max: config.maxQueryLimit, // limit each IP to 100 requests per interval
+      interval: { min: 5 },
+      max: config.maxQueryLimit,
     })
 
-    this.koaApp.use(async (ctx, next) => {
-      return await honeyTrap(ctx, next, this.models)
-    })
-
+    // 🧊 Sessions Redis
     const sessionConfig = { ...config.session }
     if (config.redis) {
       const redisUrlSplited = config.redis.split('@')
       const redisConfig = {}
       if (redisUrlSplited.length > 1) {
-        const redisUrl = redisUrlSplited[redisUrlSplited.length - 1].split(':')
+        const redisUrl = redisUrlSplited[1].split(':')
         const redisAccount = redisUrlSplited[0].split(':')
         redisConfig.host = redisUrl[0]
         redisConfig.port = redisUrl[1]
         redisConfig.password = redisAccount[redisAccount.length - 1]
       } else {
-        const redisUrl = redisUrlSplited[redisUrlSplited.length - 1].split(':')
+        const redisUrl = redisUrlSplited[0].split(':')
         redisConfig.host = redisUrl[1].replace('//', '')
         redisConfig.port = redisUrl[2]
       }
@@ -129,175 +265,35 @@ export default class App extends AppBase {
     this.koaApp.use(session(sessionConfig, this.koaApp))
     Sentry.setupKoaErrorHandler(this.koaApp)
 
+    // 🧱 Middlewares principaux
     super.addMiddlewares([
-      //sslify(),
       limiter,
-      // we add the relevant middlewares to our API
-      koaBody({
-        multipart: true,
-        formLimit: '512mb',
-        textLimit: '512mb',
-        jsonLimit: '512mb',
-      }), // automatically parses the body of POST/PUT/PATCH requests, and adds it to the koa context
+      koaBody({ multipart: true, formLimit: '512mb', textLimit: '512mb', jsonLimit: '512mb' }),
       i18n(this.koaApp, {
         directory: join(__dirname, 'locales'),
         extension: '.json',
         locales: ['en', 'fr'],
         modes: ['query', 'subdomain', 'cookie', 'header', 'tld'],
-      }), // allows us to easily localize the API
+      }),
       auth,
       sslMiddleware,
       logger(),
-      addDefaultBody(), // if no body is present, put an empty object "{}" in its place.
-      compress({}), // compresses requests made to the API
+      addDefaultBody(),
+      compress({}),
       givePassword,
-      helmet({
-        // https://github.com/helmetjs/helmet
-        contentSecurityPolicy: {
-          directives: {
-            'media-src': ["'self'", 'https://client.crisp.chat'],
-            'connect-src': [
-              'https://api.gitbook.com',
-              'https://www.google-analytics.com/j/collect',
-              "'self'",
-              'https://api.mapbox.com',
-              'https://events.mapbox.com',
-              'https://stats.beta.gouv.fr',
-              'https://forms-eu1.hsforms.com',
-              'https://hubspot-forms-static-embed-eu1.s3.amazonaws.com',
-              'https://stats.beta-gouv.cloud-ed.fr',
-              'https://*.hotjar.com',
-              'https://*.hotjar.io',
-              'wss://*.hotjar.com',
-              '*.justice.gouv.fr',
-              'https://client.crisp.chat',
-              'https://storage.crisp.chat',
-              'wss://client.relay.crisp.chat',
-              'wss://stream.relay.crisp.chat',
-            ],
-            'font-src': ["'self'", 'https://fonts.gstatic.com', 'data:', 'https://*.hotjar.com', 'https://client.crisp.chat'],
-            'img-src': [
-              "'self'",
-              'data:',
-              'https://js-eu1.hsforms.net',
-              'https://api.hubspot.com',
-              'https://forms-eu1.hsforms.com',
-              'https://forms.hsforms.com',
-              'https://www.ionos.fr',
-              'https://img.freepik.com',
-              'https://image.noelshack.com',
-              'https://i.goopics.net/',
-              'https://client.crisp.chat',
-              'https://image.crisp.chat',
-              'https://storage.crisp.chat',
-            ],
-            // "img-src": [
-            //   "'self'",
-            //   "data:",
-            //   "https://js-eu1.hsforms.net",
-            //   "https://api.hubspot.com",
-            //   "https://forms-eu1.hsforms.com",
-            //   "https://forms.hsforms.com",
-            //   "https://www.ionos.fr",
-            //   "https://img.freepik.com",
-            //   "https://image.noelshack.com",
-            //   "https://i.goopics.net/",
-            // ],
-            'script-src': [
-              "'self'",
-              'https://*.hsforms.net',
-              '*.beta.gouv.fr',
-              '*.a-just.incubateur.net',
-              '*.calendly.com',
-              '*.google-analytics.com',
-              '*.hotjar.com',
-              "'sha256-jq7VWlK1R1baYNg3rH3wI3uXJc6evRSm19ho/ViohcE='",
-              "'sha256-92TNq2Axm9gJIJETcB7r4qpDc3JjxqUYF1fKonG4mvg='",
-              "'sha256-WXdHEUxHRTHqWKtUCBtUckcV5wN4y9jQwkZrGjfqr40='",
-              "'sha256-9jsqNCkYsDU3te2WUjv9qXV1DKXI1vT9hz3g7nNens8='",
-              "'sha256-Z/I+tLSqFCDH08E3fvI/F+QNinxE6TM+KmCxNmRcAAw='",
-              "'sha256-tBBLGYs6fvYemOy9hpbgu6tIIJNpdIZpuGpDXkhGTVw='",
-              "'sha256-HVge3cnZEH/UZtmZ65oo81F6FB06/nfTNYudQkA58AE='",
-              "'sha256-6x6g2SYysPsSMI15om2cLqbYnqaoyjXQD+Aivk9OP4U='",
-              "'sha256-A+0b+HOyTgrPPZgW1Tcb6UJIvj7fs09WPLWFtyqq1ks='",
-              //...scriptSha1Generate([`${__dirname}/front/index.html`]),
-              'https://client.crisp.chat',
-              'https://settings.crisp.chat',
-            ],
-            'default-src': ["'none'"],
-            'style-src': [
-              "'self'",
-              ...styleSha1Generate([`${__dirname}/front/index.html`]),
-              'cdnjs.cloudflare.com',
-              "'sha256-Ks+4bfA56EzWbsVt5/a+A7rCibdXWRQVb7y2dkDLIZM='",
-              "'sha256-MKASWYfd3dGFQes9nQT5XnslE3xYlnUb4cHpxhk4fag='",
-              "'sha256-eK2nDKvEyw7RbvnsAc4UTeSvLsouV8qnHxl0X48dCbs='",
-              'https://client.crisp.chat',
-              "'sha256-7Vo533bZB5hNdpZy9SiCUDc3JcgD9jqDXEc9aVAk5nY='",
-              "'sha256-UP0QZg7irvSMvOBz9mH2PIIE28+57UiavRfeVea0l3g='",
-              "'sha256-Mj/pDR/CuVebTo+8zwX6PU1+MXNnrzFL+dgRa0Q0JF0='",
-              "'sha256-BUZLvafdn4L6W6euGkBpnDrFVzIGLdSRjgp2e2gC+NE='",
-            ],
-            'worker-src': ['blob:'],
-            'frame-src': [
-              'https://app.videas.fr/',
-              'https://docs.a-just.beta.gouv.fr',
-              'https://meta.a-just.beta.gouv.fr',
-              'https://forms-eu1.hsforms.com/',
-              'https://calendly.com',
-              'https://game.crisp.chat',
-            ],
-            'worker-src': ['blob:'],
-            'frame-src': [
-              'https://app.videas.fr/',
-              'https://docs.a-just.beta.gouv.fr',
-              'https://meta.a-just.beta.gouv.fr',
-              'https://forms-eu1.hsforms.com/',
-              'https://calendly.com',
-            ],
-            'object-src': ["'self'"],
-            //'report-uri': ['/api/csp/report'],
-            'base-uri': ["'self'"],
-            'form-action': ["'self'", '*.hsforms.com'],
-            'upgrade-insecure-requests': [],
-          },
-          //reportOnly: true,
-        },
-        crossOriginEmbedderPolicy: false,
-        crossOriginOpenerPolicy: false,
-        crossOriginResourcePolicy: false,
-        originAgentCluster: false,
-        referrerPolicy: false,
-        strictTransportSecurity: {
-          maxAge: 31536000,
-          includeSubDomains: false,
-        },
-        xContentTypeOptions: 'nosniff',
-        xDnsPrefetchControl: false,
-        xDownloadOptions: false,
-        xFrameOptions: { action: 'deny' },
-        xPermittedCrossDomainPolicies: false,
-        xPoweredBy: false,
-        //xXssProtection: 1, don't work
-      }),
+      helmet(cspConfig),
       async (ctx, next) => {
-        //console.log('Client IP', ctx.request.ip)
         ctx.set('x-xss-protection', '1')
-
         if (CSP_URL_IGNORE_RULES.find((u) => ctx.url.startsWith(u))) {
           ctx.set('content-security-policy', '')
         }
-
         await next()
       },
       async (ctx, next) => {
         if (ctx.url.includes('/ap-bo/') && basicAuth) {
           await basicAuth.check((req, res, err) => {
-            if (err) {
-              throw err
-            } else {
-              next()
-            }
+            if (err) throw err
+            else next()
           })(ctx.req, ctx.res)
         } else {
           await next()
@@ -305,21 +301,19 @@ export default class App extends AppBase {
       },
     ])
 
+    // 🧩 CORS
     if (config.corsUrl) {
-      super.addMiddlewares([
-        cors({ origin: config.corsUrl, credentials: true }), // add cors headers to the requests
-      ])
+      super.addMiddlewares([cors({ origin: config.corsUrl, credentials: true })])
     } else {
-      super.addMiddlewares([
-        cors({ credentials: true }), // add cors headers to the requests
-      ])
+      super.addMiddlewares([cors({ credentials: true })])
     }
 
-    super.mountFolder(join(__dirname, 'routes-logs'), '/logs/') // adds a folder to scan for route files
-    super.mountFolder(join(__dirname, 'routes-api'), '/api/') // adds a folder to scan for route files
-    super.mountFolder(join(__dirname, 'routes-admin'), '/ap-bo/') // adds a folder to scan for route files
+    // 🔌 Montages de routes
+    super.mountFolder(join(__dirname, 'routes-logs'), '/logs/')
+    super.mountFolder(join(__dirname, 'routes-api'), '/api/')
+    super.mountFolder(join(__dirname, 'routes-admin'), '/ap-bo/')
     super.mountFolder(join(__dirname, 'routes-docker'), '/docker/')
-    super.mountFolder(join(__dirname, 'routes'), '/') // adds a folder to scan for route files
+    super.mountFolder(join(__dirname, 'routes'), '/')
 
     return super.start()
   }
@@ -333,7 +327,11 @@ export default class App extends AppBase {
 
   async warmupRedisCache(force = false) {
     await waitForRedis()
+    console.log('✅ Redis prêt!')
+
     const redis = getRedisClient()
+
+    //await redis.del('warmup-redis-lock')
 
     if (!redis) {
       console.warn('⚠️ Redis non disponible, warmup ignoré.')
@@ -349,8 +347,11 @@ export default class App extends AppBase {
       const lockSet = await redis.set(lockKey, lockId, { NX: true, EX: lockTTL })
       if (!lockSet) {
         console.log('⛔️ Warmup déjà en cours, on skip.')
+        const currentLock = await redis.get(lockKey)
+        console.log(`⛔️ Lock déjà pris : ${currentLock}`)
         return
       }
+
       hasLock = true
 
       console.log(`🚀 Début du warmupRedisCache @ ${new Date().toISOString()} (force=${force})`)
@@ -399,15 +400,37 @@ export default class App extends AppBase {
       if (hasLock) {
         try {
           const currentLock = await redis.get(lockKey)
-          if (currentLock === lockId) {
+
+          if (!currentLock) {
+            console.warn('⚠️ Aucun lock trouvé — il a probablement expiré avant la fin du warmup.')
+          } else if (currentLock === lockId) {
             await redis.del(lockKey)
+            console.log('🔓 Lock Redis libéré proprement.')
           } else {
-            console.warn('⚠️ Lock expiré ou pris par un autre process — non supprimé')
+            console.warn(`⚠️ Lock toujours actif mais détenu par une autre instance : ${currentLock}`)
           }
         } catch (err) {
-          console.error('❌ Impossible de libérer le lock Redis', err)
+          console.error('❌ Erreur lors de la tentative de libération du lock Redis :', err)
         }
       }
     }
+  }
+
+  waitForPostgres = async (sequelizeInstance, maxRetries = 10, delayMs = 2000) => {
+    let attempt = 0
+
+    while (attempt < maxRetries) {
+      try {
+        await sequelizeInstance.authenticate()
+        console.log('✅ PostgreSQL est prêt')
+        return
+      } catch (err) {
+        attempt++
+        console.warn(`⏳ Tentative ${attempt}/${maxRetries} : PostgreSQL non prêt (${err.message})`)
+        await new Promise((resolve) => setTimeout(resolve, delayMs))
+      }
+    }
+
+    throw new Error(`❌ PostgreSQL inaccessible après ${maxRetries} tentatives`)
   }
 }
