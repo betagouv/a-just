cbbc793e31d72676fe50284d88156dbfe4e1fc2c
Redwane <56769614+rzafari42@users.noreply.github.com>
2025-07-17 12:55:26 +0200

    [E2E Tests] [Access] - Wait for cockpit to load

--- end-to-end/cypress/e2e/access.cy.js
+++ end-to-end/cypress/e2e/access.cy.js
@@ -203,7 +203,7 @@ describe("Test d'accés aux pages", () => {
     checkBottomMenu();
   });
 
-  it("Give only access to Magistrat and check user does not have access to Greffie and Contractuel datas on panorama ", () => {
+  it("Give only access to Magistrat and check user does not have access to Greffier and Contractuel datas on panorama ", () => {
     const accessUrls = accessUrlList.map((access) => access.id);
     const accessFonctions = accessFonctionsList
       .filter((access) => access.label === "Accès aux magistrats")
@@ -323,6 +323,7 @@ describe("Test d'accés aux pages", () => {
     }).then(() => {
       cy.login();
       cy.visit("/cockpit");
+      cy.wait(10000); // Wait for the page to load completely
       cy.location("pathname").should("contain", "/cockpit");
       cy.get(".sub-main-header .categories-switch")
         .should("not.contain.text", "Siège")
