591861238c582faaa013723386a9d81b42548ad7
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2025-07-21 17:19:56 +0200

    fix angular upgrade

--- front/src/app/components/alert-small/alert-small.component.ts
+++ front/src/app/components/alert-small/alert-small.component.ts
@@ -1,7 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { ActionsInterface } from '../popup/popup.component';
-import { SanitizeHtmlPipe } from '../../pipes/sanitize-html/sanitize-html.pipe';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { MatIconModule } from '@angular/material/icon'
 
 /**
  * Composant alert qui affiche une alerte bloquante
@@ -10,7 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'aj-alert-small',
   standalone: true,
-  imports: [SanitizeHtmlPipe, MatIconModule],
+  imports: [MatIconModule],
   templateUrl: './alert-small.component.html',
   styleUrls: ['./alert-small.component.scss'],
 })
@@ -18,20 +16,20 @@ export class AlertSmallComponent {
   /**
    * Message text dans l'alerte
    */
-  @Input() alertMessage: string = '';
+  @Input() alertMessage: string = ''
   /**
    * Id de l'alerte
    */
-  @Input() alertId: number = 0;
+  @Input() alertId: number = 0
   /**
    * Event, clique sur le bouton pour fermer l'alert
    */
-  @Output() onClose: EventEmitter<null> = new EventEmitter();
+  @Output() onClose: EventEmitter<null> = new EventEmitter()
 
   /**
    * Close fonction
    */
   close() {
-    this.onClose.emit();
+    this.onClose.emit()
   }
 }
