c1a8ad53fdf349c54b5df25667f78a06422885fb
Redwane <56769614+rzafari42@users.noreply.github.com>
2025-08-10 19:58:54 +0200

    [E2E]: Simmulator - increased wait time

--- end-to-end/cypress/e2e/simulator.cy.js
+++ end-to-end/cypress/e2e/simulator.cy.js
@@ -6,14 +6,14 @@ describe("Simulator page", () => {
   // Vérification que la page du simulateur se charge correctement
   it("Check simulator page load", () => {
     cy.visit("/simulateur");
-    cy.wait(1000);
+    cy.wait(10000);
     cy.url().should("include", "/simulateur");
   });
 
   // Vérification que la page du simulateur à blanc se charge correctement
   it("Check white simulator page load", () => {
     cy.visit("/simulateur-sans-donnees");
-    cy.wait(1000);
+    cy.wait(10000);
     cy.url().should("include", "/simulateur-sans-donnees");
   });
 });
