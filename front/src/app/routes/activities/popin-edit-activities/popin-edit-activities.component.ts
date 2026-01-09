5c90497a18bde4367644ab695f678ec65bfd880b
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2026-01-09 15:55:58 +0100

    remove unused comment

--- front/src/app/routes/activities/popin-edit-activities/popin-edit-activities.component.ts
+++ front/src/app/routes/activities/popin-edit-activities/popin-edit-activities.component.ts
@@ -1084,7 +1084,6 @@ export class PopinEditActivitiesComponent extends MainClass implements OnChanges
   onClickInformationElement(htmlElement: string) {
     const element = document.querySelector(htmlElement)
     if (element && element.parentElement) {
-      console.log('element', element.parentElement)
       const icon = element.parentElement.querySelector('i')
       if (icon) {
         icon.click()
