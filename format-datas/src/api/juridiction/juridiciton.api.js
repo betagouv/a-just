109c928b68c1cafa595a0782d23b5a3b8d96887a
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2026-03-31 10:25:09 +0200

    remove format datas

--- format-datas/src/api/juridiction/juridiciton.api.js
+++
@@ -1,7 +0,0 @@
-import { instanceAxios } from "../../../utils/axios";
-
-export const onGetIelstListApi = async () => {
-  return await instanceAxios.get("/juridictions/get-all-ielst").then((res) => {
-    return res.data.data ? res.data.data : null;
-  });
-};
