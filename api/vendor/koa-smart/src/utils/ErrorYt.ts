72810d2a1df5c85ae14d33fe22f0e01517acb39c
François-Xavier Montigny <francois-xavier.montigny@dev-together.com>
2026-04-10 13:18:53 +0200

    remove custom koa-smart

--- api/vendor/koa-smart/src/utils/ErrorYt.ts
+++
@@ -1,8 +0,0 @@
-import ErrorApp from './ErrorApp';
-
-export default class ErrorYt extends ErrorApp {
-  constructor(status, message, toTranslate = false) {
-    super(status, message, toTranslate);
-    this.typeError = 'ErrorYt';
-  }
-}
