5a5144ead9eb79829c95d1af3f164163b5d71dbf
Redwane Zafari <56769614+rzafari42@users.noreply.github.com>
2026-05-06 16:55:59 +0200

    fix/update cypress env in end-to-end command.ts file
    Updated the method of accessing environment variable for server URL.

--- end-to-end/support/commands.ts
+++ end-to-end/support/commands.ts
@@ -46,8 +46,9 @@ Cypress.Commands.add("login", () => {
   cy.task("log", "[LOGIN] Starting cy.session()...");
   cy.session("login", () => {
     // Wait for backend to be available before attempting login
-    const serverUrl =
-      cy.env(["NG_APP_SERVER_URL"]) || "http://localhost:8081/api";
+    //const serverUrl =
+    //  cy.env(["NG_APP_SERVER_URL"]) || "http://localhost:8081/api";
+    const serverUrl = Cypress.env("NG_APP_SERVER_URL") || "http://localhost:8081/api";
 
     cy.task(
       "log",
