f1efb4457fe7b90bda9599e2300873ef6f34c4c6
PaulBallestra <paulballestra@gmail.com>
2026-06-26 16:35:23 +0200

    :art: Updated Style for banner, banner button and user feedback texts

--- front/src/app/interfaces/feedback.ts
+++ front/src/app/interfaces/feedback.ts
@@ -10,8 +10,8 @@ export interface FeedbackSubmitInterface {
 
 export const FEEDBACK_QUESTION = 'Comment évaluez-vous votre expérience avec A-JUST ?'
 
-export const FEEDBACK_BANNER_TEXT = 'Envie de donner votre avis ?'
+export const FEEDBACK_BANNER_TEXT = "Quel est votre niveau de satisfaction concernant l'outil A-JUST ?"
 
-export const FEEDBACK_BANNER_BUTTON = 'Donner mon avis'
+export const FEEDBACK_BANNER_BUTTON = 'Je donne mon avis'
 
 export const FEEDBACK_POPUP_TITLE = 'Votre avis compte'
